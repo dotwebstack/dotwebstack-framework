0562fb52c4947435b895b50e2aef07a3119f45db
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-16 14:28:08 +0200

    Add new test class for representation

---
+++ core/src/test/java/org/dotwebstack/framework/RepresentationTest.java
@@ -0,0 +1,9 @@
+package org.dotwebstack.framework;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RepresentationTest {
+
+}
