b09d62f180cf015cbe8933b28671d9c6c069937f
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-22 12:45:30 +0200

    Add representation and resource provider

---
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/representation/Representation.java
@@ -0,0 +1,77 @@
+package org.dotwebstack.framework.frontend.ld.representation;
+
+import java.util.Objects;
+import org.dotwebstack.framework.informationproduct.InformationProduct;
+import org.eclipse.rdf4j.model.IRI;
+
+public class Representation {
+
+  private IRI identifier;
+
+  private InformationProduct informationProduct;
+
+  private String urlPattern;
+
+  // private Stage stage;
+
+  private Representation(Builder builder) {
+    identifier = builder.identifier;
+    informationProduct = builder.informationProduct;
+    urlPattern = builder.urlPattern;
+    // stage = builder.stage;
+  }
+
+  public IRI getIdentifier() {
+    return identifier;
+  }
+
+  public InformationProduct getInformationProduct() {
+    return informationProduct;
+  }
+
+  public String getUrlPattern() {
+    return urlPattern;
+  }
+
+ /* public Stage getStage() {
+    return stage;
+  }*/
+
+  public static class Builder {
+
+    private IRI identifier;
+
+    private InformationProduct informationProduct;
+
+    private String urlPattern;
+
+    // private Stage stage;
+
+    public Builder(IRI identifier, InformationProduct informationProduct, String urlPattern) {
+      this.identifier = Objects.requireNonNull(identifier);
+      this.informationProduct = Objects.requireNonNull(informationProduct);
+      this.urlPattern = urlPattern;
+    }
+
+    public Builder informationProduct(
+        InformationProduct informationProduct) {
+      this.informationProduct = informationProduct;
+      return this;
+    }
+
+    public Builder urlPattern(String urlPattern) {
+      this.urlPattern = urlPattern;
+      return this;
+    }
+
+     /*public Builder stage(Stage stage) {
+      this.stage = stage;
+      return this;
+    }*/
+
+    public Representation build() {
+      return new Representation(this);
+    }
+  }
+
+}
---
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/representation/RepresentationResourceProvider.java
@@ -0,0 +1,56 @@
+package org.dotwebstack.framework.frontend.ld.representation;
+
+import org.dotwebstack.framework.AbstractResourceProvider;
+import org.dotwebstack.framework.config.ConfigurationBackend;
+import org.dotwebstack.framework.config.ConfigurationException;
+import org.dotwebstack.framework.informationproduct.InformationProductResourceProvider;
+import org.dotwebstack.framework.vocabulary.ELMO;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.query.GraphQuery;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RepresentationResourceProvider extends AbstractResourceProvider<Representation> {
+
+  private InformationProductResourceProvider informationProductResourceProvider;
+
+  @Autowired
+  public RepresentationResourceProvider(ConfigurationBackend configurationBackend,
+      InformationProductResourceProvider informationProductResourceProvider) {
+    // [todo] add stageResourceProvider
+    super(configurationBackend);
+    this.informationProductResourceProvider = informationProductResourceProvider;
+  }
+
+  @Override
+  protected GraphQuery getQueryForResources(RepositoryConnection conn) {
+    String query = "CONSTRUCT { ?s ?p ?o } WHERE { ?s ?p ?o . ?s a ?type . }";
+    GraphQuery graphQuery = conn.prepareGraphQuery(query);
+    graphQuery.setBinding("type", ELMO.REPRESENTATION);
+    return graphQuery;
+  }
+
+  @Override
+  protected Representation createResource(Model model, IRI identifier) {
+    // [todo] add stage
+    IRI informationProductIRI = getObjectIRI(model, identifier, ELMO.INFORMATION_PRODUCT_PROP)
+        .orElseThrow(() -> new ConfigurationException(
+            String.format("No <%s> information product has been found for representation <%s>.",
+                ELMO.INFORMATION_PRODUCT_PROP,
+                identifier)));
+
+    final String URL_PATTERN;
+    if (getObjectString(model, identifier, ELMO.URL_PATTERN).isPresent()) {
+      URL_PATTERN = getObjectString(model, identifier, ELMO.URL_PATTERN).get();
+    } else {
+      throw new ConfigurationException(String
+          .format("No <%s> url pattern has been found for representation <%s>.", ELMO.URL_PATTERN,
+              identifier));
+    }
+    Representation.Builder builder = new Representation.Builder(identifier,
+        informationProductResourceProvider.get((informationProductIRI)),
+        URL_PATTERN);
+    return builder.build();
+  }
+}
