34d50ffff55b34f35af16836e435c65a3faa614f
Joost Farla <joost@apiwise.nl>
2017-08-23 09:16:14 +0200

    Fix Javadoc build

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/OpenApiExtension.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/OpenApiExtension.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-class OpenApiExtension {
+public class OpenApiExtension {
 
   private SwaggerImporter swaggerImporter;
 
