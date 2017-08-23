402dad4dc91be2ddc0bafa62db0a5c1fbe0d7645
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-23 13:58:38 +0200

    Change getter of urlPatterns to return a collection

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/representation/Representation.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/representation/Representation.java
@@ -33,8 +33,8 @@ public class Representation {
     return informationProduct;
   }
 
-  public String getUrlPattern() {
-    return urlPattern;
+  public Collection<String> getUrlPatterns() {
+    return urlPatterns;
   }
 
   public Stage getStage() {
