34e68fb38ae1f7a98a17f211881ecfb940d43afc
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-24 11:36:20 +0200

    Add import of representation resource provider

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
@@ -2,6 +2,7 @@ package org.dotwebstack.framework.frontend.ld;
 
 import java.util.Objects;
 import org.dotwebstack.framework.frontend.http.HttpConfiguration;
+import org.dotwebstack.framework.frontend.ld.representation.RepresentationResourceProvider;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.io.ResourceLoader;
 
