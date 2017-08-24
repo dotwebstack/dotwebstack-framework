4f20c04e201f8b31c60c2effefdc0f5732202feb
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-24 11:21:54 +0200

    Add declaration of resource loader

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
@@ -5,6 +5,8 @@ import org.springframework.core.io.ResourceLoader;
 
 public class RequestMapper implements ResourceLoaderAware {
 
+  private ResourceLoader resourceLoader;
+
   @Override
   public void setResourceLoader(ResourceLoader resourceLoader) {
 
