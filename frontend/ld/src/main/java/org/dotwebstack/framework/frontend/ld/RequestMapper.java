781a29742caf982473be141476931b5426ccc333
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-24 11:13:15 +0200

    Implements resource loader aware

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
@@ -1,5 +1,12 @@
 package org.dotwebstack.framework.frontend.ld;
 
-public class RequestMapper {
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
 
+public class RequestMapper implements ResourceLoaderAware {
+
+  @Override
+  public void setResourceLoader(ResourceLoader resourceLoader) {
+
+  }
 }
