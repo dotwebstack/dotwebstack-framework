8b0e8599b2b1021475d5dd12595f49727f803aa7
Joost Aafjes <joostaafjes@gmail.com>
2017-08-28 12:05:07 +0200

    changes after code review simplified code (no Host Header check)

--- frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HostPreMatchingRequestFilter.java
+++ frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HostPreMatchingRequestFilter.java
@@ -1,17 +1,15 @@
 package org.dotwebstack.framework.frontend.http;
 
+import com.google.common.net.HttpHeaders;
+import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
-
-import com.google.common.net.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 @PreMatching
 @Provider
@@ -23,24 +21,22 @@ public class HostPreMatchingRequestFilter implements ContainerRequestFilter {
   public void filter(ContainerRequestContext requestContext)
       throws IOException {
 
-    // get host from header (forwarded host if set, otherwise host)
+    UriInfo uriInfo = requestContext.getUriInfo();
+    UriBuilder hostUriBuilder = uriInfo.getBaseUriBuilder();
+
+    // get host from header forwarded host if set
     String host = requestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST);
-    if (host == null) {
-      host = requestContext.getHeaderString(HttpHeaders.HOST);
-    }
-
-    // strip port
     if (host != null) {
-      host = host.split(":")[0];
-
-      // prefix host to request path
-      UriInfo uriInfo = requestContext.getUriInfo();
-
-      UriBuilder uriBuilder = UriBuilder.fromUri(uriInfo.getBaseUri() + host + "/" + uriInfo.getPath());
-      LOG.debug("Set new request path to {} (was {})", uriBuilder.toString(), uriInfo.getAbsolutePath());
-
-      requestContext.setRequestUri(uriBuilder.build());
+      hostUriBuilder.uri("http://" + host);
     }
+
+    UriBuilder uriBuilder = UriBuilder.fromUri(uriInfo.getBaseUri() +
+        hostUriBuilder.build().getHost() + "/" + uriInfo.getPath());
+
+    LOG.info("Set new request path to {} (was {})", uriBuilder.toString(),
+        uriInfo.getAbsolutePath());
+   
+    requestContext.setRequestUri(uriBuilder.build());
   }
 
 }
--- frontend/http/src/test/java/org/dotwebstack/framework/frontend/http/HostPreMatchingRequestFilterTest.java
+++ frontend/http/src/test/java/org/dotwebstack/framework/frontend/http/HostPreMatchingRequestFilterTest.java
@@ -1,24 +1,19 @@
 package org.dotwebstack.framework.frontend.http;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.google.common.net.HttpHeaders;
-import com.google.common.reflect.ClassPath;
+import java.net.URI;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import org.dotwebstack.framework.test.DBEERPEDIA;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.net.URI;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ResourceInfo;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.UriBuilder;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HostPreMatchingRequestFilterTest {
@@ -31,61 +26,58 @@ public class HostPreMatchingRequestFilterTest {
 
   private HostPreMatchingRequestFilter hostPreMatchingRequestFilter;
 
-
   @Before
   public void setUp() throws Exception {
     hostPreMatchingRequestFilter = new HostPreMatchingRequestFilter();
+
+    // Arrange
+    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
+
+    URI uri = URI.create("http://" + DBEERPEDIA.ORG_HOST + "/");
+    when(uriInfo.getBaseUri()).thenReturn(uri);
+    when(uriInfo.getBaseUriBuilder()).thenReturn(UriBuilder.fromUri(uri));
+    when(uriInfo.getPath()).thenReturn("beer");
   }
 
   @Test
   public void prefixPathWithHost() throws Exception {
 
-    // Arrange
-    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
-    when(uriInfo.getBaseUri()).thenReturn(URI.create("http://" + DBEERPEDIA.ORG_HOST));
-    when(uriInfo.getPath()).thenReturn("beer");
-
-    when(containerRequestContext.getHeaderString(HttpHeaders.HOST)).thenReturn(DBEERPEDIA.NL_HOST);
-
     // Act
     hostPreMatchingRequestFilter.filter(containerRequestContext);
 
-    // Result
-    verify(containerRequestContext).setRequestUri(UriBuilder.fromUri("http://" + DBEERPEDIA.ORG_HOST + DBEERPEDIA.NL_HOST + "/beer").build());
+    // Assert
+    verify(containerRequestContext).setRequestUri(UriBuilder.fromUri("http://" +
+        DBEERPEDIA.ORG_HOST + "/" + DBEERPEDIA.ORG_HOST + "/beer").build());
   }
 
   @Test
   public void prefixPathWithXForwardedHost() throws Exception {
 
     // Arrange
-    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
-    when(uriInfo.getBaseUri()).thenReturn(URI.create("http://" + DBEERPEDIA.ORG_HOST));
-    when(uriInfo.getPath()).thenReturn("beer");
-
-    when(containerRequestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST)).thenReturn(DBEERPEDIA.NL_HOST);
+    when(containerRequestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST))
+        .thenReturn(DBEERPEDIA.NL_HOST);
 
     // Act
     hostPreMatchingRequestFilter.filter(containerRequestContext);
 
-    // Result
-    verify(containerRequestContext).setRequestUri(UriBuilder.fromUri("http://" + DBEERPEDIA.ORG_HOST + DBEERPEDIA.NL_HOST + "/beer").build());
+    // Assert
+    verify(containerRequestContext).setRequestUri(UriBuilder.fromUri("http://" +
+        DBEERPEDIA.ORG_HOST + "/" + DBEERPEDIA.NL_HOST + "/beer").build());
   }
 
   @Test
   public void prefixPathWithXForwardedHostButIgnorePort() throws Exception {
 
     // Arrange
-    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
-    when(uriInfo.getBaseUri()).thenReturn(URI.create("http://" + DBEERPEDIA.ORG_HOST));
-    when(uriInfo.getPath()).thenReturn("beer");
-
-    when(containerRequestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST)).thenReturn(DBEERPEDIA.NL_HOST + ":8080");
+    when(containerRequestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST)).
+        thenReturn(DBEERPEDIA.NL_HOST + ":8080");
 
     // Act
     hostPreMatchingRequestFilter.filter(containerRequestContext);
 
-    // Result
-    verify(containerRequestContext).setRequestUri(UriBuilder.fromUri("http://" + DBEERPEDIA.ORG_HOST + DBEERPEDIA.NL_HOST + "/beer").build());
+    // Assert
+    verify(containerRequestContext).setRequestUri(UriBuilder.fromUri("http://" +
+        DBEERPEDIA.ORG_HOST + "/" + DBEERPEDIA.NL_HOST + "/beer").build());
   }
 
 }
