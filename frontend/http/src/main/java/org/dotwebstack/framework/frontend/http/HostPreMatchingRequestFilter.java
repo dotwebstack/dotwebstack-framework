37c6d9a35c439d29b7923a48b2aaeb90b6a0dd28
joostaafjes <joostaafjes@users.noreply.github.com>
2017-08-28 15:27:14 +0200

    Host pre matching request filter (#15)
    * test version
    
    * first version
    
    * getPath has no / at the beginning -> fix
    
    * update integration test
    
    * changes after code review
    simplified code (no Host Header check)

---
+++ frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HostPreMatchingRequestFilter.java
@@ -0,0 +1,42 @@
+package org.dotwebstack.framework.frontend.http;
+
+import com.google.common.net.HttpHeaders;
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@PreMatching
+@Provider
+public class HostPreMatchingRequestFilter implements ContainerRequestFilter {
+
+  private static final Logger LOG = LoggerFactory.getLogger(HostPreMatchingRequestFilter.class);
+
+  @Override
+  public void filter(ContainerRequestContext requestContext)
+      throws IOException {
+
+    UriInfo uriInfo = requestContext.getUriInfo();
+    UriBuilder hostUriBuilder = uriInfo.getBaseUriBuilder();
+
+    // get host from header forwarded host if set
+    String host = requestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST);
+    if (host != null) {
+      hostUriBuilder.uri("http://" + host);
+    }
+
+    UriBuilder uriBuilder = UriBuilder.fromUri(uriInfo.getBaseUri() +
+        hostUriBuilder.build().getHost() + "/" + uriInfo.getPath());
+
+    LOG.info("Set new request path to {} (was {})", uriBuilder.toString(),
+        uriInfo.getAbsolutePath());
+   
+    requestContext.setRequestUri(uriBuilder.build());
+  }
+
+}
