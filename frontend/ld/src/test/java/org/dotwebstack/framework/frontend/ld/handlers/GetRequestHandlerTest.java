4b866569ad52ef6950dbacb9e0f783a5440689f2
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-29 10:34:15 +0200

    Add test case for handlers

---
+++ frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/handlers/GetRequestHandlerTest.java
@@ -0,0 +1,50 @@
+package org.dotwebstack.framework.frontend.ld.handlers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.dotwebstack.framework.frontend.ld.representation.Representation;
+import org.dotwebstack.framework.test.DBEERPEDIA;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetRequestHandlerTest {
+
+  @Mock
+  Representation representation;
+
+  @Mock
+  ContainerRequestContext containerRequestContext;
+
+  GetRequestHandler getRequestHandler;
+
+  @Before
+  public void setUp() {
+    getRequestHandler = new GetRequestHandler(representation);
+  }
+
+  @Test
+  public void alwaysReturnRepresentationIdentifier() {
+    // Arrange
+    when(representation.getIdentifier()).thenReturn(DBEERPEDIA.BREWERY_REPRESENTATION);
+    UriInfo uriInfo = mock(UriInfo.class);
+    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
+    when(uriInfo.getPath()).thenReturn("/");
+
+    // Act
+    Response response = getRequestHandler.apply(containerRequestContext);
+
+    // Assert
+    assertThat(response.getStatus(), equalTo(200));
+    assertThat(response.getEntity(), equalTo(DBEERPEDIA.BREWERY_REPRESENTATION.stringValue()));
+  }
+}
