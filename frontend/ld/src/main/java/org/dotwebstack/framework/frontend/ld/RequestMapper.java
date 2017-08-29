1f842d955f6cdc96f1a037e31ba7ebf7b893a0a7
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-29 09:53:26 +0200

    Add imports for request mapper

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
@@ -2,13 +2,14 @@ package org.dotwebstack.framework.frontend.ld;
 
 import java.util.Objects;
 import org.dotwebstack.framework.frontend.http.HttpConfiguration;
+import org.dotwebstack.framework.frontend.ld.handlers.GetRequestHandler;
 import org.dotwebstack.framework.frontend.ld.representation.Representation;
 import org.dotwebstack.framework.frontend.ld.representation.RepresentationResourceProvider;
+import org.glassfish.jersey.server.model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ResourceLoaderAware;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
 
 public class RequestMapper implements ResourceLoaderAware {
 
