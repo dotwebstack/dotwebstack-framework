bc730b62577e245d28a5054b105a55fb0f752366
Joost Farla <joostfarla@users.noreply.github.com>
2017-08-29 12:38:46 +0200

    Disable WADL + add tests for OPTIONS/HEAD requests (#16)

--- frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HttpConfiguration.java
+++ frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HttpConfiguration.java
@@ -1,6 +1,7 @@
 package org.dotwebstack.framework.frontend.http;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ public class HttpConfiguration extends ResourceConfig {
   public HttpConfiguration() {
     super();
     packages("org.dotwebstack.framework.frontend.http");
+    property(ServerProperties.WADL_FEATURE_DISABLE, true);
   }
 
 }
