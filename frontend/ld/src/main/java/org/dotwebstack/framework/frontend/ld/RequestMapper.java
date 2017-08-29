131a912d3b915e8075b2cca9669755066831219e
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-08-29 10:06:41 +0200

    Add service annotation

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/RequestMapper.java
@@ -11,11 +11,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-public class RequestMapper implements ResourceLoaderAware {
+@Service
+public class RequestMapper {
 
   private static final Logger LOG = LoggerFactory.getLogger(RequestMapper.class);
-
-  private ResourceLoader resourceLoader;
 
   private HttpConfiguration httpConfiguration;
 
