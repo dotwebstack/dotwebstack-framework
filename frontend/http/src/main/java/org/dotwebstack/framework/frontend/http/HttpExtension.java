98a54dda3cf7119f715d076bc31f6f25b90fd309
Joost Farla <joost@apiwise.nl>
2017-08-30 16:05:52 +0200

    Minor

--- frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HttpExtension.java
+++ frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HttpExtension.java
@@ -2,6 +2,6 @@ package org.dotwebstack.framework.frontend.http;
 
 public interface HttpExtension {
 
-  public void initialize(HttpConfiguration httpConfiguration);
+  void initialize(HttpConfiguration httpConfiguration);
 
 }
