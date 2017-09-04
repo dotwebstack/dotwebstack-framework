84ce76bc5ace22c1267578e9a4ae21d1fabbbda3
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-09-04 08:57:12 +0200

    Add tests for http configuration resource check

--- frontend/http/src/test/java/org/dotwebstack/framework/frontend/http/HttpConfigurationTest.java
+++ frontend/http/src/test/java/org/dotwebstack/framework/frontend/http/HttpConfigurationTest.java
@@ -1,5 +1,9 @@
 package org.dotwebstack.framework.frontend.http;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class HttpConfigurationTest {
@@ -7,6 +11,29 @@ public class HttpConfigurationTest {
   @Test
   public void construct() {
     new HttpConfiguration();
+  }
+
+  @Test
+  public void resourceNotAlreadyRegisteredTest() {
+    final String absolutePath = "https://run.forrest.run/";
+    HttpConfiguration httpConfiguration = new HttpConfiguration();
+    org.glassfish.jersey.server.model.Resource.Builder resourceBuilder = org.glassfish.jersey.server.model.Resource
+        .builder().path(absolutePath);
+    assertThat(httpConfiguration.resourceAlreadyRegistered(absolutePath), equalTo(false));
+    httpConfiguration.registerResources(resourceBuilder.build());
+    assertThat(httpConfiguration.getResources(), hasSize(1));
+  }
+
+  @Test
+  public void resourceAlreadyRegisteredTest() {
+    final String absolutePath = "https://run.forrest.run/";
+    HttpConfiguration httpConfiguration = new HttpConfiguration();
+    org.glassfish.jersey.server.model.Resource.Builder resourceBuilder = org.glassfish.jersey.server.model.Resource
+        .builder().path(absolutePath);
+    assertThat(httpConfiguration.resourceAlreadyRegistered(absolutePath), equalTo(false));
+    httpConfiguration.registerResources(resourceBuilder.build());
+    assertThat(httpConfiguration.getResources(), hasSize(1));
+    assertThat(httpConfiguration.resourceAlreadyRegistered(absolutePath), equalTo(true));
   }
 
 }
