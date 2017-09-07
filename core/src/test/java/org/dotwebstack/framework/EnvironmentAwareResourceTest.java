846bf13515c19073eec3aaff0389fe7582d66226
Joost Aafjes <joostaafjes@gmail.com>
2017-09-07 11:04:21 +0200

    add tests

--- core/src/test/java/org/dotwebstack/framework/EnvironmentAwareResourceTest.java
+++ core/src/test/java/org/dotwebstack/framework/EnvironmentAwareResourceTest.java
@@ -62,9 +62,67 @@ public class EnvironmentAwareResourceTest {
   }
 
   @Test
-  public void environmentVariableShouldNotBeReplaced() throws IOException {
+  public void multipleEnvironmentVariables() throws IOException {
+    // Arrange
+    String input = "$NAME${NAME}$NAME";
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+
+    // Act
+    String output = IOUtils.toString(new EnvironmentAwareResource(inputStream).getInputStream());
+
+    // Assert
+    assertThat(output, is(DBEERPEDIA.BREWERY_DAVO_NAME + DBEERPEDIA.BREWERY_DAVO_NAME +
+        DBEERPEDIA.BREWERY_DAVO_NAME));
+  }
+
+  @Test
+  public void evironmentVariablesFollowedBySpecialCharacters() throws IOException {
+    // Arrange
+    String input = "$$NAME:${NAME}-$NAME$$NAME4$NAME";
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+
+    // Act
+    String output = IOUtils.toString(new EnvironmentAwareResource(inputStream).getInputStream());
+
+    // Assert
+    assertThat(output,
+        is(String.format("$%s:%s-%s$%s4%s", DBEERPEDIA.BREWERY_DAVO_NAME,
+            DBEERPEDIA.BREWERY_DAVO_NAME, DBEERPEDIA.BREWERY_DAVO_NAME,
+            DBEERPEDIA.BREWERY_DAVO_NAME, DBEERPEDIA.BREWERY_DAVO_NAME)));
+  }
+
+  @Test
+  public void evironmentVariablesWithNewLines() throws IOException {
+    // Arrange
+    String input = "$NAME\n${NAME}\n\r$NAME\r";
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+
+    // Act
+    String output = IOUtils.toString(new EnvironmentAwareResource(inputStream).getInputStream());
+
+    // Assert
+    assertThat(output,
+        is(String.format("%s\n%s\n\r%s\r", DBEERPEDIA.BREWERY_DAVO_NAME,
+            DBEERPEDIA.BREWERY_DAVO_NAME, DBEERPEDIA.BREWERY_DAVO_NAME)));
+  }
+
+  @Test
+  public void environmentVariableShouldNotBeReplaced1() throws IOException {
     // Arrange
     String input = "$NAMES";
+    InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+
+    // Act
+    String output = IOUtils.toString(new EnvironmentAwareResource(inputStream).getInputStream());
+
+    // Assert
+    assertThat(output, is(input));
+  }
+
+  @Test
+  public void environmentVariableShouldNotBeReplaced2() throws IOException {
+    // Arrange
+    String input = "$NAME_";
     InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
 
     // Act
