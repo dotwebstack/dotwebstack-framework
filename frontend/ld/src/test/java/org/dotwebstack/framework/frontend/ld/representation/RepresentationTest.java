e7408c68336dca1e7acc977cd668474f89b3464e
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-09-07 15:02:57 +0200

    Change value of url pattern

--- frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/representation/RepresentationTest.java
+++ frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/representation/RepresentationTest.java
@@ -8,7 +8,6 @@ import org.dotwebstack.framework.frontend.http.site.Site;
 import org.dotwebstack.framework.frontend.http.stage.Stage;
 import org.dotwebstack.framework.informationproduct.InformationProduct;
 import org.dotwebstack.framework.test.DBEERPEDIA;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -31,14 +30,13 @@ public class RepresentationTest {
   @Test
   public void builder() {
     // Act
-    final Representation representation = new Representation.Builder(DBEERPEDIA.BREWERIES,
-        DBEERPEDIA.URL_PATTERN.stringValue()).build();
+    final Representation representation = new Representation.Builder(DBEERPEDIA.BREWERIES).build();
 
     // Assert
     assertThat(representation.getIdentifier(), equalTo(DBEERPEDIA.BREWERIES));
     assertThat(representation.getInformationProduct(), equalTo(null));
     assertThat(representation.getStage(), equalTo(null));
-    Assert.assertNotNull(representation.getUrlPatterns());
+    assertThat(representation.getUrlPatterns(), equalTo(null));
   }
 
   @Test
@@ -50,35 +48,38 @@ public class RepresentationTest {
     assertThat(representation.getIdentifier(), equalTo(DBEERPEDIA.BREWERIES));
     assertThat(representation.getInformationProduct(), equalTo(null));
     assertThat(representation.getStage(), equalTo(null));
-    Assert.assertNotNull(representation.getUrlPatterns());
+    assertThat(representation.getUrlPatterns(), equalTo(null));
   }
 
   @Test
   public void builderWithUrlPattern() {
     // Act
-    final Representation representation = new Representation.Builder(DBEERPEDIA.BREWERIES,
-        DBEERPEDIA.URL_PATTERN.stringValue()).build();
+    final Representation representation = new Representation.Builder(DBEERPEDIA.BREWERIES)
+        .urlPatterns(DBEERPEDIA.URL_PATTERN_VALUE)
+        .build();
 
     // Assert
     assertThat(representation.getIdentifier(), equalTo(DBEERPEDIA.BREWERIES));
 
     assertThat(representation.getUrlPatterns().toArray()[0],
-        equalTo(DBEERPEDIA.URL_PATTERN.stringValue()));
+        equalTo(DBEERPEDIA.URL_PATTERN_VALUE));
   }
 
   @Test
   public void builderMultipleUrlPatterns() {
     // Act
     final Representation representation =
-        new Representation.Builder(DBEERPEDIA.BREWERIES, DBEERPEDIA.URL_PATTERN.stringValue(),
-            DBEERPEDIA.URL_PATTERN.stringValue(), DBEERPEDIA.URL_PATTERN.stringValue()).build();
+        new Representation.Builder(DBEERPEDIA.BREWERIES)
+            .urlPatterns(DBEERPEDIA.URL_PATTERN_VALUE, DBEERPEDIA.URL_PATTERN_VALUE,
+                DBEERPEDIA.URL_PATTERN_VALUE)
+            .build();
 
     // Assert
     assertThat(representation.getIdentifier(), equalTo(DBEERPEDIA.BREWERIES));
     assertThat(representation.getInformationProduct(), equalTo(null));
     assertThat(representation.getStage(), equalTo(null));
     assertThat(representation.getUrlPatterns().toArray()[0],
-        equalTo(DBEERPEDIA.URL_PATTERN.stringValue()));
+        equalTo(DBEERPEDIA.URL_PATTERN_VALUE));
     assertThat(representation.getUrlPatterns().size(), equalTo(3));
   }
 
@@ -92,16 +93,18 @@ public class RepresentationTest {
     final Stage stage = new Stage.Builder(DBEERPEDIA.BREWERIES, site).basePath(
         DBEERPEDIA.BASE_PATH.stringValue()).build();
 
-    final Representation representation = new Representation.Builder(DBEERPEDIA.BREWERIES,
-        DBEERPEDIA.URL_PATTERN.stringValue()).informationProduct(informationProduct).stage(
-            stage).build();
+    final Representation representation = new Representation.Builder(DBEERPEDIA.BREWERIES)
+        .informationProduct(informationProduct)
+        .stage(stage)
+        .urlPatterns(DBEERPEDIA.URL_PATTERN_VALUE)
+        .build();
 
     // Assert
     assertThat(representation.getIdentifier(), equalTo(DBEERPEDIA.BREWERIES));
     assertThat(representation.getInformationProduct(), equalTo(informationProduct));
     assertThat(representation.getStage(), equalTo(stage));
     assertThat(representation.getUrlPatterns().toArray()[0],
-        equalTo(DBEERPEDIA.URL_PATTERN.stringValue()));
+        equalTo(DBEERPEDIA.URL_PATTERN_VALUE));
     assertThat(representation.getUrlPatterns().size(), equalTo(1));
   }
 
