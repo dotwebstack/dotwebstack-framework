d7fad9fa11be955b135ecb7c71984a6ae2fb9166
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-09-11 09:17:30 +0200

    Add imports and change init of information product

--- frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/RequestMapperTest.java
+++ frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/RequestMapperTest.java
@@ -4,24 +4,34 @@ package org.dotwebstack.framework.frontend.ld;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.util.HashMap;
 import java.util.Map;
-import org.dotwebstack.framework.backend.BackendSource;
+import org.dotwebstack.framework.backend.Backend;
+import org.dotwebstack.framework.backend.BackendResourceProvider;
 import org.dotwebstack.framework.config.ConfigurationBackend;
 import org.dotwebstack.framework.frontend.http.HttpConfiguration;
 import org.dotwebstack.framework.frontend.http.site.Site;
 import org.dotwebstack.framework.frontend.http.stage.Stage;
 import org.dotwebstack.framework.frontend.ld.representation.Representation;
 import org.dotwebstack.framework.frontend.ld.representation.RepresentationResourceProvider;
+import org.dotwebstack.framework.informationproduct.AbstractInformationProduct;
 import org.dotwebstack.framework.informationproduct.InformationProduct;
+import org.dotwebstack.framework.informationproduct.InformationProductResourceProvider;
 import org.dotwebstack.framework.test.DBEERPEDIA;
+import org.dotwebstack.framework.vocabulary.ELMO;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.query.GraphQuery;
+import org.eclipse.rdf4j.query.impl.IteratingGraphQueryResult;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
 import org.glassfish.jersey.server.model.Resource;
@@ -95,10 +105,20 @@ public class RequestMapperTest {
         .basePath(DBEERPEDIA.BASE_PATH.stringValue())
         .build();
 
-    informationProduct = new InformationProduct.Builder(DBEERPEDIA.BREWERIES,
-        backendSource)
-        .label(DBEERPEDIA.BREWERIES_LABEL.stringValue())
-        .build();
+    informationProductResourceProvider =
+        new InformationProductResourceProvider(configurationBackend, backendResourceProvider);
+
+    when(backendResourceProvider.get(any())).thenReturn(backend);
+    when(graphQuery.evaluate()).thenReturn(new IteratingGraphQueryResult(ImmutableMap.of(),
+        ImmutableList.of(
+            valueFactory.createStatement(DBEERPEDIA.PERCENTAGES_INFORMATION_PRODUCT, RDF.TYPE,
+                ELMO.INFORMATION_PRODUCT),
+            valueFactory.createStatement(DBEERPEDIA.PERCENTAGES_INFORMATION_PRODUCT,
+                ELMO.BACKEND_PROP, DBEERPEDIA.BACKEND))));
+
+    informationProduct = mock(InformationProduct.class);
+    when(backend.createInformationProduct(eq(DBEERPEDIA.PERCENTAGES_INFORMATION_PRODUCT), eq(null),
+        any())).thenReturn(informationProduct);
 
     representation = new Representation.Builder(DBEERPEDIA.BREWERIES)
         .informationProduct(informationProduct)
