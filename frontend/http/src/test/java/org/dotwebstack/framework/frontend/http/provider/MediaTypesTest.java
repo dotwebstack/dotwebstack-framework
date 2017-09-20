687dd4d0edb76c5b4fe408749b5551fddf8eb01c
Rick Fleuren <info@rickfleuren.nl>
2017-09-20 12:50:05 +0200

    fix voor coverage

---
+++ frontend/http/src/test/java/org/dotwebstack/framework/frontend/http/provider/MediaTypesTest.java
@@ -0,0 +1,42 @@
+package org.dotwebstack.framework.frontend.http.provider;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class MediaTypesTest {
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void constructor_CannotBeInstantiated_BecauseAccessPrivate() throws NoSuchMethodException {
+    // Arrange & Act
+    Constructor<MediaTypes> constructor = MediaTypes.class.getDeclaredConstructor();
+
+    // Assert
+    assertThat(Modifier.isPrivate(constructor.getModifiers()), equalTo(true));
+  }
+
+  @Test
+  public void constructor_ThrowsException_TryingToInstantiate() throws NoSuchMethodException,
+      IllegalAccessException, InvocationTargetException, InstantiationException {
+    // Arrange
+    Constructor<MediaTypes> constructor = MediaTypes.class.getDeclaredConstructor();
+    constructor.setAccessible(true);
+
+    // Assert
+    thrown.expect(InvocationTargetException.class);
+    thrown.expectCause(hasProperty("message", equalTo(
+        "class org.dotwebstack.framework.frontend.http.provider.MediaTypes is not meant to be instantiated.")));
+
+    // Act
+    constructor.newInstance();
+  }
+}
