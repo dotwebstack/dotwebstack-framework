7b0f37a70644b241a371cfcfcc0d32255ac8b53a
Joost Aafjes <joostaafjes@gmail.com>
2017-10-13 16:32:41 +0200

    fix @ConfigurationProperties update tests

--- core/src/test/java/org/dotwebstack/framework/ApplicationPropertiesTest.java
+++ core/src/test/java/org/dotwebstack/framework/ApplicationPropertiesTest.java
@@ -2,25 +2,27 @@ package org.dotwebstack.framework;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
 
 import org.dotwebstack.framework.test.DBEERPEDIA;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ApplicationPropertiesTest {
 
-  @Mock
   private ApplicationProperties applicationProperties;
+
+  @Before
+  public void setUp() {
+    // Arrange
+    applicationProperties = new ApplicationProperties();
+  }
 
   @Test
   public void applicationProperty_SetDefaultValueForResourcePath_WhenInstantiated()
       throws Exception {
-    // Arrange
-    applicationProperties = new ApplicationProperties();
 
     // Assert
     assertThat(applicationProperties.getResourcePath(), is("file:src/main/resources"));
@@ -29,7 +31,7 @@ public class ApplicationPropertiesTest {
   @Test
   public void applicationProperty_ResourcePathIsSet_WithValue() throws Exception {
     // Arrange
-    when(applicationProperties.getResourcePath()).thenReturn(DBEERPEDIA.RESOURCE_PATH);
+    applicationProperties.setResourcePath(DBEERPEDIA.RESOURCE_PATH);
 
     // Assert
     assertThat(applicationProperties.getResourcePath(), is(DBEERPEDIA.RESOURCE_PATH));
@@ -38,7 +40,7 @@ public class ApplicationPropertiesTest {
   @Test
   public void applicationProperty_SystemGraphIsSet_WithValue() throws Exception {
     // Arrange
-    when(applicationProperties.getSystemGraph()).thenReturn(DBEERPEDIA.SYSTEM_GRAPH_IRI);
+    applicationProperties.setSystemGraph(DBEERPEDIA.SYSTEM_GRAPH);
 
     // Assert
     assertThat(applicationProperties.getSystemGraph(), is(DBEERPEDIA.SYSTEM_GRAPH_IRI));
