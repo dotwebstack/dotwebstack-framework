e9a241be154d9606af3bd22f52b2d74a01792dc6
Peter van 't Hof <Peter.vantHof@Kadaster.NL>
2017-10-30 14:52:52 +0100

    Feedback Rick

--- core/src/main/java/org/dotwebstack/framework/informationproduct/InformationProduct.java
+++ core/src/main/java/org/dotwebstack/framework/informationproduct/InformationProduct.java
@@ -17,7 +17,8 @@ public interface InformationProduct {
    * @param parameterValues Mapping of parameter name to value.
    * @throws BackendException If no value of for required parameter has been supplied.
    */
-  // TODO Mapping of Parameter instance to value? (instead of parameter String name to value)
+  // NOTE Consider a mapping of Parameter instance to value (instead of parameter String name to
+  // value), when the need arises
   Object getResult(Map<String, String> parameterValues);
 
   ResultType getResultType();
