8facb6f5023c3ff4a38a69c8a7b3064434997a32
Joost Farla <joost@apiwise.nl>
2017-11-08 21:25:05 +0100

    Remove imports

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/appearance/Appearance.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/appearance/Appearance.java
@@ -1,19 +1,8 @@
 package org.dotwebstack.framework.frontend.ld.appearance;
 
-import java.io.OutputStream;
-import java.io.StringWriter;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import lombok.NonNull;
-import org.dotwebstack.framework.frontend.http.stage.Stage;
-import org.dotwebstack.framework.informationproduct.InformationProduct;
-import org.dotwebstack.framework.vocabulary.ELMO;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.impl.LinkedHashModel;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.Rio;
 
 public class Appearance {
 
