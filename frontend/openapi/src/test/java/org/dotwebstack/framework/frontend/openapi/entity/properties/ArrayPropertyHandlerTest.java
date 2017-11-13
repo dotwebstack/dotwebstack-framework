ac58979b550ece3c0b41110ef0d28baf1b1ce950
Bert Verhelst <bert.verhelst@kadaster.nl>
2017-11-13 15:38:57 +0100

    GRID-1893 fix some checkstyle issues

--- frontend/openapi/src/test/java/org/dotwebstack/framework/frontend/openapi/entity/properties/ArrayPropertyHandlerTest.java
+++ frontend/openapi/src/test/java/org/dotwebstack/framework/frontend/openapi/entity/properties/ArrayPropertyHandlerTest.java
@@ -1,7 +1,7 @@
 package org.dotwebstack.framework.frontend.openapi.entity.properties;
 
-
 import static org.hamcrest.CoreMatchers.is;
+
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
