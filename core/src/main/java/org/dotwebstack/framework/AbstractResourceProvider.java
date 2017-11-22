26038c9ad8360e07a6a0484914d1ec5d8300b8b3
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-11-22 14:08:34 +0100

    Add new methods to get collections of string / iri

--- core/src/main/java/org/dotwebstack/framework/AbstractResourceProvider.java
+++ core/src/main/java/org/dotwebstack/framework/AbstractResourceProvider.java
@@ -1,8 +1,10 @@
 package org.dotwebstack.framework;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import lombok.NonNull;
@@ -32,7 +34,7 @@ public abstract class AbstractResourceProvider<R> implements ResourceProvider<R>
   private HashMap<IRI, R> resources = new HashMap<>();
 
   public AbstractResourceProvider(@NonNull ConfigurationBackend configurationBackend,
-      @NonNull ApplicationProperties applicationProperties) {
+                                  @NonNull ApplicationProperties applicationProperties) {
     this.configurationBackend = configurationBackend;
     this.applicationProperties = applicationProperties;
   }
@@ -42,11 +44,9 @@ public abstract class AbstractResourceProvider<R> implements ResourceProvider<R>
     if (!resources.containsKey(identifier)) {
       String availableResources = resources.keySet().stream().map(i -> "<" + i + ">").collect(
           Collectors.toSet()).toString();
-
       throw new IllegalArgumentException(String.format(
           "Resource <%s> not found. Available resources: %s", identifier, availableResources));
     }
-
     return resources.get(identifier);
   }
 
@@ -58,20 +58,16 @@ public abstract class AbstractResourceProvider<R> implements ResourceProvider<R>
   @PostConstruct
   public void loadResources() {
     RepositoryConnection repositoryConnection;
-
     try {
       repositoryConnection = configurationBackend.getRepository().getConnection();
     } catch (RepositoryException e) {
       throw new ConfigurationException("Error while getting repository connection.", e);
     }
-
     GraphQuery query = getQueryForResources(repositoryConnection);
-
     SimpleDataset simpleDataset = new SimpleDataset();
     simpleDataset.addDefaultGraph(applicationProperties.getSystemGraph());
     simpleDataset.addDefaultGraph(ELMO.CONFIG_GRAPHNAME);
     query.setDataset(simpleDataset);
-
     try {
       Model model = QueryResults.asModel(query.evaluate());
       model.subjects().forEach(identifier -> {
@@ -94,8 +90,17 @@ public abstract class AbstractResourceProvider<R> implements ResourceProvider<R>
     return Models.objectString(model.filter(subject, predicate, null));
   }
 
+  protected Optional<String[]> getObjectStrings(Model model, IRI subject, IRI predicate) {
+    Set<String> objectStrings = Models.objectStrings(model.filter(subject, predicate, null));
+    return Optional.of(objectStrings.toArray(new String[objectStrings.size()]));
+  }
+
   protected Optional<IRI> getObjectIRI(Model model, IRI subject, IRI predicate) {
     return Models.objectIRI(model.filter(subject, predicate, null));
+  }
+
+  protected Optional<Collection<IRI>> getObjectIris(Model model, IRI subject, IRI predicate) {
+    return Optional.of(Models.objectIRIs(model.filter(subject, predicate, null)));
   }
 
 }
