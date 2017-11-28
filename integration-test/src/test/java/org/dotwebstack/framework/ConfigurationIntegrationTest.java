5610dfe677b00c9508abddb0eb6eb63f8e42c4b9
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-11-28 08:29:32 +0100

    Change integration test

--- integration-test/src/test/java/org/dotwebstack/framework/ConfigurationIntegrationTest.java
+++ integration-test/src/test/java/org/dotwebstack/framework/ConfigurationIntegrationTest.java
@@ -48,7 +48,7 @@ public class ConfigurationIntegrationTest {
     assertThat(informationProductResourceProvider.getAll().entrySet(), hasSize(3));
     assertThat(informationProductResourceProvider.get(DBEERPEDIA.TUPLE_BREWERIES), notNullValue());
     assertThat(informationProductResourceProvider.get(DBEERPEDIA.GRAPH_BREWERIES), notNullValue());
-    assertThat(representationResourceProvider.getAll().entrySet(), hasSize(3));
+    assertThat(representationResourceProvider.getAll().entrySet(), hasSize(4));
     assertThat(representationResourceProvider.get(DBEERPEDIA.GRAPH_BREWERY_LIST_REPRESENTATION),
         notNullValue());
     assertThat(representationResourceProvider.get(DBEERPEDIA.TUPLE_BREWERY_LIST_REPRESENTATION),
