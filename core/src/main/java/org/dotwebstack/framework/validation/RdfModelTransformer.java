f7c36e0a4cb42cb67da0ceb3d3e8898947c2b726
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2017-12-05 12:49:45 +0100

    Remove jena model

--- core/src/main/java/org/dotwebstack/framework/validation/RdfModelTransformer.java
+++ core/src/main/java/org/dotwebstack/framework/validation/RdfModelTransformer.java
@@ -5,22 +5,40 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.util.FileUtils;
+import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParser;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.Rio;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.topbraid.spin.util.JenaUtil;
 
 public class RdfModelTransformer {
 
-  public static Model transformTrigFileToModel(Resource trigFile) throws IOException {
+  public static Model mergeResourceWithPrefixes(InputStream inputStreamPrefixes,
+      InputStream inputStreamData) throws IOException {
+    final Resource mergedDataResource =
+        new InputStreamResource(new SequenceInputStream(inputStreamPrefixes, inputStreamData));
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+    RDFWriter turtleWriter = Rio.createWriter(RDFFormat.TURTLE, byteArrayOutputStream);
+    RDFParser trigParser = Rio.createParser(RDFFormat.TRIG);
+    trigParser.setRDFHandler(turtleWriter);
+    trigParser.parse(mergedDataResource.getInputStream(), "");
+
+    Model result = Rio.parse(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), "",
+        RDFFormat.TURTLE);
+
+    byteArrayOutputStream.close();
+    inputStreamData.close();
+    return result;
+  }
+
+  public static Model getModel(Resource trigFile) throws IOException {
     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
     InputStream trigFileInputStream = trigFile.getInputStream();
     RDFWriter turtleWriter = Rio.createWriter(RDFFormat.TURTLE, byteArrayOutputStream);
+
     RDFParser trigParser = Rio.createParser(RDFFormat.TRIG);
     trigParser.setRDFHandler(turtleWriter);
     if (trigFile instanceof InputStreamResource) {
@@ -28,42 +46,26 @@ public class RdfModelTransformer {
     } else {
       trigParser.parse(trigFileInputStream, "");
     }
-    Model model = JenaUtil.createMemoryModel();
-    model.read(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), "",
-        FileUtils.langTurtle);
+
+    org.eclipse.rdf4j.model.Model result = Rio.parse(
+        new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), "", RDFFormat.TURTLE);
+
     byteArrayOutputStream.close();
-    trigFileInputStream.close();
-    return model;
+    return result;
   }
 
-  public static Model transformInputStreamToModel(InputStream inputStreamData) throws IOException {
+  public static Model getModel(InputStream inputStream) throws IOException {
     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
     RDFWriter turtleWriter = Rio.createWriter(RDFFormat.TURTLE, byteArrayOutputStream);
-    RDFParser trigParser = Rio.createParser(RDFFormat.TRIG);
-    trigParser.setRDFHandler(turtleWriter);
-    trigParser.parse(inputStreamData, "");
-    Model model = JenaUtil.createMemoryModel();
-    model.read(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), "",
-        FileUtils.langTurtle);
-    byteArrayOutputStream.close();
-    inputStreamData.close();
-    return model;
-  }
 
-  public static Model mergeResourceWithPrefixes(InputStream inputStreamPrefixes,
-                                                InputStream inputStreamData) throws IOException {
-    final Resource mergedDataResource = new InputStreamResource(
-        new SequenceInputStream(inputStreamPrefixes, inputStreamData));
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    RDFWriter turtleWriter = Rio.createWriter(RDFFormat.TURTLE, byteArrayOutputStream);
     RDFParser trigParser = Rio.createParser(RDFFormat.TRIG);
     trigParser.setRDFHandler(turtleWriter);
-    trigParser.parse(mergedDataResource.getInputStream(), "");
-    Model model = JenaUtil.createMemoryModel();
-    model.read(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), "",
-        FileUtils.langTurtle);
+    trigParser.parse(inputStream, "");
+
+    org.eclipse.rdf4j.model.Model result = Rio.parse(
+        new ByteArrayInputStream(byteArrayOutputStream.toByteArray()), "", RDFFormat.TURTLE);
+
     byteArrayOutputStream.close();
-    inputStreamData.close();
-    return model;
+    return result;
   }
 }
