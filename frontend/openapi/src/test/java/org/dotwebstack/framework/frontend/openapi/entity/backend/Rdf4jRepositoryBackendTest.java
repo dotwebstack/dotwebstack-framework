80ce970acbc5858f9bec00975502af7d9dc995df
John Schaap <John.Schaap@kadaster.nl>
2017-12-07 17:03:46 +0100

    GRID-2146 org.dotwebstack.framework.frontend.openapi.entity.backend exception coverage in unittests added

--- frontend/openapi/src/test/java/org/dotwebstack/framework/frontend/openapi/entity/backend/Rdf4jRepositoryBackendTest.java
+++ frontend/openapi/src/test/java/org/dotwebstack/framework/frontend/openapi/entity/backend/Rdf4jRepositoryBackendTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -22,9 +23,12 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryLockedException;
 import org.eclipse.rdf4j.repository.RepositoryResult;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -32,8 +36,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class Rdf4jRepositoryBackendTest {
 
-  private Rdf4jRepositoryBackend backend;
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
+  private Rdf4jRepositoryBackend backend;
   @Mock
   private Repository repository;
 
@@ -107,6 +113,39 @@ public class Rdf4jRepositoryBackendTest {
   }
 
   @Test
+  public void listSubjects_IllegalArgument_ThrowsExeption() {
+    RepositoryConnection repositoryConnection = mock(RepositoryConnection.class);
+    when(repository.getConnection()).thenReturn(repositoryConnection);
+    Value object = SimpleValueFactory.getInstance().createIRI("http://www.test.nl#test");
+    Value prop = SimpleValueFactory.getInstance().createBNode();
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(
+        "Property needs to be a URI node (property type: bNode).");
+    // Act
+    Collection<Value> values = backend.listSubjects( prop, object);
+  }
+
+
+
+  @Test
+  public void listSubjects_RepositoryFailedt_ThrowsExeption() {
+    RepositoryConnection repositoryConnection = mock(RepositoryConnection.class);
+
+    when(repository.getConnection()).thenReturn(repositoryConnection);
+    Exception dummy = new Exception("x");
+
+    doThrow(new RepositoryLockedException("a","b","c",dummy))
+        .when(repositoryConnection).begin();
+    Value object = SimpleValueFactory.getInstance().createIRI("http://www.test.nl#test");
+    Value prop = SimpleValueFactory.getInstance().createIRI("http://www.test.nl#test");
+    thrown.expect(Rdf4jBackendRuntimeException.class);
+    thrown.expectMessage(     "Error while querying RDF4J repository.");
+    // Act
+    Collection<Value> values = backend.listSubjects( prop, object);
+  }
+
+
+  @Test
   public void createUri_ReturnsUri_WhenCreated() throws Exception {
 
     IRI iri = backend.createURI("http://www.test.nl");
@@ -163,4 +202,37 @@ public class Rdf4jRepositoryBackendTest {
     assertEquals("http://www.test.nl", literal.getDatatype().toString());
   }
 
+
+  @Test
+  public void listObjects_IllegalArgument_ThrowsExeption() {
+    RepositoryConnection repositoryConnection = mock(RepositoryConnection.class);
+    when(repository.getConnection()).thenReturn(repositoryConnection);
+    Value subject = SimpleValueFactory.getInstance().createIRI("http://www.test.nl#test");
+    Value prop = SimpleValueFactory.getInstance().createBNode();
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(
+            "Subject needs to be a URI or blank node, property a URI node (types: [subject: URI, property: bNode]).");
+    // Act
+    Collection<Value> values = backend.listObjects(subject, prop);
+  }
+
+
+
+  @Test
+  public void listObjects_RepositoryFailedt_ThrowsExeption() {
+    RepositoryConnection repositoryConnection = mock(RepositoryConnection.class);
+
+    when(repository.getConnection()).thenReturn(repositoryConnection);
+    Exception dummy = new Exception("x");
+
+    doThrow(new RepositoryLockedException("a","b","c",dummy))
+            .when(repositoryConnection)
+            .begin();
+    Value subject = SimpleValueFactory.getInstance().createIRI("http://www.test.nl#test");
+    Value prop = SimpleValueFactory.getInstance().createIRI("http://www.test.nl#test");
+    thrown.expect(Rdf4jBackendRuntimeException.class);
+    thrown.expectMessage(     "Error while querying RDF4J repository.");
+    // Act
+    Collection<Value> values = backend.listObjects(subject, prop);
+  }
 }
