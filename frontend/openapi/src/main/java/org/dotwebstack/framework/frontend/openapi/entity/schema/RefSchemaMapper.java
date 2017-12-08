b6c539df21aeba30ea5cd38cba1076ef1080a8e1
Peter van 't Hof <Peter.vantHof@Kadaster.NL>
2017-12-08 08:37:11 +0100

    GRID-2168: RefSchemaMapper now delegates it work to the ObjectSchemaMapper

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/entity/schema/RefSchemaMapper.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/entity/schema/RefSchemaMapper.java
@@ -1,9 +1,7 @@
 package org.dotwebstack.framework.frontend.openapi.entity.schema;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
 import io.swagger.models.Model;
+import io.swagger.models.properties.ObjectProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.RefProperty;
 import lombok.NonNull;
@@ -31,12 +29,16 @@ public class RefSchemaMapper implements SchemaMapper<RefProperty, Object> {
           "Unable to resolve reference to swagger model: '%s'.", schema.getSimpleRef()));
     }
 
-    Builder<String, Object> builder = ImmutableMap.builder();
-    refModel.getProperties().forEach((propKey, propValue) -> builder.put(propKey,
-        Optional.fromNullable(schemaMapperAdapter.mapGraphValue(propValue, graphEntityContext,
-            schemaMapperAdapter, value))));
+    ObjectProperty objectProperty = new ObjectProperty(refModel.getProperties());
 
-    return builder.build();
+    objectProperty.setDescription(refModel.getDescription());
+    objectProperty.setExample(refModel.getExample());
+    objectProperty.setProperties(refModel.getProperties());
+    objectProperty.setTitle(refModel.getTitle());
+    objectProperty.setVendorExtensions(refModel.getVendorExtensions());
+
+    return schemaMapperAdapter.mapGraphValue(objectProperty, graphEntityContext,
+        schemaMapperAdapter, value);
   }
 
   public boolean supports(Property property) {
