69e1846c1e6731652dce971fd374e095fbbe6a12
Peter van 't Hof <Peter.vantHof@Kadaster.NL>
2017-12-12 16:02:08 +0100

    GRID-2172: added LD path functions used by teksten endpoints

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/entity/LdPathExecutor.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/entity/LdPathExecutor.java
@@ -5,7 +5,14 @@ import java.util.Collection;
 import lombok.NonNull;
 import org.apache.marmotta.ldpath.LDPath;
 import org.apache.marmotta.ldpath.exception.LDPathParseException;
+import org.apache.marmotta.ldpath.model.Constants;
+import org.apache.marmotta.ldpath.parser.Configuration;
+import org.apache.marmotta.ldpath.parser.DefaultConfiguration;
 import org.dotwebstack.framework.frontend.openapi.entity.backend.Rdf4jRepositoryBackend;
+import org.dotwebstack.framework.frontend.openapi.ldpath.JoinFunction;
+import org.dotwebstack.framework.frontend.openapi.ldpath.KeepAfterLastFunction;
+import org.dotwebstack.framework.frontend.openapi.ldpath.SortByPropertyFunction;
+import org.dotwebstack.framework.frontend.openapi.ldpath.TrimFunction;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.repository.Repository;
@@ -15,6 +22,16 @@ import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 
 public class LdPathExecutor {
+
+  private static final Configuration<Value> LD_PATH_CONFIG;
+
+  static {
+    LD_PATH_CONFIG = new DefaultConfiguration<>();
+    LD_PATH_CONFIG.addFunction(Constants.NS_LMF_FUNCS + "keepAfterLast",
+        new KeepAfterLastFunction<>());
+    LD_PATH_CONFIG.addFunction(Constants.NS_LMF_FUNCS + "trim", new TrimFunction<>());
+    LD_PATH_CONFIG.addFunction(Constants.NS_LMF_FUNCS + "join", new JoinFunction<>());
+  }
 
   private final LDPath<Value> ldpath;
 
@@ -40,7 +57,11 @@ public class LdPathExecutor {
       throw new LdPathExecutorRuntimeException("Unable to initialize RDF4JRepository.", re);
     }
 
-    return new LDPath<>(repositoryBackend);
+    LDPath<Value> result = new LDPath<>(repositoryBackend, LD_PATH_CONFIG);
+
+    result.registerFunction(new SortByPropertyFunction<>(ldpathNamespaces));
+
+    return result;
   }
 
   public Collection<Value> ldPathQuery(Value context, @NonNull String query) {
