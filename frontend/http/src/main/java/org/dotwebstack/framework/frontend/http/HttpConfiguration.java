274a693f8c31781b31a6c3bf86d7845be86441b0
RobinTopper <robin.topper@transfer-solutions.com>
2018-01-15 15:14:43 +0100

    [GRID-2061] Decoupled ObjectMapper & support for multiple ObjectProperty types (#100)
    * GRID-2061: GeometrySchemaMapper implemented
    
    * GRID-2061: SchemaMapper priority fixed
    
    * GRID-2061: Modified ObjectMapper generation and access modifiers
    
    * Commentaar Joost verwerkt
    
    * Temporary fix for failing integrationtest
    
    * GRID-2061: Commentaar Peter verwerkt
    
    * GRID-2061: Commentaar Peter verwerkt #2
    
    * Use already defined constant

--- frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HttpConfiguration.java
+++ frontend/http/src/main/java/org/dotwebstack/framework/frontend/http/HttpConfiguration.java
@@ -3,22 +3,16 @@ package org.dotwebstack.framework.frontend.http;
 import java.util.List;
 import lombok.NonNull;
 import org.dotwebstack.framework.frontend.http.error.WebApplicationExceptionMapper;
-import org.dotwebstack.framework.frontend.http.jackson.ObjectMapperProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.model.Resource;
 import org.glassfish.jersey.servlet.ServletProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HttpConfiguration extends ResourceConfig {
 
-  @Autowired
   public HttpConfiguration(@NonNull List<HttpModule> httpModules) {
-    super();
-
-    register(ObjectMapperProvider.class);
     register(HostPreMatchingRequestFilter.class);
     register(WebApplicationExceptionMapper.class);
     property(ServletProperties.FILTER_STATIC_CONTENT_REGEX, "/(robots.txt|(assets|webjars)/.*)");
