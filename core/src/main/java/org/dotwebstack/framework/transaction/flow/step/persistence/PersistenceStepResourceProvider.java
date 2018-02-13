0341935ff930c71a1b0ccc2dcbadc2058e8c5f36
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-02-13 13:50:21 +0100

    Implement createResource method for a persistenceStep

--- core/src/main/java/org/dotwebstack/framework/transaction/flow/step/persistence/PersistenceStepResourceProvider.java
+++ core/src/main/java/org/dotwebstack/framework/transaction/flow/step/persistence/PersistenceStepResourceProvider.java
@@ -2,6 +2,7 @@ package org.dotwebstack.framework.transaction.flow.step.persistence;
 
 import org.dotwebstack.framework.AbstractResourceProvider;
 import org.dotwebstack.framework.ApplicationProperties;
+import org.dotwebstack.framework.backend.BackendResourceProvider;
 import org.dotwebstack.framework.config.ConfigurationBackend;
 import org.dotwebstack.framework.vocabulary.ELMO;
 import org.eclipse.rdf4j.model.IRI;
@@ -10,6 +11,8 @@ import org.eclipse.rdf4j.query.GraphQuery;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 
 public class PersistenceStepResourceProvider extends AbstractResourceProvider<PersistenceStep> {
+
+  private BackendResourceProvider backendResourceProvider;
 
   public PersistenceStepResourceProvider(ConfigurationBackend configurationBackend,
       ApplicationProperties applicationProperties) {
@@ -28,6 +31,13 @@ public class PersistenceStepResourceProvider extends AbstractResourceProvider<Pe
 
   @Override
   protected PersistenceStep createResource(Model model, IRI identifier) {
-    return null;
+    PersistenceStep.Builder builder = new PersistenceStep.Builder(identifier);
+    getObjectIRI(model, identifier, ELMO.PERSISTENCE_STRATEGY_PROP).ifPresent(
+        builder::persistenceStrategy);
+    getObjectIRI(model, identifier, ELMO.BACKEND_PROP).ifPresent(
+        iri -> builder.backend(backendResourceProvider.get(iri)));
+    getObjectIRI(model, identifier, ELMO.TARGET_GRAPH_PROP).ifPresent(builder::targetGraph);
+
+    return builder.build();
   }
 }
