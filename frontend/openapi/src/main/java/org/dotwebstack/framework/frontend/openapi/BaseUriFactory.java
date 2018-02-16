d3e90ba1ffc8a420761e0b23dc94740890d0252e
NathanAtKadaster <nathan.vandalen@kadaster.nl>
2018-02-16 14:48:09 +0100

    GRID-2068: Remove Logger

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/BaseUriFactory.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/BaseUriFactory.java
@@ -5,16 +5,12 @@ import java.net.URISyntaxException;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.apache.http.client.utils.URIBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @UtilityClass
 public class BaseUriFactory {
 
-  private static final Logger LOG = LoggerFactory.getLogger(BaseUriFactory.class);
-
   public static String newBaseUri(@NonNull URI absolutePath, @NonNull String basePath) {
-    String baseUri = "";
+    String baseUri;
     try {
       URI base = new URIBuilder()
           .setScheme(absolutePath.getScheme())
