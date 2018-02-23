4ddce9183ae93037aef1247d95af3a434d7b5b70
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-02-23 09:19:10 +0100

    Add removed tests and change test parameters

--- core/src/test/java/org/dotwebstack/framework/param/TermParameterDefinitionTest.java
+++ core/src/test/java/org/dotwebstack/framework/param/TermParameterDefinitionTest.java
@@ -10,6 +10,7 @@ import com.google.common.collect.ImmutableMap;
 import org.dotwebstack.framework.param.term.StringTermParameter;
 import org.dotwebstack.framework.param.term.TermParameter;
 import org.dotwebstack.framework.test.DBEERPEDIA;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.XMLSchema;
 import org.junit.Test;
@@ -49,6 +50,40 @@ public class TermParameterDefinitionTest {
     // Assert
     assertThat(result.handle(ImmutableMap.of()), is("bar"));
     assertThat(result.getDefaultValue(), is("bar"));
+  }
+
+  @Test
+  public void createRequiredParameter_createsRequiredParameter_ForProvidedShape() {
+    // Arrange
+    IRI foo = VALUE_FACTORY.createIRI(XMLSchema.NAMESPACE, "foo");
+    ParameterDefinition definition = new TermParameterDefinition(DBEERPEDIA.NAME_PARAMETER_ID,
+        "name", new ShaclShape(XMLSchema.STRING, foo, ImmutableList.of()));
+
+    // Act
+    Parameter result = definition.createRequiredParameter();
+
+    // Assert
+    assertThat(result, instanceOf(StringTermParameter.class));
+    assertThat(result.getIdentifier(), is(DBEERPEDIA.NAME_PARAMETER_ID));
+    assertThat(result.getName(), is("name"));
+    assertThat(result.isRequired(), is(true));
+  }
+
+  @Test
+  public void createOptionalParameter_createsOptionalParameter_ForProvidedShape() {
+    // Arrange
+    IRI foo = VALUE_FACTORY.createIRI(XMLSchema.NAMESPACE, "foo");
+    ParameterDefinition definition = new TermParameterDefinition(DBEERPEDIA.NAME_PARAMETER_ID,
+        "name", new ShaclShape(XMLSchema.STRING, foo, ImmutableList.of()));
+
+    // Act
+    Parameter result = definition.createOptionalParameter();
+
+    // Assert
+    assertThat(result, instanceOf(StringTermParameter.class));
+    assertThat(result.getIdentifier(), is(DBEERPEDIA.NAME_PARAMETER_ID));
+    assertThat(result.getName(), is("name"));
+    assertThat(result.isRequired(), is(false));
   }
 
 }
