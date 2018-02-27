f8a3e5fe5ab7adc4bc66bf7b1e817ac7f34ddbd0
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-02-27 09:36:57 +0100

    Add endpoint structur and endpoint object def.

---
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/endpoint/EndPoint.java
@@ -0,0 +1,66 @@
+package org.dotwebstack.framework.frontend.ld.endpoint;
+
+import lombok.NonNull;
+import org.dotwebstack.framework.frontend.http.stage.Stage;
+import org.dotwebstack.framework.frontend.ld.parameter.ParameterMapper;
+import org.eclipse.rdf4j.model.Resource;
+
+public class EndPoint {
+
+  private Resource identifier;
+
+  private String pathPattern;
+
+  private String label;
+
+  private Stage stage;
+
+  private ParameterMapper parameterMapper;
+
+  private EndPoint(Builder builder) {
+    identifier = builder.identifier;
+    pathPattern = builder.pathPattern;
+    label = builder.label;
+    stage = builder.stage;
+    parameterMapper = builder.parameterMapper;
+  }
+
+  public static class Builder {
+
+    private Resource identifier;
+
+    private String pathPattern;
+
+    private String label;
+
+    private Stage stage;
+
+    private ParameterMapper parameterMapper;
+
+    public Builder(@NonNull Resource identifier, @NonNull String pathPattern) {
+      this.identifier = identifier;
+      this.pathPattern = pathPattern;
+    }
+
+    public Builder label(@NonNull String label) {
+      this.label = label;
+      return this;
+    }
+
+    public Builder stage(@NonNull Stage stage) {
+      this.stage = stage;
+      return this;
+    }
+
+    public Builder parameterMapper(@NonNull ParameterMapper parameterMapper) {
+      this.parameterMapper = parameterMapper;
+      return this;
+    }
+
+    public EndPoint build() {
+      return new EndPoint(this);
+    }
+
+  }
+
+}
---
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/endpoint/EndPointResourceProvider.java
@@ -0,0 +1,7 @@
+package org.dotwebstack.framework.frontend.ld.endpoint;
+
+public class EndPointResourceProvider {
+
+
+
+}
