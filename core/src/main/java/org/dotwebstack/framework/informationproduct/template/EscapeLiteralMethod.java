1fae86a3a1c4ce8c6d66cdcdb8a0e7bf45bcc493
NathanAtKadaster <nathan.vandalen@kadaster.nl>
2018-02-28 16:55:07 +0100

    GRID-2065: Refactor escape method `EscapeLiteralMethod` class

--- core/src/main/java/org/dotwebstack/framework/informationproduct/template/EscapeLiteralMethod.java
+++ core/src/main/java/org/dotwebstack/framework/informationproduct/template/EscapeLiteralMethod.java
@@ -3,28 +3,16 @@ package org.dotwebstack.framework.informationproduct.template;
 import freemarker.template.SimpleScalar;
 import freemarker.template.TemplateMethodModelEx;
 import freemarker.template.TemplateModelException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
 
 public class EscapeLiteralMethod implements TemplateMethodModelEx {
 
   /* the way this method gets exposed to FreeMarker context */
   static final String CTX_NAME = "literal";
-
-  private static final Map<String, String> SPARQL_ESCAPE_SEARCH_REPLACEMENTS = new HashMap<>();
-
-  static {
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\t", "\\t");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\n", "\\n");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\r", "\\r");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\b", "\\b");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\f", "\\f");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\"", "\\\"");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("'", "\\'");
-    SPARQL_ESCAPE_SEARCH_REPLACEMENTS.put("\\", "\\\\");
-  }
+  private static final String[] in = {"\t", "\n", "\r", "\b", "\f", "\"", "'", "\\"};
+  private static final String[] out = {"\\t", "\\n", "\\r", "\\b", "\\f", "\\\"", "\\'", "\\\\"};
 
   @Override
   public Object exec(@NonNull List arguments) throws TemplateModelException {
@@ -33,21 +21,7 @@ public class EscapeLiteralMethod implements TemplateMethodModelEx {
           "No arguments provided. Expected first argument of type String");
     }
 
-    return escape(((SimpleScalar) arguments.get(0)).getAsString());
+    return StringUtils.replaceEach(((SimpleScalar) arguments.get(0)).getAsString(), in, out);
   }
 
-  private static String escape(String string) {
-
-    StringBuilder builder = new StringBuilder(string);
-    for (int i = 0; i < builder.length(); i++) {
-      String replacement = SPARQL_ESCAPE_SEARCH_REPLACEMENTS.get("" + builder.charAt(i));
-      if (replacement != null) {
-        builder.deleteCharAt(i);
-        builder.insert(i, replacement);
-        // advance past the replacement
-        i += (replacement.length() - 1);
-      }
-    }
-    return builder.toString();
-  }
 }
