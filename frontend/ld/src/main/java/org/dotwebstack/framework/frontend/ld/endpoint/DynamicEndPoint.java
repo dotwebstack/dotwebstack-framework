6cfe4d3d800e55522af8993d241296d5996f84f5
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-03-01 14:55:49 +0100

    Add dynamicendpoint

---
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/endpoint/DynamicEndPoint.java
@@ -0,0 +1,34 @@
+package org.dotwebstack.framework.frontend.ld.endpoint;
+
+import lombok.NonNull;
+import org.dotwebstack.framework.frontend.ld.parameter.ParameterMapper;
+import org.eclipse.rdf4j.model.Resource;
+
+public class DynamicEndPoint extends EndPoint {
+
+  private ParameterMapper parameterMapper;
+
+  private DynamicEndPoint(Builder builder) {
+    super(builder);
+    this.parameterMapper = builder.parameterMapper;
+  }
+
+  public ParameterMapper getParameterMapper() {
+    return parameterMapper;
+  }
+
+  public static class Builder extends EndPoint.Builder {
+
+    private ParameterMapper parameterMapper;
+
+    public Builder(@NonNull Resource identifier, @NonNull String pathPattern,
+        @NonNull ParameterMapper parameterMapper) {
+      super(identifier, pathPattern);
+      this.parameterMapper = parameterMapper;
+    }
+
+    public DynamicEndPoint build() {
+      return new DynamicEndPoint(this);
+    }
+  }
+}
