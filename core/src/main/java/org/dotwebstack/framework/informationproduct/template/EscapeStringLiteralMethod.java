2f31acba5d12dbe1a72e70ee91cf500a75de9f4e
petervanthof <peter@petervanthof.IT>
2018-03-06 07:22:38 +0100

    GRID-2687: DoD -added some documentation (#126)

--- core/src/main/java/org/dotwebstack/framework/informationproduct/template/EscapeStringLiteralMethod.java
+++ core/src/main/java/org/dotwebstack/framework/informationproduct/template/EscapeStringLiteralMethod.java
@@ -7,6 +7,20 @@ import java.util.List;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Escapes the following characters within a string literal:
+ * <ul>
+ * <li>\t</li>
+ * <li>\t</li>
+ * <li>\r</li>
+ * <li>\b</li>
+ * <li>\f</li>
+ * <li>"</li>
+ * <li>'</li>
+ * <li>\</li>
+ * </ul>
+ * This method can be used by invoking the {@code literal()} function within a Freemarker template.
+ */
 public class EscapeStringLiteralMethod implements TemplateMethodModelEx {
 
   /* the way this method gets exposed to FreeMarker context */
@@ -14,6 +28,10 @@ public class EscapeStringLiteralMethod implements TemplateMethodModelEx {
   private static final String[] in = {"\t", "\n", "\r", "\b", "\f", "\"", "'", "\\"};
   private static final String[] out = {"\\t", "\\n", "\\r", "\\b", "\\f", "\\\"", "\\'", "\\\\"};
 
+  /**
+   * @throws TemplateModelException If no argument is provided. Expected is a {@code List} with
+   *         exactly one element of type {@code String}.
+   */
   @Override
   public Object exec(@NonNull List arguments) throws TemplateModelException {
     if (arguments.isEmpty()) {
