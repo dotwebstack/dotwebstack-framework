61a7d537f580e0ee6eb44299ff44a628e519e830
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-03-15 11:19:08 +0100

    Add direct-/dynamicendpoint resource provider

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/mappers/LdEndPointRequestMapper.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/mappers/LdEndPointRequestMapper.java
@@ -1,5 +1,8 @@
 package org.dotwebstack.framework.frontend.ld.mappers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import javax.ws.rs.HttpMethod;
 import lombok.NonNull;
 import org.dotwebstack.framework.frontend.http.ExpandFormatParameter;
@@ -8,6 +11,7 @@ import org.dotwebstack.framework.frontend.ld.SupportedMediaTypesScanner;
 import org.dotwebstack.framework.frontend.ld.endpoint.AbstractEndPoint;
 import org.dotwebstack.framework.frontend.ld.endpoint.DirectEndPoint;
 import org.dotwebstack.framework.frontend.ld.endpoint.DirectEndPointResourceProvider;
+import org.dotwebstack.framework.frontend.ld.endpoint.DynamicEndPointResourceProvider;
 import org.dotwebstack.framework.frontend.ld.handlers.EndPointRequestHandlerFactory;
 import org.dotwebstack.framework.frontend.ld.representation.Representation;
 import org.glassfish.jersey.server.model.Resource;
@@ -21,23 +25,31 @@ public class LdEndPointRequestMapper {
 
   private static final Logger LOG = LoggerFactory.getLogger(LdEndPointRequestMapper.class);
 
-  private final DirectEndPointResourceProvider endPointResourceProvider;
+  private final DirectEndPointResourceProvider directEndPointResourceProvider;
+
+  private final DynamicEndPointResourceProvider dynamicEndPointResourceProvider;
 
   private final SupportedMediaTypesScanner supportedMediaTypesScanner;
 
   private final EndPointRequestHandlerFactory endPointRequestHandlerFactory;
 
   @Autowired
-  public LdEndPointRequestMapper(@NonNull DirectEndPointResourceProvider endPointResourceProvider,
+  public LdEndPointRequestMapper(
+      @NonNull DirectEndPointResourceProvider directEndPointResourceProvider,
+      @NonNull DynamicEndPointResourceProvider dynamicEndPointResourceProvider,
       @NonNull SupportedMediaTypesScanner supportedMediaTypesScanner,
       @NonNull EndPointRequestHandlerFactory endPointRequestHandlerFactory) {
-    this.endPointResourceProvider = endPointResourceProvider;
+    this.directEndPointResourceProvider = directEndPointResourceProvider;
+    this.dynamicEndPointResourceProvider = dynamicEndPointResourceProvider;
     this.supportedMediaTypesScanner = supportedMediaTypesScanner;
     this.endPointRequestHandlerFactory = endPointRequestHandlerFactory;
   }
 
   public void loadEndPoints(HttpConfiguration httpConfiguration) {
-    for (AbstractEndPoint endPoint : endPointResourceProvider.getAll().values()) {
+    List<AbstractEndPoint> allEndPoints = new ArrayList<>();
+    allEndPoints.addAll(directEndPointResourceProvider.getAll().values());
+    allEndPoints.addAll(dynamicEndPointResourceProvider.getAll().values());
+    for (AbstractEndPoint endPoint : allEndPoints) {
       if (endPoint.getStage() != null) {
         mapRepresentation(endPoint, httpConfiguration);
       } else {
@@ -47,22 +59,40 @@ public class LdEndPointRequestMapper {
   }
 
   private void mapRepresentation(AbstractEndPoint endPoint, HttpConfiguration httpConfiguration) {
+    String basePath = endPoint.getStage().getFullPath();
+    String absolutePath = basePath.concat(endPoint.getPathPattern());
+    Resource.Builder resourceBuilder = Resource.builder().path(absolutePath);
     if (endPoint instanceof DirectEndPoint) {
-      final Representation representation = ((DirectEndPoint) endPoint).getGetRepresentation();
-      String basePath = endPoint.getStage().getFullPath();
-      String absolutePath = basePath.concat(endPoint.getPathPattern());
-      Resource.Builder resourceBuilder = Resource.builder().path(absolutePath);
+      Optional<Representation> getRepresentation =
+          Optional.ofNullable(((DirectEndPoint) endPoint).getGetRepresentation());
+      Optional<Representation> postRepresentation =
+          Optional.ofNullable(((DirectEndPoint) endPoint).getPostRepresentation());
+
+      getRepresentation.ifPresent(representation -> {
+        resourceBuilder.addMethod(HttpMethod.GET).handledBy(
+            endPointRequestHandlerFactory.newEndPointRequestHandler(endPoint)).produces(
+                supportedMediaTypesScanner.getMediaTypes(
+                    representation.getInformationProduct().getResultType())).nameBindings(
+                        ExpandFormatParameter.class);
+      });
+      postRepresentation.ifPresent(representation -> {
+        resourceBuilder.addMethod(HttpMethod.POST).handledBy(
+            endPointRequestHandlerFactory.newEndPointRequestHandler(endPoint)).produces(
+                supportedMediaTypesScanner.getMediaTypes(
+                    representation.getInformationProduct().getResultType())).nameBindings(
+                        ExpandFormatParameter.class);
+      });
+    } else {
       resourceBuilder.addMethod(HttpMethod.GET).handledBy(
           endPointRequestHandlerFactory.newEndPointRequestHandler(endPoint)).produces(
-              supportedMediaTypesScanner.getMediaTypes(
-                  representation.getInformationProduct().getResultType())).nameBindings(
-                      ExpandFormatParameter.class);
-      if (!httpConfiguration.resourceAlreadyRegistered(absolutePath)) {
-        httpConfiguration.registerResources(resourceBuilder.build());
-        LOG.debug("Mapped GET operation for request path {}", absolutePath);
-      } else {
-        LOG.error("Resource <{}> is not registered", absolutePath);
-      }
+              supportedMediaTypesScanner.getAllSupportedMediaTypes()).nameBindings(
+                  ExpandFormatParameter.class);
+    }
+    if (!httpConfiguration.resourceAlreadyRegistered(absolutePath)) {
+      httpConfiguration.registerResources(resourceBuilder.build());
+      LOG.debug("Mapped GET operation for request path {}", absolutePath);
+    } else {
+      LOG.error("Resource <{}> is not registered", absolutePath);
     }
   }
 
