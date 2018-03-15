aa719f23047b9440fd6d1fc76145df9ce909af0b
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-03-15 16:36:21 +0100

    Add test for endpoint request handler and dynamicendpoints

--- frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/handlers/EndPointRequestHandlerTest.java
+++ frontend/ld/src/test/java/org/dotwebstack/framework/frontend/ld/handlers/EndPointRequestHandlerTest.java
@@ -3,12 +3,17 @@ package org.dotwebstack.framework.frontend.ld.handlers;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
@@ -16,11 +21,15 @@ import javax.ws.rs.core.UriInfo;
 import org.dotwebstack.framework.backend.ResultType;
 import org.dotwebstack.framework.config.ConfigurationException;
 import org.dotwebstack.framework.frontend.ld.endpoint.DirectEndPoint;
+import org.dotwebstack.framework.frontend.ld.endpoint.DynamicEndPoint;
 import org.dotwebstack.framework.frontend.ld.entity.GraphEntity;
 import org.dotwebstack.framework.frontend.ld.entity.TupleEntity;
+import org.dotwebstack.framework.frontend.ld.parameter.ParameterMapper;
 import org.dotwebstack.framework.frontend.ld.representation.Representation;
 import org.dotwebstack.framework.frontend.ld.representation.RepresentationResourceProvider;
 import org.dotwebstack.framework.informationproduct.InformationProduct;
+import org.dotwebstack.framework.test.DBEERPEDIA;
+import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.query.GraphQueryResult;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.junit.Before;
@@ -62,6 +71,7 @@ public class EndPointRequestHandlerTest {
     getRequestHandler = new EndPointRequestHandler(endPoint, endPointRequestParameterMapper,
         representationResourceProvider);
     when(endPoint.getGetRepresentation()).thenReturn(representation);
+    when(endPoint.getPostRepresentation()).thenReturn(representation);
     when(representation.getInformationProduct()).thenReturn(informationProduct);
   }
 
@@ -102,7 +112,7 @@ public class EndPointRequestHandlerTest {
   }
 
   @Test
-  public void apply_ReturnQueryRepresentation_WhenTupleQueryResult() {
+  public void apply_ReturnQueryRepresentation_WhenTupleQueryResultGetRequest() {
     // Arrange
     TupleQueryResult queryResult = mock(TupleQueryResult.class);
     when(informationProduct.getResult(ImmutableMap.of())).thenReturn(queryResult);
@@ -125,6 +135,112 @@ public class EndPointRequestHandlerTest {
     TupleEntity entity = (TupleEntity) response.getEntity();
     assertThat(entity.getQueryResult(), equalTo(queryResult));
     assertThat(entity.getRepresentation(), equalTo(endPoint.getGetRepresentation()));
+  }
+
+  @Test
+  public void apply_ReturnQueryRepresentation_WhenTupleQueryResultGetRequestDynamicEndpoint() {
+    // Arrange
+    TupleQueryResult queryResult = mock(TupleQueryResult.class);
+    when(informationProduct.getResult(any())).thenReturn(queryResult);
+    when(informationProduct.getResultType()).thenReturn(ResultType.TUPLE);
+    when(representation.getInformationProduct()).thenReturn(informationProduct);
+
+    UriInfo uriInfo = mock(UriInfo.class);
+    final String appliesTo = "subjectValue";
+    MultivaluedMap<String, String> parameterValues = new MultivaluedHashMap<>();
+    parameterValues.putSingle("subject", appliesTo);
+    Map<Resource, Representation> allRepresentations = new HashMap<>();
+    allRepresentations.put(representation.getIdentifier(), representation);
+
+    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
+    when(containerRequestContext.getUriInfo().getPathParameters()).thenReturn(parameterValues);
+    when(uriInfo.getPath()).thenReturn("/");
+    when(containerRequestContext.getRequest()).thenReturn(mock(Request.class));
+    when(containerRequestContext.getRequest().getMethod()).thenReturn(HttpMethod.GET);
+    DynamicEndPoint dynamicEndPoint = mock(DynamicEndPoint.class);
+    when(dynamicEndPoint.getParameterMapper()).thenReturn(mock(ParameterMapper.class));
+    when(representationResourceProvider.getAll()).thenReturn(allRepresentations);
+    when(representation.getAppliesTo()).thenReturn(ImmutableList.of(appliesTo));
+    EndPointRequestHandler requestHandler = new EndPointRequestHandler(dynamicEndPoint,
+        endPointRequestParameterMapper, representationResourceProvider);
+
+    // Act
+    Response response = requestHandler.apply(containerRequestContext);
+
+    // Assert
+    assertThat(response.getStatus(), equalTo(200));
+    assertThat(response.getEntity(), instanceOf(TupleEntity.class));
+    TupleEntity entity = (TupleEntity) response.getEntity();
+    assertThat(entity.getQueryResult(), equalTo(queryResult));
+  }
+
+  @Test
+  public void apply_ReturnQueryRepresentation_WhenTupleQueryResultPostRequest() {
+    // Arrange
+    TupleQueryResult queryResult = mock(TupleQueryResult.class);
+    when(informationProduct.getResult(ImmutableMap.of())).thenReturn(queryResult);
+    when(informationProduct.getResultType()).thenReturn(ResultType.TUPLE);
+
+    UriInfo uriInfo = mock(UriInfo.class);
+    MultivaluedMap<String, String> parameterValues = mock(MultivaluedMap.class);
+    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
+    when(containerRequestContext.getUriInfo().getPathParameters()).thenReturn(parameterValues);
+    when(uriInfo.getPath()).thenReturn("/");
+    when(containerRequestContext.getRequest()).thenReturn(mock(Request.class));
+    when(containerRequestContext.getRequest().getMethod()).thenReturn(HttpMethod.POST);
+
+    // Act
+    Response response = getRequestHandler.apply(containerRequestContext);
+
+    // Assert
+    assertThat(response.getStatus(), equalTo(200));
+    assertThat(response.getEntity(), instanceOf(TupleEntity.class));
+    TupleEntity entity = (TupleEntity) response.getEntity();
+    assertThat(entity.getQueryResult(), equalTo(queryResult));
+    assertThat(entity.getRepresentation(), equalTo(endPoint.getGetRepresentation()));
+  }
+
+  @Test
+  public void apply_ConfigurationException_WhenUnsupportedRequestDirectEndPoint() {
+    // Arrange
+    thrown.expect(ConfigurationException.class);
+    thrown.expectMessage(String.format("Result type %s not supported for endpoint %s",
+        HttpMethod.PUT, DBEERPEDIA.DOC_ENDPOINT));
+
+    UriInfo uriInfo = mock(UriInfo.class);
+    MultivaluedMap<String, String> parameterValues = mock(MultivaluedMap.class);
+    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
+    when(containerRequestContext.getUriInfo().getPathParameters()).thenReturn(parameterValues);
+    when(uriInfo.getPath()).thenReturn("/");
+    when(containerRequestContext.getRequest()).thenReturn(mock(Request.class));
+    when(containerRequestContext.getRequest().getMethod()).thenReturn(HttpMethod.PUT);
+    when(endPoint.getIdentifier()).thenReturn(DBEERPEDIA.DOC_ENDPOINT);
+
+    // Act
+    getRequestHandler.apply(containerRequestContext);
+  }
+
+  @Test
+  public void apply_ConfigurationException_WhenUnsupportedRequestDynamicEndPoint() {
+    // Arrange
+    thrown.expect(ConfigurationException.class);
+    thrown.expectMessage(String.format("Result type %s not supported for endpoint %s",
+        HttpMethod.PUT, DBEERPEDIA.DOC_ENDPOINT));
+
+    UriInfo uriInfo = mock(UriInfo.class);
+    MultivaluedMap<String, String> parameterValues = mock(MultivaluedMap.class);
+    when(containerRequestContext.getUriInfo()).thenReturn(uriInfo);
+    when(containerRequestContext.getUriInfo().getPathParameters()).thenReturn(parameterValues);
+    when(uriInfo.getPath()).thenReturn("/");
+    when(containerRequestContext.getRequest()).thenReturn(mock(Request.class));
+    when(containerRequestContext.getRequest().getMethod()).thenReturn(HttpMethod.PUT);
+    DynamicEndPoint dynamicEndPoint = mock(DynamicEndPoint.class);
+    when(dynamicEndPoint.getIdentifier()).thenReturn(DBEERPEDIA.DOC_ENDPOINT);
+    EndPointRequestHandler requestHandler = new EndPointRequestHandler(dynamicEndPoint,
+        endPointRequestParameterMapper, representationResourceProvider);
+
+    // Act
+    requestHandler.apply(containerRequestContext);
   }
 
   @Test
