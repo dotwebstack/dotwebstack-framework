91457d0f5059c648fec997e3f604c4aaab857c6a
NathanAtKadaster <32732634+NathanAtKadaster@users.noreply.github.com>
2018-03-30 12:08:53 +0200

    [GRID-2863] Complete BooleanSchemaMapper (#138)
    * GRID-2863: Add Handler for LDPath
    
    * GRID-2863: Add Handler for LDPath
    
    * GRID-2863: Add tests for `CONSTANT_VALUE` vendor extension
    
    * GRID-2863: Add Implementation for Constant_Value
    
    * GRID-2863: Prepping for major refactor of SchemaMappers
    
    * GRID-2863: Fix BSMConstantValueTest
    
    * Review
    
    * Refactor BooleanSchemaMapper tests
    
    * GRID-2863: Add Implementation for Constant_Value
    
    * Clean up BSM tests
    
    * Fix Checkstyle violations
    
    * Fix lexographical import order
    Refactor Property to Schema
    
    * refactor Property to Schema and GraphEntity to Entity

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/entity/schema/BooleanSchemaMapper.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/entity/schema/BooleanSchemaMapper.java
@@ -1,14 +1,27 @@
 package org.dotwebstack.framework.frontend.openapi.entity.schema;
 
+import com.google.common.collect.ImmutableSet;
 import io.swagger.models.properties.BooleanProperty;
 import io.swagger.models.properties.Property;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import lombok.NonNull;
+import org.dotwebstack.framework.frontend.openapi.OpenApiSpecificationExtensions;
 import org.dotwebstack.framework.frontend.openapi.entity.GraphEntity;
+import org.dotwebstack.framework.frontend.openapi.entity.LdPathExecutor;
 import org.dotwebstack.framework.frontend.openapi.entity.TupleEntity;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.vocabulary.XMLSchema;
 import org.springframework.stereotype.Service;
 
 @Service
-class BooleanSchemaMapper implements SchemaMapper<BooleanProperty, Boolean> {
+class BooleanSchemaMapper extends AbstractSchemaMapper<BooleanProperty, Boolean> {
+
+  private static final Set<IRI> SUPPORTED_TYPES = ImmutableSet.of(XMLSchema.BOOLEAN);
+  private static final Set<String> SUPPORTED_VENDOR_EXTENSIONS = ImmutableSet.of(
+      OpenApiSpecificationExtensions.LDPATH, OpenApiSpecificationExtensions.CONSTANT_VALUE);
 
   @Override
   public Boolean mapTupleValue(@NonNull BooleanProperty schema, @NonNull TupleEntity entity,
@@ -19,12 +32,69 @@ class BooleanSchemaMapper implements SchemaMapper<BooleanProperty, Boolean> {
   @Override
   public Boolean mapGraphValue(@NonNull BooleanProperty schema, @NonNull GraphEntity entity,
       @NonNull ValueContext valueContext, @NonNull SchemaMapperAdapter schemaMapperAdapter) {
+    validateVendorExtensions(schema, SUPPORTED_VENDOR_EXTENSIONS);
+    Map<String, Object> vendorExtensions = schema.getVendorExtensions();
+
+    if (vendorExtensions.containsKey(OpenApiSpecificationExtensions.LDPATH)) {
+      LdPathExecutor ldPathExecutor = entity.getLdPathExecutor();
+      return handleLdPathVendorExtension(schema, valueContext.getValue(), ldPathExecutor);
+    }
+
+    if (vendorExtensions.containsKey(OpenApiSpecificationExtensions.CONSTANT_VALUE)) {
+      return handleConstantValueVendorExtension(schema);
+    }
+
+    // TODO: line below should never be reached. Return null instead?
     return SchemaMapperUtils.castLiteralValue(valueContext.getValue()).booleanValue();
+  }
+
+  @SuppressWarnings("squid:S2447")
+  private Boolean handleLdPathVendorExtension(BooleanProperty property, Value context,
+      LdPathExecutor ldPathExecutor) {
+    String vendorExtension = OpenApiSpecificationExtensions.LDPATH;
+    String ldPathQuery = (String) property.getVendorExtensions().get(vendorExtension);
+
+    if (ldPathQuery == null) {
+      handleRequired(property, vendorExtension);
+      return null;
+    }
+
+    /* at this point we're sure that ld-path is not null */
+    Collection<Value> queryResult = ldPathExecutor.ldPathQuery(context, ldPathQuery);
+    if (!property.getRequired() && queryResult.isEmpty()) {
+      return null;
+    }
+
+    return SchemaMapperUtils.castLiteralValue(
+        getSingleStatement(queryResult, ldPathQuery)).booleanValue();
+  }
+
+  @SuppressWarnings("squid:S2447")
+  private Boolean handleConstantValueVendorExtension(BooleanProperty property) {
+    String vendorExtension = OpenApiSpecificationExtensions.CONSTANT_VALUE;
+    Object value = property.getVendorExtensions().get(vendorExtension);
+
+    if (value != null) {
+      if (isSupportedLiteral(value)) {
+        return SchemaMapperUtils.castLiteralValue((Value) value).booleanValue();
+      }
+
+      return Boolean.valueOf(value.toString());
+    }
+
+    handleRequired(property, vendorExtension);
+
+    return null;
   }
 
   @Override
   public boolean supports(@NonNull Property schema) {
     return schema instanceof BooleanProperty;
+  }
+
+  @Override
+  protected Set<IRI> getSupportedDataTypes() {
+    return SUPPORTED_TYPES;
   }
 
 }
--- frontend/openapi/src/test/java/org/dotwebstack/framework/frontend/openapi/entity/schema/BooleanSchemaMapperTest.java
+++ frontend/openapi/src/test/java/org/dotwebstack/framework/frontend/openapi/entity/schema/BooleanSchemaMapperTest.java
@@ -1,10 +1,15 @@
 package org.dotwebstack.framework.frontend.openapi.entity.schema;
 
+import static org.dotwebstack.framework.frontend.openapi.OpenApiSpecificationExtensions.CONSTANT_VALUE;
+import static org.dotwebstack.framework.frontend.openapi.OpenApiSpecificationExtensions.LDPATH;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import io.swagger.models.properties.BooleanProperty;
 import io.swagger.models.properties.StringProperty;
+import java.util.Collections;
+import org.dotwebstack.framework.frontend.openapi.OpenApiSpecificationExtensions;
+import org.dotwebstack.framework.frontend.openapi.entity.GraphEntity;
 import org.dotwebstack.framework.frontend.openapi.entity.TupleEntity;
 import org.dotwebstack.framework.test.DBEERPEDIA;
 import org.junit.Before;
@@ -19,7 +24,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class BooleanSchemaMapperTest {
 
   @Rule
-  public final ExpectedException thrown = ExpectedException.none();
+  public final ExpectedException exception = ExpectedException.none();
 
   @Mock
   private TupleEntity tupleEntityMock;
@@ -28,17 +33,22 @@ public class BooleanSchemaMapperTest {
 
   private BooleanProperty schema;
 
+  @Mock
+  private GraphEntity entitymock;
+  private SchemaMapperAdapter schemaMapperAdapter;
+
   @Before
   public void setUp() {
     schemaMapper = new BooleanSchemaMapper();
     schema = new BooleanProperty();
+    schemaMapperAdapter = new SchemaMapperAdapter(Collections.singletonList(schemaMapper));
   }
 
   @Test
   public void mapTupleValue_ThrowsException_ForNonLiterals() {
     // Assert
-    thrown.expect(SchemaMapperRuntimeException.class);
-    thrown.expectMessage("Value is not a literal value.");
+    exception.expect(SchemaMapperRuntimeException.class);
+    exception.expectMessage("Value is not a literal value.");
 
     // Arrange & Act
     schemaMapper.mapTupleValue(schema, tupleEntityMock,
@@ -46,7 +56,7 @@ public class BooleanSchemaMapperTest {
   }
 
   @Test
-  public void mapTupleValue_ReturnValue_ForLiterals() {
+  public void mapTupleValue_ReturnsValue_ForLiterals() {
     // Arrange & Act
     Boolean result = schemaMapper.mapTupleValue(schema, tupleEntityMock,
         ValueContext.builder().value(DBEERPEDIA.BROUWTOREN_CRAFT_MEMBER).build());
@@ -71,6 +81,23 @@ public class BooleanSchemaMapperTest {
 
     // Assert
     assertThat(supported, equalTo(false));
+  }
+
+  @Test
+  public void mapGraphValue_ThrowsEx_MultipleVendorExtensions() {
+    // Arrange
+    schema.vendorExtension(OpenApiSpecificationExtensions.LDPATH, "ld-Path");
+    schema.vendorExtension(CONSTANT_VALUE, "true");
+    final ValueContext context = ValueContext.builder().build();
+
+    // Assert
+    exception.expect(SchemaMapperRuntimeException.class);
+    exception.expectMessage("BooleanProperty ");
+    exception.expectMessage(CONSTANT_VALUE);
+    exception.expectMessage(LDPATH);
+
+    // Act
+    schemaMapper.mapGraphValue(schema, entitymock, context, schemaMapperAdapter);
   }
 
 }
