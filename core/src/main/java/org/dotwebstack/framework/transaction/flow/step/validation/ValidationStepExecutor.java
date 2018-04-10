3d4f6335563f6ca3b7ade9ac7ff39c70be6c3169
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-04-10 11:50:22 +0200

    Remove file config. backend and backend request

--- core/src/main/java/org/dotwebstack/framework/transaction/flow/step/validation/ValidationStepExecutor.java
+++ core/src/main/java/org/dotwebstack/framework/transaction/flow/step/validation/ValidationStepExecutor.java
@@ -1,50 +1,30 @@
-package org.dotwebstack.framework.backend.sparql.validationstep;
+package org.dotwebstack.framework.transaction.flow.step.validation;
 
 import java.util.Collection;
 import java.util.Map;
 import lombok.NonNull;
-import org.dotwebstack.framework.backend.BackendException;
-import org.dotwebstack.framework.config.FileConfigurationBackend;
 import org.dotwebstack.framework.param.Parameter;
 import org.dotwebstack.framework.transaction.flow.step.AbstractStepExecutor;
-import org.dotwebstack.framework.transaction.flow.step.validation.ValidationStep;
 import org.dotwebstack.framework.validation.ShaclValidationException;
 import org.dotwebstack.framework.validation.ShaclValidator;
 import org.dotwebstack.framework.validation.ValidationReport;
-import org.eclipse.rdf4j.RDF4JException;
 import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.query.QueryResults;
 
 public class ValidationStepExecutor extends AbstractStepExecutor<ValidationStep> {
-
-  private FileConfigurationBackend backend;
 
   private Model transactionModel;
 
   public ValidationStepExecutor(@NonNull ValidationStep validationStep,
-      @NonNull Model transactionModel, @NonNull FileConfigurationBackend backend) {
+      @NonNull Model transactionModel) {
     super(validationStep);
-    this.backend = backend;
     this.transactionModel = transactionModel;
   }
 
   @Override
   public void execute(Collection<Parameter> parameters, Map<String, String> parameterValues) {
-    final Model validationModel;
-
-    try {
-      validationModel =
-          QueryResults.asModel(backend.getRepository().getConnection().getStatements(null, null,
-              null, step.getConformsTo()));
-    } catch (RDF4JException ex) {
-      throw new BackendException(
-          String.format("Could not read configuration at graph {%s} as model {%s}",
-              step.getConformsTo(), ex.getMessage()),
-          ex);
-    }
-
     final ShaclValidator shaclValidator = new ShaclValidator();
-    final ValidationReport report = shaclValidator.validate(transactionModel, validationModel);
+    final ValidationReport report =
+        shaclValidator.validate(transactionModel, step.getValidationModel());
     if (!report.isValid()) {
       throw new ShaclValidationException(report.printReport());
     }
