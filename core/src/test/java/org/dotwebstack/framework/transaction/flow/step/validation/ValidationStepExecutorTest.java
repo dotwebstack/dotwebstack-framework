858cca3702633165a511bcdef2365179eb1b5cad
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-04-10 12:59:31 +0200

    Remove unused code

--- core/src/test/java/org/dotwebstack/framework/transaction/flow/step/validation/ValidationStepExecutorTest.java
+++ core/src/test/java/org/dotwebstack/framework/transaction/flow/step/validation/ValidationStepExecutorTest.java
@@ -1,16 +1,16 @@
 package org.dotwebstack.framework.transaction.flow.step.validation;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.withSettings;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import org.dotwebstack.framework.backend.BackendResourceProvider;
 import org.dotwebstack.framework.config.FileConfigurationBackend;
 import org.dotwebstack.framework.param.Parameter;
 import org.dotwebstack.framework.validation.RdfModelTransformer;
@@ -20,7 +20,6 @@ import org.dotwebstack.framework.vocabulary.ELMO;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
-import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,7 +32,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.ResourcePatternResolver;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ValidationStepExecutorTest {
@@ -54,9 +52,6 @@ public class ValidationStepExecutorTest {
 
   @Mock
   private ShaclValidator shaclValidator;
-
-  @Mock
-  private BackendResourceProvider backendResourceProvider;
 
   @Mock
   private Resource validDataResource;
@@ -87,41 +82,25 @@ public class ValidationStepExecutorTest {
 
   @Before
   public void setUp() throws IOException {
-    repository = new SailRepository(new MemoryStore());
     transactionModel = new LinkedHashModel();
-    resourceLoader =
-        mock(ResourceLoader.class, withSettings().extraInterfaces(ResourcePatternResolver.class));
     elmoConfigurationResource = mock(Resource.class);
-    // elmoShapesResource = new ClassPathResource("/model/elmo-shapes.trig");
     shaclValidator = mock(ShaclValidator.class);
-    // when(shapesResource.getFile()).thenReturn(
-    // new ClassPathResource("/shaclvalidation/model/shapes.trig").getFile());
     when(shapesResource.getInputStream()).thenReturn(new InputStreamResource(
         new ClassPathResource("/shaclvalidation/shapes.trig").getInputStream()).getInputStream());
-    when(shapesResource.getFilename()).thenReturn("shapes.trig");
 
-    // when(validDataResource.getFile()).thenReturn(
-    // new ClassPathResource("/shaclvalidation/model/validData.trig").getFile());
     when(validDataResource.getInputStream()).thenReturn(
         new InputStreamResource(new ClassPathResource(
             "/shaclvalidation/validData.trig").getInputStream()).getInputStream());
     when(validDataResource.getFilename()).thenReturn("validData.trig");
-
-    // when(invalidDataResource.getFile()).thenReturn(
-    // new ClassPathResource("/shaclvalidation/model/invalidData.trig").getFile());
     when(invalidDataResource.getInputStream()).thenReturn(
         new InputStreamResource(new ClassPathResource(
             "/shaclvalidation/invalidData.trig").getInputStream()).getInputStream());
     when(invalidDataResource.getFilename()).thenReturn("invalidData.trig");
 
-    // when(elmoShapesResource.getFile()).thenReturn(
-    // new ClassPathResource("/shaclvalidation/model/elmo-shapes.trig").getFile());
     when(elmoShapesResource.getInputStream()).thenReturn(new InputStreamResource(
         new ClassPathResource("/model/elmo-shapes.trig").getInputStream()).getInputStream());
     when(elmoShapesResource.getFilename()).thenReturn("elmo-shapes.trig");
 
-    // when(elmoConfigurationResource.getFile()).thenReturn(
-    // new ClassPathResource("/shaclvalidation/model/elmo.trig").getFile());
     when(elmoConfigurationResource.getInputStream()).thenReturn(new InputStreamResource(
         new ClassPathResource("/model/elmo.trig").getInputStream()).getInputStream());
     when(elmoConfigurationResource.getFilename()).thenReturn("elmo.trig");
@@ -129,12 +108,6 @@ public class ValidationStepExecutorTest {
     report = mock(ValidationReport.class);
     when(report.isValid()).thenReturn(true);
     when(shaclValidator.validate(any(), (Model) any())).thenReturn(report);
-
-    Resource[] projectResources = new Resource[] {elmoShapesResource, shapesResource,
-        validDataResource, invalidDataResource, elmoConfigurationResource};
-    when(((ResourcePatternResolver) resourceLoader).getResources(any())).thenReturn(
-        projectResources);
-
   }
 
   @Test
@@ -154,20 +127,40 @@ public class ValidationStepExecutorTest {
     // Arrange
     transactionModel = RdfModelTransformer.getModel(validDataResource.getInputStream());
     final Model validationModel = RdfModelTransformer.getModel(shapesResource.getInputStream());
-    System.out.println("validation model output --> " + validationModel.toString());
-
-    backend = new FileConfigurationBackend(elmoConfigurationResource, repository,
-        "/shaclvalidation", elmoShapesResource, shaclValidator);
-    backend.setResourceLoader(resourceLoader);
-    backend.setEnvironment(environment);
 
     validationStep = new ValidationStep.Builder(ELMO.VALIDATION_STEP, validationModel).conformsTo(
         ELMO.SHACL_CONCEPT_GRAPHNAME).build();
     validationStepExecutor = new ValidationStepExecutor(validationStep, transactionModel);
-    backend.loadResources();
 
     // Act
     validationStepExecutor.execute(parameters, parameterValues);
+  }
+
+  @Test
+  public void validate_getShaclValidationReport_invalidConfiguration() throws Exception {
+    //
+    transactionModel = RdfModelTransformer.getModel(validDataResource.getInputStream());
+    final Model validationModel = RdfModelTransformer.getModel(shapesResource.getInputStream());
+
+    validationStep = new ValidationStep.Builder(ELMO.VALIDATION_STEP, validationModel).conformsTo(
+        ELMO.SHACL_CONCEPT_GRAPHNAME).build();
+    validationStepExecutor = new ValidationStepExecutor(validationStep, transactionModel);
+
+    // Act
+    validationStepExecutor.execute(parameters, parameterValues);
+
+    // Act
+    final ValidationReport report =
+        shaclValidator.validate(RdfModelTransformer.getModel(invalidDataResource.getInputStream()),
+            RdfModelTransformer.getModel(shapesResource.getInputStream()));
+
+    // Assert
+    assertThat(report.isValid(), equalTo(false));
+    assertThat(report.getErrors().size(), equalTo(1));
+    final String errorKey = report.getErrors().keySet().iterator().next();
+    assertThat(report.getErrors().get(errorKey).getReport(),
+        equalTo("Invalid configuration at path [http://example.org#lid] on node "
+            + "[http://example.org#HuwelijkMarcoNanda] with error message [More than 2 values]"));
   }
 
 }
