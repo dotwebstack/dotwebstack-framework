15b21554d0dc4004889dded33b65b7aa03a46f17
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-04-10 11:52:25 +0200

    Remove file config. backend and change create step method

--- core/src/main/java/org/dotwebstack/framework/transaction/flow/step/validation/ValidationStepFactory.java
+++ core/src/main/java/org/dotwebstack/framework/transaction/flow/step/validation/ValidationStepFactory.java
@@ -2,31 +2,29 @@ package org.dotwebstack.framework.transaction.flow.step.validation;
 
 import java.util.Optional;
 import lombok.NonNull;
-import org.dotwebstack.framework.backend.BackendResourceProvider;
+import org.dotwebstack.framework.backend.BackendException;
 import org.dotwebstack.framework.config.ConfigurationBackend;
-import org.dotwebstack.framework.config.FileConfigurationBackend;
+import org.dotwebstack.framework.config.ConfigurationException;
 import org.dotwebstack.framework.transaction.flow.step.Step;
 import org.dotwebstack.framework.transaction.flow.step.StepFactory;
 import org.dotwebstack.framework.vocabulary.ELMO;
+import org.eclipse.rdf4j.RDF4JException;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.util.Models;
 import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.QueryResults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ValidationStepFactory implements StepFactory {
 
-  private BackendResourceProvider backendResourceProvider;
-
   private ConfigurationBackend configurationBackend;
 
   @Autowired
-  public ValidationStepFactory(@NonNull BackendResourceProvider backendResourceProvider,
-      @NonNull ConfigurationBackend configurationBackend) {
-    this.backendResourceProvider = backendResourceProvider;
+  public ValidationStepFactory(@NonNull ConfigurationBackend configurationBackend) {
     this.configurationBackend = configurationBackend;
   }
 
@@ -37,13 +35,24 @@ public class ValidationStepFactory implements StepFactory {
 
   @Override
   public Step create(Model stepModel, Resource identifier) {
-    ValidationStep.Builder builder =
-        new ValidationStep.Builder(identifier, backendResourceProvider);
+    final IRI conformsTo = getObjectIRI(stepModel, identifier, ELMO.CONFORMS_TO_PROP).orElseThrow(
+        () -> new ConfigurationException(
+            String.format("No <%s> statement has been found for validation step <%s>.",
+                ELMO.CONFORMS_TO_PROP, identifier)));
+    final Model validationModel;
+    try {
+      validationModel =
+          QueryResults.asModel(configurationBackend.getRepository().getConnection().getStatements(
+              null, null, null, conformsTo));
+    } catch (RDF4JException ex) {
+      throw new BackendException(
+          String.format("Could not read configuration at graph {%s} as model {%s}", conformsTo,
+              ex.getMessage()),
+          ex);
+    }
+    ValidationStep.Builder builder = new ValidationStep.Builder(identifier, validationModel);
+    builder.conformsTo(conformsTo);
     getObjectString(stepModel, identifier, RDFS.LABEL).ifPresent(builder::label);
-    getObjectIRI(stepModel, identifier, ELMO.CONFORMS_TO_PROP).ifPresent(builder::conformsTo);
-    getObjectIRI(stepModel, identifier, ELMO.BACKEND_PROP).ifPresent(
-        iri -> builder.backend(backendResourceProvider.get(iri)));
-    builder.fileConfigurationBackend((FileConfigurationBackend) configurationBackend);
 
     return builder.build();
   }
