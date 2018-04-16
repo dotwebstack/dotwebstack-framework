182a0e5b4ade07dc820f5912b5ef22a96c31f9ff
topper <robin.topper@kadaster.nl>
2018-04-16 17:03:41 +0200

    Refactor HostPreMatchingRequestFilter and RequestUriParameterSource

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/parameter/source/RequestUriParameterSource.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/parameter/source/RequestUriParameterSource.java
@@ -1,8 +1,8 @@
 package org.dotwebstack.framework.frontend.ld.parameter.source;
 
-import com.google.common.net.HttpHeaders;
 import java.net.URI;
 import javax.ws.rs.container.ContainerRequestContext;
+import org.dotwebstack.framework.frontend.http.HostPreMatchingRequestFilter;
 import org.dotwebstack.framework.vocabulary.HTTP;
 import org.eclipse.rdf4j.model.IRI;
 
@@ -15,15 +15,13 @@ public class RequestUriParameterSource implements ParameterSource {
   @Override
   public String getValue(ContainerRequestContext containerRequestContext) {
     URI uri = containerRequestContext.getUriInfo().getAbsolutePath();
-    String forwardedHost =
-        containerRequestContext.getHeaderString(HttpHeaders.X_FORWARDED_HOST).split(",")[0];
-    String toBeRemoved =
-        forwardedHost == null || forwardedHost.equals("") ? uri.getHost() : forwardedHost;
+    String addedDomain =
+        (String) containerRequestContext.getProperty(HostPreMatchingRequestFilter.ADDED_DOMAIN);
 
     /*
      * Remove first 'domain' part of path that we have added in HostPreMatchingRequestFilter
      */
-    String path = uri.getPath().replaceAll("^/" + toBeRemoved, "");
+    String path = uri.getPath().replaceAll("^/" + addedDomain, "");
 
     return String.format("%s://%s%s", uri.getScheme(), uri.getHost(), path);
   }
