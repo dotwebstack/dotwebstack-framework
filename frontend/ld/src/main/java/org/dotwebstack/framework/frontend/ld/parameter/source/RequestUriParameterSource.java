14361493d35d31c057323c3a4ef1d933a58378c3
RobinTopper <robin.topper@transfer-solutions.com>
2018-04-17 09:39:11 +0200

    GRID-2231: Refactor RequestUriParameterSource and HostPreMatchingRequestFilter (#145)
    * Use X_FORWARDED_HOST header in RequestUriParameterSource
    
    * Cover situation where X_FORWARDED_HOST not set
    
    * Refactor HostPreMatchingRequestFilter and RequestUriParameterSource
    
    * Let unittest test expected behaviour

--- frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/parameter/source/RequestUriParameterSource.java
+++ frontend/ld/src/main/java/org/dotwebstack/framework/frontend/ld/parameter/source/RequestUriParameterSource.java
@@ -2,6 +2,7 @@ package org.dotwebstack.framework.frontend.ld.parameter.source;
 
 import java.net.URI;
 import javax.ws.rs.container.ContainerRequestContext;
+import org.dotwebstack.framework.frontend.http.HostPreMatchingRequestFilter;
 import org.dotwebstack.framework.vocabulary.HTTP;
 import org.eclipse.rdf4j.model.IRI;
 
@@ -11,13 +12,16 @@ public class RequestUriParameterSource implements ParameterSource {
     return HTTP.REQUEST_URI;
   }
 
+  @Override
   public String getValue(ContainerRequestContext containerRequestContext) {
     URI uri = containerRequestContext.getUriInfo().getAbsolutePath();
+    String addedDomain =
+        (String) containerRequestContext.getProperty(HostPreMatchingRequestFilter.ADDED_DOMAIN);
 
     /*
      * Remove first 'domain' part of path that we have added in HostPreMatchingRequestFilter
      */
-    String path = uri.getPath().replaceAll("^/" + uri.getHost(), "");
+    String path = uri.getPath().replaceAll("^/" + addedDomain, "");
 
     return String.format("%s://%s%s", uri.getScheme(), uri.getHost(), path);
   }
