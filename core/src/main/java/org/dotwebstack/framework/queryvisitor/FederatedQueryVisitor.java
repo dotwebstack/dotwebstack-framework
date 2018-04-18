d21dd8da50dd87ac0bca1981c01de3708fb79f5e
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-04-18 15:33:16 +0200

    Add backend to federatedqueryvisitor

--- core/src/main/java/org/dotwebstack/framework/queryvisitor/FederatedQueryVisitor.java
+++ core/src/main/java/org/dotwebstack/framework/queryvisitor/FederatedQueryVisitor.java
@@ -1,17 +1,33 @@
 package org.dotwebstack.framework.queryvisitor;
 
+import java.util.Optional;
+import lombok.NonNull;
+import org.dotwebstack.framework.backend.Backend;
+import org.dotwebstack.framework.backend.BackendResourceProvider;
+import org.dotwebstack.framework.config.ConfigurationException;
+import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.query.algebra.Service;
 import org.eclipse.rdf4j.query.algebra.helpers.AbstractQueryModelVisitor;
 
 public class FederatedQueryVisitor extends AbstractQueryModelVisitor {
 
+  private BackendResourceProvider backendResourceProvider;
+
+  private Backend backend;
+
+  public FederatedQueryVisitor(@NonNull BackendResourceProvider backendResourceProvider) {
+    this.backendResourceProvider = backendResourceProvider;
+  }
+
+  public Backend getBackend() {
+    return backend;
+  }
+
   @Override
   public void meet(Service service) {
-    // first impl. just show value content
-    System.out.println("***");
-    System.out.println("Service endpoint [name] " + service.getServiceRef().getName());
-    System.out.println("Service endpoint [value] " + service.getServiceRef().getValue());
-    System.out.println("***");
+    final Resource backendIir = (Resource) service.getServiceRef().getValue();
+    backend = Optional.of(backendResourceProvider.get(backendIir)).orElseThrow(
+        () -> new ConfigurationException(String.format("Backend {%s} not found.", backendIir)));
   }
 
 }
