2cb203a4862440b3af2ae983f452bf8f0d5d6205
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-04-23 13:01:34 +0200

    Add test for an assertion step with service tag

--- core/src/test/java/org/dotwebstack/framework/transaction/flow/step/assertion/AssertionStepFactoryTest.java
+++ core/src/test/java/org/dotwebstack/framework/transaction/flow/step/assertion/AssertionStepFactoryTest.java
@@ -4,10 +4,16 @@ import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableBiMap;
+import org.dotwebstack.framework.backend.Backend;
 import org.dotwebstack.framework.backend.BackendResourceProvider;
 import org.dotwebstack.framework.test.DBEERPEDIA;
 import org.dotwebstack.framework.vocabulary.ELMO;
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
@@ -36,6 +42,12 @@ public class AssertionStepFactoryTest {
   public void setup() {
     // Arrange
     assertionStepFactory = new AssertionStepFactory(backendResourceProvider);
+    when(backendResourceProvider.getAll()).thenReturn(ImmutableBiMap.of());
+    Backend backend = mock(Backend.class);
+    when(backend.getEndpoint()).thenReturn(mock(Literal.class));
+    when(backend.getEndpoint().stringValue()).thenReturn("http://localhost:8080/sparql");
+    when(backendResourceProvider.get(any())).thenReturn(backend);
+
   }
 
   @Test
@@ -68,6 +80,27 @@ public class AssertionStepFactoryTest {
     assertThat(assertionStep.getIdentifier(), equalTo(DBEERPEDIA.ASSERTION_IF_EXIST_STEP));
     assertThat(assertionStep.getLabel(), equalTo(DBEERPEDIA.BREWERIES_LABEL.stringValue()));
     assertThat(assertionStep.getAssertionQuery(), equalTo(DBEERPEDIA.ASK_ALL_QUERY.stringValue()));
+  }
+
+  @Test
+  public void create_CreateAssertionStep_WithValidDataAndServiceTag() {
+    // Arrange
+    final Literal transformedQuery = valueFactory.createLiteral(
+        "PREFIX dbeerpedia: <http://dbeerpedia.org#> ASK WHERE { ?s ?p ?o SERVICE <http://localhost:8080/sparql> { ?s rdfs:label ?p } }");
+
+    Model stepModel = new LinkedHashModel();
+    stepModel.add(valueFactory.createStatement(DBEERPEDIA.ASSERTION_IF_EXIST_STEP, RDF.TYPE,
+        ELMO.ASSERTION_STEP));
+    stepModel.add(valueFactory.createStatement(DBEERPEDIA.ASSERTION_IF_EXIST_STEP, RDFS.LABEL,
+        DBEERPEDIA.BREWERIES_LABEL));
+    stepModel.add(valueFactory.createStatement(DBEERPEDIA.ASSERTION_IF_EXIST_STEP, ELMO.ASSERT_NOT,
+        DBEERPEDIA.ASK_ALL_QUERY_SERVICE_TAG));
+
+    // Act
+    assertionStep = assertionStepFactory.create(stepModel, DBEERPEDIA.ASSERTION_IF_EXIST_STEP);
+
+    // Assert
+    assertThat(assertionStep.getAssertionQuery(), equalTo(transformedQuery.stringValue()));
   }
 
 }
