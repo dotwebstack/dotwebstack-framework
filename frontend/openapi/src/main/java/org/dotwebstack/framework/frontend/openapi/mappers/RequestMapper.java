ebcd17c34151957990143b5ec654454235ee194b
Jonas Koperdraat <jonas.koperdraat@kadaster.nl>
2018-06-19 10:39:25 +0200

    GRID-2666 Update RequestMapper interface

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/mappers/RequestMapper.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/mappers/RequestMapper.java
@@ -4,13 +4,15 @@ import com.atlassian.oai.validator.model.ApiOperation;
 import io.swagger.models.Operation;
 import io.swagger.models.Swagger;
 import java.util.Map;
+
+import io.swagger.v3.oas.models.OpenAPI;
 import org.glassfish.jersey.server.model.Resource;
 
 public interface RequestMapper {
 
   Boolean supportsVendorExtension(Map<String, Object> vendorExtensions);
 
-  void map(Resource.Builder resourceBuilder, Swagger swagger, ApiOperation apiOperation,
-      Operation getOperation, String absolutePath);
+  void map(Resource.Builder resourceBuilder, OpenAPI openAPI, ApiOperation apiOperation,
+           Operation getOperation, String absolutePath);
 
 }
