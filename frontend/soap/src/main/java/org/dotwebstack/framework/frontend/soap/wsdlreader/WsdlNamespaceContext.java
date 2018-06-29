9fa3e2e3c8ccd487b2502ca3d963a9fcd60a0b77
architolk <msj@xs4all.nl>
2018-06-29 21:19:11 +0200

    Added implementation class - need to add something to checkstyle

---
+++ frontend/soap/src/main/java/org/dotwebstack/framework/frontend/soap/wsdlreader/WsdlNamespaceContext.java
@@ -0,0 +1,35 @@
+package org.dotwebstack.framework.frontend.soap.wsdlreader;
+
+import java.util.Iterator;
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+
+class WsdlNamespaceContext implements NamespaceContext {
+
+  private static final String XS_NS_URI = "http://www.w3.org/2001/XMLSchema";
+  private static final String DWS_NS_URI = "http://dotwebstack.org/wsdl-extension/";
+
+  @SuppressWarnings("AbbreviationAsWordInName")
+  public String getNamespaceURI(String prefix) {
+    if (prefix == null) {
+      throw new NullPointerException("Null prefix");
+    }
+    switch (prefix) {
+      case "xs": return WsdlNamespaceContext.XS_NS_URI;
+      case "dws": return WsdlNamespaceContext.DWS_NS_URI;
+      case "xml": return XMLConstants.XML_NS_URI;
+      default: return XMLConstants.NULL_NS_URI;
+    }
+  }
+
+  // This method isn't necessary for XPath processing.
+  public String getPrefix(String uri) {
+    throw new UnsupportedOperationException();
+  }
+
+  // This method isn't necessary for XPath processing either.
+  public Iterator getPrefixes(String uri) {
+    throw new UnsupportedOperationException();
+  }
+
+}
