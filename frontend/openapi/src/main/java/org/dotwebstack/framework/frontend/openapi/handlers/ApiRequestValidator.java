65bc268ce47b189c2598078ec3c622bcadb05e77
Joost Farla <joostfarla@users.noreply.github.com>
2018-07-17 11:27:10 +0200

    [GRID-3511] Ignore non-DWS parameters during request validation (#168)
    * Do not validate parameters that don't have the x-dws-parameter extension
    
    * Fix unit test
    
    * Process review Joost

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/handlers/ApiRequestValidator.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/handlers/ApiRequestValidator.java
@@ -8,6 +8,7 @@ import com.atlassian.oai.validator.model.SimpleRequest.Builder;
 import com.atlassian.oai.validator.report.ValidationReport;
 import com.google.common.collect.ImmutableList;
 import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +22,7 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.UriInfo;
 import lombok.NonNull;
 import org.dotwebstack.framework.frontend.http.error.InvalidParamsBadRequestException.InvalidParameter;
+import org.dotwebstack.framework.frontend.openapi.OpenApiSpecificationExtensions;
 
 class ApiRequestValidator {
   private static final List<String> FILTERED_HEADERS = ImmutableList.of("accept", "content-type");
@@ -53,7 +55,7 @@ class ApiRequestValidator {
 
     String strMethod = requestContext.getMethod();
     Method method = Method.valueOf(strMethod.toUpperCase());
-    Builder builder = new SimpleRequest.Builder(method, requestContext.getUriInfo().getPath());
+    Builder builder = new SimpleRequest.Builder(method, uriInfo.getPath());
 
     requestContext.getHeaders().entrySet().stream().filter(
         entry -> !FILTERED_HEADERS.contains(entry.getKey().toLowerCase())).forEach(
@@ -68,10 +70,23 @@ class ApiRequestValidator {
     builder.withBody(body);
 
     /*
+     * The OAS can contain parameters that are there just for documentation purposes. Therefore
+     * parameters that are not used in DWS (don't have the x-dotwebstack-parameter extension) but
+     * are in the OAS, do not need to be validated.
+     */
+    ApiOperation apiOperationCopy = new ApiOperation(apiOperation.getApiPath(),
+        apiOperation.getRequestPath(), apiOperation.getMethod(), apiOperation.getOperation());
+
+    List<Parameter> params = apiOperationCopy.getOperation().getParameters().stream().filter(
+        param -> param.getVendorExtensions().containsKey(
+            OpenApiSpecificationExtensions.PARAMETER)).collect(Collectors.toList());
+    apiOperationCopy.getOperation().setParameters(params);
+
+    /*
      * NB: Swagger API 2 does not have an option for case insensitivity for enums; for example, you
      * may expect validation to fail for 'POint' (if the enum is defined as 'Point')
      */
-    ValidationReport report = requestValidator.validateRequest(builder.build(), apiOperation);
+    ValidationReport report = requestValidator.validateRequest(builder.build(), apiOperationCopy);
 
     if (report.hasErrors()) {
       throw createException(report);
