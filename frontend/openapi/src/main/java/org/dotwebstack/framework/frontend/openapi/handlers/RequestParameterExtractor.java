ad0d8e6bf354916441d0fc1f40fe0bc7d303d8b8
Nathan van Dalen <nathan.vandalen@kadaster.nl>
2018-09-13 10:29:59 +0200

    GCO-785: Add URI encoding for Path and Query params

--- frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/handlers/RequestParameterExtractor.java
+++ frontend/openapi/src/main/java/org/dotwebstack/framework/frontend/openapi/handlers/RequestParameterExtractor.java
@@ -1,7 +1,10 @@
 package org.dotwebstack.framework.frontend.openapi.handlers;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import com.atlassian.oai.validator.model.ApiOperation;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.models.Model;
 import io.swagger.models.ModelImpl;
 import io.swagger.models.RefModel;
 import io.swagger.models.Swagger;
@@ -11,13 +14,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.net.URLEncoder;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 import lombok.NonNull;
 import org.apache.commons.io.IOUtils;
@@ -30,9 +39,8 @@ import org.springframework.stereotype.Service;
 class RequestParameterExtractor {
 
   private static final Logger LOG = LoggerFactory.getLogger(RequestParameterExtractor.class);
+  private static final String ENCODING = "UTF-8";
 
-  static final String PARAM_GEOMETRY_QUERYTYPE = "geometry_querytype";
-  static final String PARAM_GEOMETRY = "geometry";
   static final String PARAM_PAGE_NUM = "page";
   static final String PARAM_PAGE_SIZE = "size";
 
@@ -43,74 +51,100 @@ class RequestParameterExtractor {
   }
 
   RequestParameters extract(@NonNull ApiOperation apiOperation, @NonNull Swagger swagger,
-      @NonNull ContainerRequestContext containerRequestContext) {
+                            @NonNull ContainerRequestContext containerRequestContext) {
 
-    UriInfo uriInfo = containerRequestContext.getUriInfo();
-
-    RequestParameters parameters = new RequestParameters();
-
-    parameters.putAll(uriInfo.getPathParameters());
-    parameters.putAll(uriInfo.getQueryParameters());
-    parameters.putAll(containerRequestContext.getHeaders());
+    boolean properBodyParamExists = //
+        apiOperation //
+            .getOperation() //
+            .getParameters() //
+            .stream() //
+            .anyMatch(isBodyParameter(swagger)) //
+            && containerRequestContext //
+            .getHeaders() //
+            .get(HttpHeaders.CONTENT_TYPE) //
+            .stream() //
+            .anyMatch(header -> ContentType.APPLICATION_JSON.toString().startsWith(header)) //
+            && containerRequestContext.hasEntity(); //
 
     try {
-      Optional<Parameter> parameter =
-          apiOperation.getOperation().getParameters().stream().filter(parameterBody -> {
-            if ((parameterBody instanceof BodyParameter)) {
-              ModelImpl parameterModel = getBodyParameter(swagger, (BodyParameter) parameterBody);
-              return "object".equalsIgnoreCase(parameterModel.getType())
-                  && "body".equalsIgnoreCase(parameterBody.getIn());
-            }
-            return false;
-          }).findFirst();
-      extractBodyParameter(parameters, containerRequestContext, parameter);
+      return getParameters(containerRequestContext, properBodyParamExists);
     } catch (IOException ioe) {
       throw new InternalServerErrorException("Error processing request body.", ioe);
     }
+  }
+
+  private Predicate<Parameter> isBodyParameter(final Swagger swagger) {
+    return parameterBody -> {
+
+      if (parameterBody instanceof BodyParameter
+          && parameterBody.getIn().equalsIgnoreCase("body")) {
+
+        Model bodyModel = ((BodyParameter) parameterBody).getSchema();
+
+        if (bodyModel instanceof ModelImpl) {
+          return ((ModelImpl) bodyModel).getType().equalsIgnoreCase("object");
+        }
+
+        if (bodyModel instanceof RefModel) {
+          return ((ModelImpl) swagger.getDefinitions().get(
+              ((RefModel) bodyModel).getSimpleRef())).getType().equalsIgnoreCase("object");
+        }
+      }
+      return false;
+    };
+  }
+
+  private RequestParameters getParameters(ContainerRequestContext containerRequestContext,
+                                          boolean properBodyParamExists) throws IOException {
+
+    RequestParameters parameters = new RequestParameters();
+    String body = extractBody(containerRequestContext);
+    parameters.setRawBody(body);
+    UriInfo uriInfo = containerRequestContext.getUriInfo();
+
+    MultivaluedMap<String, String> headers = containerRequestContext.getHeaders();
+    MultivaluedMap<String, String> pathParameters = uriInfo.getPathParameters();
+    MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+    MultivaluedMap<String, String> bodyParameters =
+        !isBlank(body) && properBodyParamExists ? getBodyParameters(body)
+            : new MultivaluedHashMap<>();
+
+    pathParameters.entrySet().forEach(this::uriEncodeToList);
+    queryParameters.entrySet().forEach(this::uriEncodeToList);
+
+    parameters.putAll(headers);
+    parameters.putAll(queryParameters);
+    parameters.putAll(pathParameters);
+    parameters.putAll(bodyParameters);
 
     LOG.info("Extracted parameters: {}", parameters);
-
     return parameters;
   }
 
-  private static ModelImpl getBodyParameter(@NonNull Swagger swagger, BodyParameter parameterBody) {
-    ModelImpl parameterModel = null;
-    if (parameterBody.getSchema() instanceof ModelImpl) {
-      parameterModel = ((ModelImpl) (parameterBody.getSchema()));
+  private MultivaluedMap<String, String> getBodyParameters(String rawBody) throws IOException {
+    MultivaluedMap<String, String> bodyParameters = new MultivaluedHashMap<>();
+    Map<String, Object> json = objectMapper.readValue(rawBody, Map.class);
+    for (Map.Entry<String, Object> entry : json.entrySet()) {
+      bodyParameters.add(entry.getKey(), objectMapper.writeValueAsString(entry.getValue()));
     }
-    if (parameterBody.getSchema() instanceof RefModel) {
-      RefModel refModel = ((RefModel) (parameterBody.getSchema()));
-      parameterModel = (ModelImpl) swagger.getDefinitions().get(refModel.getSimpleRef());
-    }
-    return parameterModel;
+    return bodyParameters;
   }
 
-  /**
-   * Extracts the body from the supplied request.
-   */
-  private void extractBodyParameter(final RequestParameters requestParameters,
-      final ContainerRequestContext ctx, final Optional<Parameter> parameter) throws IOException {
+  void uriEncodeToList(Map.Entry<String, List<String>> entry) {
+    entry.setValue( //
+        entry.getValue() //
+            .stream() //
+            .map(this::urlEncode) //
+            .collect(Collectors.toList()));
+  }
 
-    String body = extractBody(ctx);
-    if (body == null) {
-      return;
+  private String urlEncode(String url) {
+    try {
+      return URLEncoder.encode(url, ENCODING);
+    } catch (UnsupportedEncodingException uee) {
+      LOG.error("encoding failed {}", uee.getMessage());
     }
-    requestParameters.setRawBody(body);
-    if (!parameter.isPresent()) {
-      return;
-    }
-
-    if (ctx.getHeaders().get(HttpHeaders.CONTENT_TYPE).stream() //
-        .map(String::toLowerCase) //
-        .noneMatch(header -> header.startsWith(ContentType.APPLICATION_JSON.getMimeType()))) {
-      return;
-    }
-
-    Map<String, Object> json = objectMapper.readValue(body, Map.class);
-
-    for (Map.Entry<String, Object> entry : json.entrySet()) {
-      requestParameters.put(entry.getKey(), objectMapper.writeValueAsString(entry.getValue()));
-    }
+    return url;
   }
 
   /**
@@ -126,40 +160,18 @@ class RequestParameterExtractor {
   @SuppressWarnings("resource")
   @Nullable
   private static String extractBody(ContainerRequestContext ctx) throws IOException {
-    /*
-     * do not use auto-closable construction as it is responsibility of JAX-RS to close the
-     * underlying stream
-     */
-    IsEmptyCheckInputStream inputStream = new IsEmptyCheckInputStream(ctx.getEntityStream());
 
-    if (inputStream.isEmpty()) {
+    if (!ctx.hasEntity()) {
       return null;
     }
 
+    /*
+     * do not use auto-closable construction as it is responsibility of JAX-RS to close the
+     * underlying stream
+     * still copy from PushbackStream, not from the underlying stream
+     */
     StringWriter writer = new StringWriter();
-    /* still copy from PushbackStream, not from the underlying stream */
-    IOUtils.copy(inputStream, writer);
+    IOUtils.copy(new PushbackInputStream(ctx.getEntityStream()), writer);
     return writer.toString();
   }
-
-  private static final class IsEmptyCheckInputStream extends PushbackInputStream {
-
-    private IsEmptyCheckInputStream(InputStream input) {
-      super(input);
-    }
-
-    private boolean isEmpty() throws IOException {
-      byte[] buf = new byte[1];
-      int result = read(buf, 0, 1);
-
-      if (result > 0) {
-        unread(buf[0]);
-        return false;
-      }
-
-      return true;
-    }
-
-  }
 }
-
