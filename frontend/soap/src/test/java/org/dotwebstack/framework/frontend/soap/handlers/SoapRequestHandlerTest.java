6404309354c08c23b41c5e4ac944540d2d253edd
Ordina2014 <Rohita.jagessar@ordina.nl>
2018-11-06 15:13:22 +0100

    WIP: SoapRequestHandlerTest.java

--- frontend/soap/src/test/java/org/dotwebstack/framework/frontend/soap/handlers/SoapRequestHandlerTest.java
+++ frontend/soap/src/test/java/org/dotwebstack/framework/frontend/soap/handlers/SoapRequestHandlerTest.java
@@ -1,5 +1,27 @@
 package org.dotwebstack.framework.frontend.soap.handlers;
 
-//@RunWith(MockitoJUnitRunner.class)
-public class SoapRequestHandlerTest {  
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.container.ContainerRequestContext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SoapRequestHandlerTest { 
+	
+	@Mock
+	ContainerRequestContext context;
+	
+	//service mocken?
+	
+	@Test
+	public void testApply(){
+		String soapActionName = "SOAP_ACTION";
+		
+		assertEquals((context.getHeaderString("SOAP_ACTION")),soapActionName);
+	}
+
 }
