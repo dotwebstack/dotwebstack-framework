12abe70c8cd979cac67f7d897ea72258d7854b0e
Joost Farla <joost@apiwise.nl>
2019-03-25 13:50:07 +0100

    Increase test coverage

--- backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/query/BindingSetFetcherTest.java
+++ backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/query/BindingSetFetcherTest.java
@@ -13,6 +13,7 @@ import static org.mockito.Mockito.when;
 import graphql.Scalars;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLFieldDefinition;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.impl.EmptyBindingSet;
 import org.eclipse.rdf4j.query.impl.MapBindingSet;
@@ -73,6 +74,24 @@ class BindingSetFetcherTest {
 
     // Assert
     assertThat(result, is(BUILDING_BUILT_AT_EXAMPLE));
+  }
+
+  @Test
+  void get_returnsString_forIriProperty() {
+    // Arrange
+    GraphQLFieldDefinition fieldDefinition = GraphQLFieldDefinition.newFieldDefinition()
+        .name(BUILDING_IDENTIFIER_NAME)
+        .type(Scalars.GraphQLID)
+        .build();
+    MapBindingSet bindingSet = new MapBindingSet();
+    bindingSet.addBinding(BUILDING_IDENTIFIER_NAME, RDF.SUBJECT);
+    DataFetchingEnvironment environment = createEnvironment(fieldDefinition, bindingSet);
+
+    // Act
+    Object result = bindingSetFetcher.get(environment);
+
+    // Assert
+    assertThat(result, is(RDF.SUBJECT.stringValue()));
   }
 
   private static DataFetchingEnvironment createEnvironment(GraphQLFieldDefinition fieldDefinition,
