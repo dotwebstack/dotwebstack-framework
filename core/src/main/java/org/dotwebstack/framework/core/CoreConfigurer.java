d3b551ddbbed0ef4af84b294e0cc6c7249e43fc2
Marco Simons <marco.simons@kadaster.nl>
2019-05-07 07:23:14 +0200

    IHR2-2051: Use methods for each directive definition.

--- core/src/main/java/org/dotwebstack/framework/core/CoreConfigurer.java
+++ core/src/main/java/org/dotwebstack/framework/core/CoreConfigurer.java
@@ -1,6 +1,8 @@
 package org.dotwebstack.framework.core;
 
 import static graphql.Scalars.GraphQLString;
+import static graphql.language.NonNullType.newNonNullType;
+import static graphql.language.TypeName.newTypeName;
 
 import graphql.introspection.Introspection;
 import graphql.language.DirectiveDefinition;
@@ -27,55 +29,62 @@ public class CoreConfigurer implements GraphqlConfigurer {
   private final TransformDirectiveWiring transformDirectiveWiring;
   private final ConstraintDirectiveWiring constraintDirectiveWiring;
 
+  private static final TypeName optionalString = newTypeName(GraphQLString.getName()).build();
+  private static final NonNullType requiredString = newNonNullType(optionalString).build();
+
   @Override
   public void configureTypeDefinitionRegistry(@NonNull TypeDefinitionRegistry registry) {
     registry.add(new ScalarTypeDefinition(CoreScalars.DATE.getName()));
     registry.add(new ScalarTypeDefinition(CoreScalars.DATETIME.getName()));
 
-    TypeName optionalString = TypeName.newTypeName(GraphQLString.getName()).build();
-    NonNullType requiredString = NonNullType.newNonNullType(optionalString).build();
+    registry.add(createTransformDefinition());
+    registry.add(createConstraintDefinition());
+  }
 
-    registry.add(DirectiveDefinition.newDirectiveDefinition()
-        .name(CoreDirectives.TRANSFORM_NAME)
-        .inputValueDefinition(InputValueDefinition.newInputValueDefinition()
-            .name(CoreDirectives.TRANSFORM_ARG_EXPR)
-            .type(requiredString)
-            .build())
-        .directiveLocation(DirectiveLocation.newDirectiveLocation()
-            .name(Introspection.DirectiveLocation.FIELD_DEFINITION.name())
-            .build())
-        .build());
+  private DirectiveDefinition createTransformDefinition() {
+    return DirectiveDefinition.newDirectiveDefinition()
+            .name(CoreDirectives.TRANSFORM_NAME)
+            .inputValueDefinition(InputValueDefinition.newInputValueDefinition()
+                    .name(CoreDirectives.TRANSFORM_ARG_EXPR)
+                    .type(requiredString)
+                    .build())
+            .directiveLocation(DirectiveLocation.newDirectiveLocation()
+                    .name(Introspection.DirectiveLocation.FIELD_DEFINITION.name())
+                    .build())
+            .build();
+  }
 
-    registry.add(DirectiveDefinition.newDirectiveDefinition()
-        .name(CoreDirectives.CONSTRAINT_NAME)
-        .inputValueDefinitions(
-            List.of(
-                InputValueDefinition.newInputValueDefinition()
-                .name(CoreDirectives.CONSTRAINT_ARG_MIN)
-                .type(optionalString)
-                .build(),
-                InputValueDefinition.newInputValueDefinition()
-                .name(CoreDirectives.CONSTRAINT_ARG_MAX)
-                .type(optionalString)
-                .build(),
-                InputValueDefinition.newInputValueDefinition()
-                .name(CoreDirectives.CONSTRAINT_ARG_ONEOF)
-                .type(optionalString)
-                .build()))
-        .directiveLocation(
-            DirectiveLocation.newDirectiveLocation()
-            .name(Introspection.DirectiveLocation.FIELD_DEFINITION.name())
-            .build())
-        .build());
+  private DirectiveDefinition createConstraintDefinition() {
+    return DirectiveDefinition.newDirectiveDefinition()
+            .name(CoreDirectives.CONSTRAINT_NAME)
+            .inputValueDefinitions(
+                    List.of(
+                            InputValueDefinition.newInputValueDefinition()
+                                    .name(CoreDirectives.CONSTRAINT_ARG_MIN)
+                                    .type(optionalString)
+                                    .build(),
+                            InputValueDefinition.newInputValueDefinition()
+                                    .name(CoreDirectives.CONSTRAINT_ARG_MAX)
+                                    .type(optionalString)
+                                    .build(),
+                            InputValueDefinition.newInputValueDefinition()
+                                    .name(CoreDirectives.CONSTRAINT_ARG_ONEOF)
+                                    .type(optionalString)
+                                    .build()))
+            .directiveLocation(
+                    DirectiveLocation.newDirectiveLocation()
+                            .name(Introspection.DirectiveLocation.FIELD_DEFINITION.name())
+                            .build())
+            .build();
   }
 
   @Override
   public void configureRuntimeWiring(@NonNull Builder builder) {
     builder
-    .scalar(CoreScalars.DATE)
-    .scalar(CoreScalars.DATETIME)
-    .directive(CoreDirectives.TRANSFORM_NAME, transformDirectiveWiring)
-    .directive(CoreDirectives.CONSTRAINT_NAME, constraintDirectiveWiring);
+      .scalar(CoreScalars.DATE)
+      .scalar(CoreScalars.DATETIME)
+      .directive(CoreDirectives.TRANSFORM_NAME, transformDirectiveWiring)
+      .directive(CoreDirectives.CONSTRAINT_NAME, constraintDirectiveWiring);
   }
 
 }
