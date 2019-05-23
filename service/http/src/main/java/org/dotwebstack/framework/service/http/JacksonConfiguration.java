aed9c0f69fc5db7b6439c64db5650de2f7f4eb45
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2019-05-23 16:14:00 +0200

    IHR2-2061 rebase and formatting

--- service/http/src/main/java/org/dotwebstack/framework/service/http/JacksonConfiguration.java
+++ service/http/src/main/java/org/dotwebstack/framework/service/http/JacksonConfiguration.java
@@ -14,8 +14,8 @@ public class JacksonConfiguration {
   public Jackson2ObjectMapperBuilder objectMapperBuilder(List<Module> modules) {
     Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
     builder.featuresToEnable(SerializationFeature.INDENT_OUTPUT) //
-    .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //
-    .modules(modules);
+        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //
+        .modules(modules);
     return builder;
   }
 }
