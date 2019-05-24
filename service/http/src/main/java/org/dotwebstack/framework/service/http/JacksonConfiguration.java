d5159d13738ea9cf4c86d989b34aad91e613e453
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2019-05-24 09:04:20 +0200

    IHR2-2061 apply formatting rules

--- service/http/src/main/java/org/dotwebstack/framework/service/http/JacksonConfiguration.java
+++ service/http/src/main/java/org/dotwebstack/framework/service/http/JacksonConfiguration.java
@@ -13,8 +13,9 @@ public class JacksonConfiguration {
   @Bean
   public Jackson2ObjectMapperBuilder objectMapperBuilder(List<Module> modules) {
     Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-    builder.featuresToEnable(SerializationFeature.INDENT_OUTPUT) //
-        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //
+    builder
+        .featuresToEnable(SerializationFeature.INDENT_OUTPUT)
+        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         .modules(modules);
     return builder;
   }
