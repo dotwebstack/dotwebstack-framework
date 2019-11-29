29ae3a581915c543f216e7f80067f2dcd898d010
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2019-11-29 11:42:56 +0100

    KDP-5719 run spotless

--- backend/rdf4j/src/main/java/org/dotwebstack/framework/backend/rdf4j/query/context/VerticeHelper.java
+++ backend/rdf4j/src/main/java/org/dotwebstack/framework/backend/rdf4j/query/context/VerticeHelper.java
@@ -90,8 +90,8 @@ public class VerticeHelper {
     graphPattern.and(childPatterns.toArray(new GraphPattern[0]));
 
     if (!isLeaf(edge) && addSelectClause) {
-        graphPattern = GraphPatterns.select(getSelected(subject, subject, edge).toArray(new Projectable[] {}))
-            .where(graphPattern);
+      graphPattern = GraphPatterns.select(getSelected(subject, subject, edge).toArray(new Projectable[] {}))
+          .where(graphPattern);
     }
     return singletonList(graphPattern);
   }
