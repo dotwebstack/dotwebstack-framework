853fb52c7e6acd6ae5d979128a71511e9b0840b0
Marco Simons <marco.simons@kadaster.nl>
2019-12-10 15:06:00 +0100

    KDP-5481 - Allow multiple @resource directives per type

--- backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/Rdf4jIntegrationTest.java
+++ backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/Rdf4jIntegrationTest.java
@@ -782,7 +782,8 @@ class Rdf4jIntegrationTest {
   @Test
   void graphQlQuery_returnsBreweries_FilterOnAddressSubject() {
     // Arrange
-    String query = "{breweries{address(subject : \"https://github.com/dotwebstack/beer/id/address/1\") { subject, resource }}}";
+    String query =
+        "{breweries{address(subject : \"https://github.com/dotwebstack/beer/id/address/1\") { subject, resource }}}";
 
     // Act
     ExecutionResult result = graphQL.execute(query);
