078ef2c0f0dd1baf82b4e595fcc86884ccaf3b5f
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2019-12-17 17:15:18 +0100

    IHR2-3508 fix nonselected field in query

---
+++ backend/rdf4j/src/main/java/org/dotwebstack/framework/backend/rdf4j/query/DataFetchingFilterFieldSelectionSet.java
@@ -0,0 +1,4 @@
+package org.dotwebstack.framework.backend.rdf4j;
+
+public class DataFetchingFilterFieldSelectionSet {
+}
---
+++ backend/rdf4j/src/main/java/org/dotwebstack/framework/backend/rdf4j/query/FilteredField.java
@@ -0,0 +1,4 @@
+package org.dotwebstack.framework.backend.rdf4j.query;
+
+public class FilteredField {
+}
