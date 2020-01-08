fde5dc3299e2bad6fc50a0b08ebdfabd94d75b57
klaijb <bastiaan.klaij@kadaster.nl>
2020-01-08 11:14:18 +0100

    [KDP-6088] - test sonar mvn change

--- service/openapi/src/test/java/org/dotwebstack/framework/service/openapi/helper/GraphQlValueHelperTest.java
+++ service/openapi/src/test/java/org/dotwebstack/framework/service/openapi/helper/GraphQlValueHelperTest.java
@@ -13,9 +13,8 @@ import org.junit.jupiter.api.Test;
 public class GraphQlValueHelperTest {
 
   @Test
-  public void getStringValue_returnsValue() {
+  public void getStringValue_returnsValue_forSupportedTypes() {
     // Arrange / Act / Assert
-
     assertEquals("1", GraphQlValueHelper.getStringValue(new IntValue(BigInteger.ONE)));
     assertEquals("1", GraphQlValueHelper.getStringValue(new StringValue("1")));
     assertEquals("1", GraphQlValueHelper.getStringValue(new FloatValue(BigDecimal.ONE)));
