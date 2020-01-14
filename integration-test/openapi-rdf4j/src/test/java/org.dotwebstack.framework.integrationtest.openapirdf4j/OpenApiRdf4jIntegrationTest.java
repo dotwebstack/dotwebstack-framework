6da71f5a5654112cbaaefaacb1bdb6a9b57bcb06
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2020-01-14 09:06:10 +0100

    5792 add testcases and jsonmapper

--- integration-test/openapi-rdf4j/src/test/java/org.dotwebstack.framework.integrationtest.openapirdf4j/OpenApiRdf4jIntegrationTest.java
+++ integration-test/openapi-rdf4j/src/test/java/org.dotwebstack.framework.integrationtest.openapirdf4j/OpenApiRdf4jIntegrationTest.java
@@ -1,17 +1,35 @@
 package org.dotwebstack.framework.integrationtest.openapirdf4j;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import graphql.ExecutionResult;
+import io.swagger.v3.core.util.Json;
 import org.dotwebstack.framework.test.TestApplication;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertTrue;
+import java.io.File;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureWebTestClient
@@ -20,6 +38,44 @@ public class OpenApiRdf4jIntegrationTest {
 
   @Autowired
   private WebTestClient webClient;
+
+  private ObjectMapper mapper = new ObjectMapper();
+
+  @Test
+  void graphqlQuery_ReturnsMap_ForObjectQueryField() throws IOException {
+    // Arrange & Act
+    String result = webClient.get()
+        .uri("/brewery/123")
+        .exchange()
+        .expectBody(String.class)
+        .returnResult()
+        .getResponseBody();
+
+    JsonNode expectedObj = mapper.readTree(getClass().getResourceAsStream("/results/brewery_123.json"));
+    JsonNode actualObj = mapper.readTree(result);
+
+    // Assert
+    assertEquals(expectedObj, actualObj);
+  }
+
+  @Test
+  void graphqlQuery_ReturnsMap_ForNestedListNonNullQuery() throws IOException {
+    //String query = "{ beer(identifier: \"6\") { identifier, beerTypesRaw { name }  }}";
+
+    // Arrange & Act
+    String result = webClient.get()
+        .uri("/beers/6?expand=beerTypesRaw")
+        .exchange()
+        .expectBody(String.class)
+        .returnResult()
+        .getResponseBody();
+
+    JsonNode expectedObj = mapper.readTree(getClass().getResourceAsStream("/results/beer_6.json"));
+    JsonNode actualObj = mapper.readTree(result);
+
+    // Assert
+    assertEquals(expectedObj, actualObj);
+  }
 
   @Test
   public void openApiQuery_404_forUnknownUri() {
