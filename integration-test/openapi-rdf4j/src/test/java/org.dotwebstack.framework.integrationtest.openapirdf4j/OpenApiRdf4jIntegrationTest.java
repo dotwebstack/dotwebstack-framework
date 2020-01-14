b5a25b661bd329d52bd7823a7ff17316b4968a11
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2020-01-14 16:47:24 +0100

    KDP-5792 add test cases for response headers

--- integration-test/openapi-rdf4j/src/test/java/org.dotwebstack.framework.integrationtest.openapirdf4j/OpenApiRdf4jIntegrationTest.java
+++ integration-test/openapi-rdf4j/src/test/java/org.dotwebstack.framework.integrationtest.openapirdf4j/OpenApiRdf4jIntegrationTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
@@ -22,7 +23,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @ExtendWith(SpringExtension.class)
 @AutoConfigureWebTestClient
 @SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class OpenApiRdf4jIntegrationTest {
+class OpenApiRdf4jIntegrationTest {
 
   @Autowired
   private WebTestClient webClient;
@@ -173,109 +174,6 @@ public class OpenApiRdf4jIntegrationTest {
   }
 
   @Test
-  public void openApiRequest_200_forProvidedPathParameter() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/breweries")
-        .exchange()
-        .expectStatus()
-        .isOk();
-  }
-
-  @Test
-  public void openApiRequest_200_forProvidedRequiredParameter() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/breweries?expand=beers")
-        .exchange()
-        .expectStatus()
-        .isOk();
-  }
-
-  @Test
-  public void openApiRequest_200_forProvidedHeaderParameter() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/breweries")
-        .header("sort", "name")
-        .exchange()
-        .expectStatus()
-        .isOk();
-  }
-
-  @Test
-  public void openApiRequest_404_forUnprovidedRequiredParameter() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/brewery_with_subject")
-        .exchange()
-        .expectStatus()
-        .isBadRequest();
-  }
-
-  @Test
-  public void openApiRequest_404_forUnknownUri() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/unknown")
-        .exchange()
-        .expectStatus()
-        .isNotFound();
-  }
-
-  @Test
-  public void openApiRequest_404_forUnknownUriWithParameter() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/unknown?nonexistent=nonexistent")
-        .exchange()
-        .expectStatus()
-        .isNotFound();
-  }
-
-  @Test
-  public void openApiRequest_returnsBadRequest_forNonexistentParam() {
-    // Arrange & Act & Assert
-    this.webClient.get()
-        .uri("/breweries?nonexistent=nonexistent")
-        .exchange()
-        .expectStatus()
-        .isBadRequest();
-  }
-  
-  @Test
-  public void openApiRequest_404_forUnknownOperation() {
-    // Arrange & Act & Assert
-    this.webClient.post()
-        .uri("/breweries")
-        .exchange()
-        .expectStatus()
-        .isNotFound();
-  }
-
-  @Test
-  public void openApiRequest_returnsOpenApiSpec_forApi() {
-    // Arrange & Act
-    FluxExchangeResult<String> result = this.webClient.get()
-        .uri("/")
-        .exchange()
-        .expectStatus()
-        .isOk()
-        .returnResult(String.class);
-
-    StringBuilder builder = new StringBuilder();
-    result.getResponseBody()
-        .toStream()
-        .forEach(builder::append);
-    String openApiSpec = builder.toString();
-
-    // Assert
-    assertTrue(openApiSpec.contains("/breweries:"));
-    assertFalse(openApiSpec.contains("x-dws-expr"));
-    assertFalse(openApiSpec.contains("x-dws-envelope: true"));
-  }
-
-  @Test
   void openApiRequest_ReturnsBreweries_WithTransformedAggregate() throws IOException {
     // Arrange & Act
     String result = this.webClient.get()
@@ -341,13 +239,7 @@ public class OpenApiRdf4jIntegrationTest {
     assertResult(result, "/results/breweries_filter_on_addressSubjectNested.json");
   }
 
-  private void assertResult(String result, String jsonResultPath) throws IOException {
-    JsonNode expectedObj = mapper.readTree(getClass().getResourceAsStream(jsonResultPath));
-    JsonNode actualObj = mapper.readTree(result);
 
-    // Assert
-    assertEquals(expectedObj, actualObj);
-  }
 
   @Test
   void openApiRequest_ReturnsConfigurationException_ForInvalidPageSize() {
@@ -369,5 +261,144 @@ public class OpenApiRdf4jIntegrationTest {
         .is5xxServerError()
         .expectBody(String.class)
         .value(containsString("Constraint 'min' [1] violated on 'page' with value '0'"));
+  }
+
+  @Test
+  void openApiRequest_200_forProvidedPathParameter() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/breweries")
+        .exchange()
+        .expectStatus()
+        .isOk();
+  }
+
+  @Test
+  void openApiRequest_200_forProvidedRequiredParameter() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/breweries?expand=beers")
+        .exchange()
+        .expectStatus()
+        .isOk();
+  }
+
+  @Test
+  void openApiRequest_200_forProvidedHeaderParameter() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/breweries")
+        .header("sort", "name")
+        .exchange()
+        .expectStatus()
+        .isOk();
+  }
+
+  @Test
+  void openApiRequest_404_forUnprovidedRequiredParameter() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/brewery_with_subject")
+        .exchange()
+        .expectStatus()
+        .isBadRequest();
+  }
+
+  @Test
+  void openApiRequest_404_forUnknownUri() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/unknown")
+        .exchange()
+        .expectStatus()
+        .isNotFound();
+  }
+
+  @Test
+  void openApiRequest_404_forUnknownUriWithParameter() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/unknown?nonexistent=nonexistent")
+        .exchange()
+        .expectStatus()
+        .isNotFound();
+  }
+
+  @Test
+  void openApiRequest_returnsBadRequest_forNonexistentParam() {
+    // Arrange & Act & Assert
+    this.webClient.get()
+        .uri("/breweries?nonexistent=nonexistent")
+        .exchange()
+        .expectStatus()
+        .isBadRequest();
+  }
+  
+  @Test
+  void openApiRequest_404_forUnknownOperation() {
+    // Arrange & Act & Assert
+    this.webClient.post()
+        .uri("/breweries")
+        .exchange()
+        .expectStatus()
+        .isNotFound();
+  }
+
+  @Test
+  void openApiRequest_returnsOpenApiSpec_forApi() {
+    // Arrange & Act
+    FluxExchangeResult<String> result = this.webClient.get()
+        .uri("/")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .returnResult(String.class);
+
+    StringBuilder builder = new StringBuilder();
+    result.getResponseBody()
+        .toStream()
+        .forEach(builder::append);
+    String openApiSpec = builder.toString();
+
+    // Assert
+    assertTrue(openApiSpec.contains("/breweries:"));
+    assertFalse(openApiSpec.contains("x-dws-expr"));
+    assertFalse(openApiSpec.contains("x-dws-envelope: true"));
+  }
+
+  @Test
+  void openApiRequest_returnsDefaultXPaginationResponseHeader_whenNoParamsAreProvided() {
+    FluxExchangeResult<String> result = this.webClient.get()
+        .uri("/breweries")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .returnResult(String.class);
+
+    HttpHeaders responseHeaders = result.getResponseHeaders();
+    assertEquals("1", responseHeaders.get("X-Pagination-Page").get(0));
+    assertEquals("10", responseHeaders.get("X-Pagination-Limit").get(0));
+  }
+
+  @Test
+  void openApiRequest_returnsXPaginationResponseHeader_whenParamsAreProvided() {
+    FluxExchangeResult<String> result = this.webClient.get()
+        .uri("/breweries?page=2&pageSize=2")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .returnResult(String.class);
+
+    HttpHeaders responseHeaders = result.getResponseHeaders();
+    assertEquals("2", responseHeaders.get("X-Pagination-Page").get(0));
+    assertEquals("2", responseHeaders.get("X-Pagination-Limit").get(0));
+  }
+
+  private void assertResult(String result, String jsonResultPath) throws IOException {
+    JsonNode expectedObj = mapper.readTree(getClass().getResourceAsStream(jsonResultPath));
+    JsonNode actualObj = mapper.readTree(result);
+
+    // Assert
+    assertEquals(expectedObj, actualObj);
   }
 }
