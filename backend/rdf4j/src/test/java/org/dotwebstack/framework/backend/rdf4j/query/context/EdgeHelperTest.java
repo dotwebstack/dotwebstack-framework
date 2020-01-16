c6eb13f1bb131e7d38ef3ad0b03930b025c95556
klaijb <bastiaan.klaij@kadaster.nl>
2020-01-16 15:17:38 +0100

    [IHR2-5792] - add unit test

--- backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/query/context/EdgeHelperTest.java
+++ backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/query/context/EdgeHelperTest.java
@@ -1,7 +1,10 @@
 package org.dotwebstack.framework.backend.rdf4j.query.context;
 
-import static graphql.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import org.dotwebstack.framework.backend.rdf4j.shacl.NodeShape;
@@ -11,14 +14,16 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.sparqlbuilder.rdf.Iri;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
-class EdgeHelperTest {
+@ExtendWith(MockitoExtension.class)
+public class EdgeHelperTest {
 
   private static final ValueFactory VF = SimpleValueFactory.getInstance();
 
   @Test
-  void testHasEqualTargetClass_returnsTrue_withNullNodeShape() {
+  void hasEqualTargetClass_returnsTrue_withNullNodeShape() {
     Iri iri = () -> "<http://www.example.com#testType>";
 
     Edge edge = Edge.builder()
@@ -39,5 +44,33 @@ class EdgeHelperTest {
 
     assertTrue(EdgeHelper.hasEqualTargetClass(edge, shape));
   }
+
+  @Test
+  void hasEqualTargetClass_returnsFalse_forChildWithoutSameType() {
+    Edge edge = Edge.builder()
+        .object(Vertice.builder()
+            .edges(List.of(Edge.builder()
+                .predicate(() -> "<" + RDF.TYPE.stringValue() + ">")
+                .object(Vertice.builder()
+                    .iris(Collections.emptySet())
+                    .build())
+                .build()))
+            .build())
+        .build();
+    PropertyShape shape = PropertyShape.builder()
+        .node(NodeShape.builder()
+            .targetClasses(Set.of(VF.createIRI("http://www.example.com#testType")))
+            .build())
+        .build();
+
+    assertFalse(EdgeHelper.hasEqualTargetClass(edge, shape));
+  }
+
+  @Test
+  public void hasEqualTargetClass_returnsTrue_forNullPropertyShape() {
+    // Arrange / Act / Assert
+    assertTrue(EdgeHelper.hasEqualTargetClass(null, mock(PropertyShape.class)));
+  }
+
 
 }
