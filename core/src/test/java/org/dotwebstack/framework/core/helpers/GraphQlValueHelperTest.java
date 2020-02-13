075b9381f19f46b89d8f1b9f2b6e83ee926e5c34
Jouri Fledderman <jouri.fledderman@kadaster.nl>
2020-02-13 10:51:49 +0100

    KDP-6400 add test for now case

--- core/src/test/java/org/dotwebstack/framework/core/helpers/GraphQlValueHelperTest.java
+++ core/src/test/java/org/dotwebstack/framework/core/helpers/GraphQlValueHelperTest.java
@@ -1,6 +1,8 @@
 package org.dotwebstack.framework.core.helpers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import graphql.language.BooleanValue;
 import graphql.language.FloatValue;
@@ -10,6 +12,9 @@ import graphql.language.ObjectValue;
 import graphql.language.StringValue;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDate;
+
+import graphql.language.TypeName;
 import org.junit.jupiter.api.Test;
 
 public class GraphQlValueHelperTest {
@@ -84,5 +89,34 @@ public class GraphQlValueHelperTest {
 
     // Assert
     assertEquals("ObjectValue{objectFields=[ObjectField{name='field', value=StringValue{value='test'}}]}", actual);
+  }
+
+  @Test
+  public void getValue_returnsString_forStringValue() {
+    // Arrange
+    StringValue value = StringValue.newStringValue("string value")
+        .build();
+
+    // Act
+    String actual = (String) GraphQlValueHelper.getValue(TypeName.newTypeName("String").build(), value);
+
+    // Assert
+    assertEquals("string value", actual);
+  }
+
+  @Test
+  public void getValue_returnsDate_forNowStringValue() {
+    // Arrange
+    LocalDate localDateBeforeTest = LocalDate.now();
+    StringValue value = StringValue.newStringValue("NOW")
+        .build();
+
+    // Act
+    Object actual = GraphQlValueHelper.getValue(TypeName.newTypeName("Date").build(), value);
+
+    // Assert
+    assertEquals("LocalDate", actual.getClass().getSimpleName());
+    assertEquals(localDateBeforeTest, actual);
+    assertEquals(LocalDate.now(), actual);
   }
 }
