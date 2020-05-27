5294e2a443e4f1aa438ce7b425d937b1571660e6
Max Broens <max.broens@rabobank.nl>
2020-05-27 16:04:35 +0200

    [KDP-7303] codesmells

---
+++ service/openapi/src/test/java/org/dotwebstack/framework/service/openapi/OpenApiSchemaConfigurationTest.java
@@ -0,0 +1,27 @@
+package org.dotwebstack.framework.service.openapi;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+
+public class OpenApiSchemaConfigurationTest {
+
+  @Test
+  void test_openApiStream() throws FileNotFoundException {
+    OpenApiSchemaConfiguration openApiSchemaConfiguration = new OpenApiSchemaConfiguration();
+    InputStream stream = openApiSchemaConfiguration.openApiStream();
+
+    assertNotNull(stream);
+  }
+
+  @Test
+  void test_openApi() {
+    OpenApiSchemaConfiguration openApiSchemaConfiguration = new OpenApiSchemaConfiguration();
+    OpenAPI openApi = openApiSchemaConfiguration.openApi();
+
+    assertNotNull(openApi);
+  }
+}
