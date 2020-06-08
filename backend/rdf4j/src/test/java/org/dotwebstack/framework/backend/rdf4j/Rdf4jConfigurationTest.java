91169f0615c088ccb6ebfaab87cebd886e0e4902
Marco Simons <marco.simons@kadaster.nl>
2020-06-08 12:16:54 +0200

    Fix resource resolving

--- backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/Rdf4jConfigurationTest.java
+++ backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/Rdf4jConfigurationTest.java
@@ -206,19 +206,4 @@ class Rdf4jConfigurationTest {
     // Assert
     assertThat(nodeShapeRegistry.get(Constants.BREWERY_SHAPE), is(notNullValue()));
   }
-
-  @Test
-  void return_EmptyQueryReferenceRegistry_When_SparqlResourceFolder_DoesntExists() throws IOException {
-    // Arrange
-    Resource sparqlResource = mock(Resource.class);
-    when(sparqlResource.exists()).thenReturn(false);
-    when(resourceLoader.getResource(anyString())).thenReturn(sparqlResource);
-
-    // Act
-    Map<String, String> result = rdf4jConfiguration.queryReferenceRegistry(resourceLoader);
-
-    // Assert
-    assertThat(result.size(), is(0));
-  }
-
 }
