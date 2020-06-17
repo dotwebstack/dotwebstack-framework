49f27b525da28f39d63e45fd2db8f131686b6246
Robert-Jan Regout <robert-jan.regout@kadaster.nl>
2020-06-17 09:17:35 +0200

    @NonNull has no effect is call to super is first call.

--- backend/rdf4j/src/main/java/org/dotwebstack/framework/backend/rdf4j/mapping/ResponseMapperException.java
+++ backend/rdf4j/src/main/java/org/dotwebstack/framework/backend/rdf4j/mapping/ResponseMapperException.java
@@ -1,13 +1,12 @@
 package org.dotwebstack.framework.backend.rdf4j.mapping;
 
-import lombok.NonNull;
 import org.dotwebstack.framework.core.DotWebStackRuntimeException;
 
 class ResponseMapperException extends DotWebStackRuntimeException {
 
   static final long serialVersionUID = 1563735990022L;
 
-  ResponseMapperException(@NonNull String message, Object... arguments) {
+  ResponseMapperException(String message, Object... arguments) {
     super(message, arguments);
   }
 }
