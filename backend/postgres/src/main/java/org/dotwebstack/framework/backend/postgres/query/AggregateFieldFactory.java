8af6b638c5a84a7bbb3cf9ec474f836d716d0804
Arjen Hup <arjen.hup@kadaster.nl>
2021-05-21 15:57:11 +0200

    DHUB-359: spotless

--- backend/postgres/src/main/java/org/dotwebstack/framework/backend/postgres/query/AggregateFieldFactory.java
+++ backend/postgres/src/main/java/org/dotwebstack/framework/backend/postgres/query/AggregateFieldFactory.java
@@ -35,7 +35,8 @@ public class AggregateFieldFactory {
     var aggregateFunction = aggregateFieldConfiguration.getAggregateFunctionType();
     switch (aggregateFunction) {
       case AVG:
-        result = DSL.avg(bigDecimalField(fromTable, columnName)).cast(getNumericType(aggregateFieldConfiguration.getType()));
+        result = DSL.avg(bigDecimalField(fromTable, columnName))
+            .cast(getNumericType(aggregateFieldConfiguration.getType()));
         break;
       case COUNT:
         if (aggregateFieldConfiguration.isDistinct()) {
