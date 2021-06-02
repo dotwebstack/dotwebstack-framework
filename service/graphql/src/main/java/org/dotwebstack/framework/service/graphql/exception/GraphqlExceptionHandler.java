a87bca7d80dab5beb2581951efa2c123f9134808
switzc <cor.switzer@kadaster.nl>
2021-06-02 16:29:49 +0200

    [DHUB-321] Add exceptionHandling

---
+++ service/graphql/src/main/java/org/dotwebstack/framework/service/graphql/exception/GraphqlExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.dotwebstack.framework.service.graphql.exception;
+
+import static org.zalando.problem.Status.BAD_REQUEST;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ServerWebExchange;
+import org.zalando.problem.Problem;
+import org.zalando.problem.ThrowableProblem;
+import org.zalando.problem.spring.webflux.advice.ProblemHandling;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@ControllerAdvice
+public class GraphqlExceptionHandler implements ProblemHandling {
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public Mono<ResponseEntity<Problem>> handleIllegalArgumentException(Exception exception, ServerWebExchange request) {
+    LOG.error(exception.getMessage());
+
+    ThrowableProblem problem = Problem.builder()
+        .withStatus(BAD_REQUEST)
+        .withDetail(exception.getMessage())
+        .build();
+
+    return create(problem, request);
+  }
+}
