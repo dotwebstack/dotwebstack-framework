bb2933bd402f8b6abd3636f6797f223c87657255
switzc <cor.switzer@kadaster.nl>
2021-06-02 16:29:26 +0200

    [DHUB-321] Refactor GET method and add application/graphql support

--- service/graphql/src/main/java/org/dotwebstack/framework/service/graphql/GraphqlController.java
+++ service/graphql/src/main/java/org/dotwebstack/framework/service/graphql/GraphqlController.java
@@ -1,9 +1,15 @@
 package org.dotwebstack.framework.service.graphql;
 
+import static org.dotwebstack.framework.core.helpers.ExceptionHelper.illegalArgumentException;
+import static org.dotwebstack.framework.core.helpers.MapHelper.getNestedMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
+import java.io.IOException;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.dataloader.DataLoaderRegistry;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,44 +20,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 class GraphqlController {
 
   private final GraphQL graphQL;
 
+  private final ObjectMapper objectMapper;
+
+  private static final String QUERY = "query";
+
+  private static final String OPERATIONNAME = "operationName";
+
+  private static final String VARIABLES = "variables";
+
   public GraphqlController(GraphQL graphQL) {
     this.graphQL = graphQL;
+    this.objectMapper = new ObjectMapper();
   }
 
   @CrossOrigin
   @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Mono<Map<String, Object>> handleGet(@RequestParam("query") String query) {
-    var executionInput = ExecutionInput.newExecutionInput()
-        .query(query)
-        .dataLoaderRegistry(new DataLoaderRegistry())
-        .build();
+  public Mono<Map<String, Object>> handleGet(@RequestParam(QUERY) String query,
+      @RequestParam(value = OPERATIONNAME, required = false) String operationName,
+      @RequestParam(value = VARIABLES, required = false) String variablesJson) {
+
+    Map<String, Object> variablesMap = convertVariablesJson(variablesJson);
+
+    var executionInput = getExecutionInput(query, operationName, variablesMap);
 
     return Mono.fromFuture(graphQL.executeAsync(executionInput))
         .map(ExecutionResult::toSpecification);
   }
 
-
-//  @CrossOrigin
-//  @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-//  public Mono<Map<String, Object>> handleGet(@RequestParam("query") String query,
-//                                             @RequestParam(value = "operationName", required = false) String operationName,
-//                                             @RequestParam("variables") Map<String, Object> variables) {
-//
-//    var executionInput = getExecutionInput(query, operationName, variables);
-//
-//    return Mono.fromFuture(graphQL.executeAsync(executionInput))
-//        .map(ExecutionResult::toSpecification);
-//  }
-
-
   @CrossOrigin
-  @PostMapping(value="/graphql", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Mono<Map<String, Object>> handlePost(@RequestBody Map<String,Object> requestBody) {
+  @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public Mono<Map<String, Object>> handlePost(@RequestBody Map<String, Object> requestBody) {
 
     var executionInput = getExecutionInput(requestBody);
 
@@ -59,21 +64,40 @@ class GraphqlController {
         .map(ExecutionResult::toSpecification);
   }
 
+  @CrossOrigin
+  @PostMapping(value = "/graphql", consumes = "application/graphql", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Mono<Map<String, Object>> handlePost(@RequestBody(required = false) String body) {
+
+    var executionInput = getExecutionInput(body, null, Map.of());
+
+    return Mono.fromFuture(graphQL.executeAsync(executionInput))
+        .map(ExecutionResult::toSpecification);
+  }
+
   @SuppressWarnings("unchecked")
-  private ExecutionInput getExecutionInput(Map<String,Object> requestBody) {
-    return getExecutionInput((String) requestBody.get("query"), (String) requestBody.get("operationName"), (Map<String, Object>) requestBody.get("variables"));
+  private Map<String, Object> convertVariablesJson(String jsonMap) {
+    if (jsonMap == null) {
+      return Map.of();
+    }
+
+    try {
+      return objectMapper.readValue(jsonMap, Map.class);
+    } catch (IOException e) {
+      throw illegalArgumentException("Could not convert variables GET parameter: expected a JSON map");
+    }
+  }
+
+  private ExecutionInput getExecutionInput(Map<String, Object> requestBody) {
+    return getExecutionInput((String) requestBody.get(QUERY), (String) requestBody.get(OPERATIONNAME),
+        getNestedMap(requestBody, VARIABLES));
   }
 
   private ExecutionInput getExecutionInput(String query, String operationName, Map<String, Object> variables) {
-
-
-    var input = ExecutionInput.newExecutionInput()
+    return ExecutionInput.newExecutionInput()
         .query(query)
         .operationName(operationName)
         .variables(variables)
         .dataLoaderRegistry(new DataLoaderRegistry())
         .build();
-
-    return input;
   }
 }
