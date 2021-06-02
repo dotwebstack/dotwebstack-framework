4b48c6619cf73806fed52f1242aa809f8c17f17b
Robert-Jan Regout <robert-jan.regout@kadaster.nl>
2021-06-02 15:12:30 +0200

    Added filter validator.

--- core/src/main/java/org/dotwebstack/framework/core/config/validators/FilterValidator.java
+++ core/src/main/java/org/dotwebstack/framework/core/config/validators/FilterValidator.java
@@ -1,6 +1,14 @@
 package org.dotwebstack.framework.core.config.validators;
 
+import static org.springframework.util.StringUtils.uncapitalize;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.dotwebstack.framework.core.InvalidConfigurationException;
+import org.dotwebstack.framework.core.config.AbstractTypeConfiguration;
 import org.dotwebstack.framework.core.config.DotWebStackConfiguration;
+import org.dotwebstack.framework.core.config.FilterConfiguration;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +16,59 @@ public class FilterValidator implements DotWebStackConfigurationValidator {
 
   @Override
   public void validate(DotWebStackConfiguration dotWebStackConfiguration) {
+    List<String> validSortFields = ValidSortAndFilterFields.get(dotWebStackConfiguration);
 
+    Map<String, Map<String, FilterConfiguration>> filtersPerObjectTypeName =
+        getFiltersPerObjectTypeName(dotWebStackConfiguration);
+
+    filtersPerObjectTypeName.forEach((objectTypeName, filters) -> filters.entrySet()
+        .forEach(filterEntry -> validateFilterField(validSortFields, objectTypeName, filterEntry)));
+  }
+
+  private Map<String, Map<String, FilterConfiguration>> getFiltersPerObjectTypeName(
+      DotWebStackConfiguration dotWebStackConfiguration) {
+    return dotWebStackConfiguration.getObjectTypes()
+        .entrySet()
+        .stream()
+        .filter(this::isFilterListNotEmpty)
+        .collect(Collectors.toMap(Map.Entry::getKey, this::getFilterConfiguration));
+  }
+
+  private boolean isFilterListNotEmpty(Map.Entry<String, AbstractTypeConfiguration<?>> entry) {
+    return !entry.getValue()
+        .getFilters()
+        .isEmpty();
+  }
+
+  private Map<String, FilterConfiguration> getFilterConfiguration(
+      Map.Entry<String, AbstractTypeConfiguration<?>> entry) {
+    return entry.getValue()
+        .getFilters();
+  }
+
+  private void validateFilterField(List<String> validSortFields, String objectTypeName,
+      Map.Entry<String, FilterConfiguration> filterEntry) {
+    String filterFieldName = getFilterFieldName(objectTypeName, filterEntry);
+
+    if (!validSortFields.contains(filterFieldName)) {
+      throw new InvalidConfigurationException(
+          String.format("Filter field '%s' in object type '%s' can't be resolved.", filterFieldName, objectTypeName));
+    }
+  }
+
+  private String getFilterFieldName(String objectTypeName, Map.Entry<String, FilterConfiguration> filterEntry) {
+    String fieldName;
+
+    if (filterEntry.getValue()
+        .getField() != null) {
+      fieldName = filterEntry.getValue()
+          .getField();
+    } else {
+      fieldName = filterEntry.getKey();
+    }
+
+    return fieldName.contains(".") ? fieldName
+        : uncapitalize(objectTypeName).concat(".")
+            .concat(fieldName);
   }
 }
