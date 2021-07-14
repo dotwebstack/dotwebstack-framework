387513c8e64670d70aed987666bbc36257f5b5c1
Robert-Jan Regout <robert-jan.regout@kadaster.nl>
2021-07-14 13:05:19 +0200

    Added Context to GraphQL schema.

---
+++ core/src/main/java/org/dotwebstack/framework/core/datafetchers/ContextConstants.java
@@ -0,0 +1,8 @@
+package org.dotwebstack.framework.core.datafetchers;
+
+class ContextConstants {
+
+  public static final String CONTEXT_TYPE_NAME = "Context";
+
+  public static final String CONTEXT_ARGUMENT_NAME = "context";
+}
