48471e748b9fb2c1dc05dd55830900b2a87589de
Marco Simons <marco.simons@kadaster.nl>
2021-07-16 17:51:23 +0200

    DHUB-423 - IT tests

--- integration-test/graphql-postgres/src/test/java/org/dotwebstack/framework/integrationtest/graphqlpostgres/GraphQlPostgresWithContextIntegrationTest.java
+++ integration-test/graphql-postgres/src/test/java/org/dotwebstack/framework/integrationtest/graphqlpostgres/GraphQlPostgresWithContextIntegrationTest.java
@@ -2,7 +2,6 @@ package org.dotwebstack.framework.integrationtest.graphqlpostgres;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIterableWithSize.iterableWithSize;
 import static org.hamcrest.collection.IsMapContaining.hasEntry;
 import static org.hamcrest.core.IsIterableContaining.hasItems;
@@ -20,6 +19,8 @@ import org.hamcrest.MatcherAssert;
 import org.jooq.tools.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -75,7 +76,24 @@ class GraphQlPostgresWithContextIntegrationTest {
   }
 
   @Test
-  void getRequest_returnBeers_forActualDefault() {
+  void getRequest_returnBeer_forActual() {
+    var query =
+        "{\n" + "  beer(identifier_beer: \"b0e7cf18-e3ce-439b-a63e-034c8452f59c\", context:{validOn: \"2020-09-01\", "
+            + "availableOn: \"2020-02-01T00:00:00Z\"}) {\n" + "    name\n" + "    soldPerYear\n" + "    brewery {\n"
+            + "      identifier_brewery\n" + "      name\n" + "    }\n" + "  }\n" + "}";
+
+    JsonNode json = executeQuery(query);
+
+    assertThat(json.has(ERRORS), is(false));
+
+    Map<String, Object> data = getDataFromJsonNode(json);
+
+    assertThat(data, hasEntry(equalTo("beer"),
+        hasEntry(equalTo("name"), equalTo("Beer 1 validStart: 2019-01-01, availableStart: 2019-04-01T12:00:00Z"))));
+  }
+
+  @Test
+  void getRequest_returnBeers_forDefault() {
     var query = "{\n" + "  beers(context:{}) {\n" + "    \tnodes {\n" + "        name\n" + "        soldPerYear\n"
         + "        brewery {\n" + "          name\n" + "        }\n" + "      }\n" + "  }\n" + "}";
 
@@ -94,83 +112,6 @@ class GraphQlPostgresWithContextIntegrationTest {
   }
 
   @Test
-  void getRequest_returnBeers_forActual() {
-    var query = "{\n" + "  beers(context:{validOn: \"2020-09-01\", availableOn: \"2020-02-01T00:00:00Z\"}) {\n"
-        + "    \tnodes {\n" + "        name\n" + "        soldPerYear\n" + "        brewery {\n"
-        + "          identifier_brewery\n" + "          name\n" + "        }\n" + "      }\n" + "  }\n" + "}";
-
-    JsonNode json = executeQuery(query);
-
-    assertThat(json.has(ERRORS), is(false));
-
-    Map<String, Object> data = getDataFromJsonNode(json);
-
-    assertThat(data, hasEntry(equalTo("beers"), hasEntry(equalTo("nodes"),
-        hasItems(
-            hasEntry(equalTo("name"), equalTo("Beer 1 validStart: 2019-01-01, availableStart: 2019-04-01T12:00:00Z")),
-            hasEntry(equalTo("name"), equalTo("Beer 2")), hasEntry(equalTo("name"), equalTo("Beer 3")),
-            hasEntry(equalTo("name"), equalTo("Beer 4")), hasEntry(equalTo("name"), equalTo("Beer 5")),
-            hasEntry(equalTo("name"), equalTo("Beer 6"))))));
-  }
-
-  @Test
-  void getRequest_returnBeer_forActual() {
-    var query =
-        "{\n" + "  beer(identifier_beer: \"b0e7cf18-e3ce-439b-a63e-034c8452f59c\", context:{validOn: \"2020-09-01\", "
-            + "availableOn: \"2020-02-01T00:00:00Z\"}) {\n" + "    name\n" + "    soldPerYear\n" + "    brewery {\n"
-            + "      identifier_brewery\n" + "      name\n" + "    }\n" + "  }\n" + "}";
-
-    JsonNode json = executeQuery(query);
-
-    assertThat(json.has(ERRORS), is(false));
-
-    Map<String, Object> data = getDataFromJsonNode(json);
-
-    assertThat(data, hasEntry(equalTo("beer"),
-        hasEntry(equalTo("name"), equalTo("Beer 1 validStart: 2019-01-01, availableStart: 2019-04-01T12:00:00Z"))));
-  }
-
-  @Test
-  void getRequest_returnBeers_forValidFirstAvailableActual() {
-    var query = "{\n" + "  beers(context:{validOn: \"2018-06-01\", availableOn: \"2020-02-01T00:00:00Z\"}) {\n"
-        + "    \tnodes {\n" + "        name\n" + "        soldPerYear\n" + "        brewery {\n"
-        + "          identifier_brewery\n" + "          name\n" + "        }\n" + "      }\n" + "  }\n" + "}";
-
-    JsonNode json = executeQuery(query);
-
-    assertThat(json.has(ERRORS), is(false));
-
-    Map<String, Object> data = getDataFromJsonNode(json);
-
-    assertThat(data, hasEntry(equalTo("beers"), hasEntry(equalTo("nodes"),
-        hasItems(
-            hasEntry(equalTo("name"), equalTo("Beer 1 validStart: 2018-01-01, availableStart: 2019-04-01T12:00:00Z")),
-            hasEntry(equalTo("name"), equalTo("Beer 2")), hasEntry(equalTo("name"), equalTo("Beer 3")),
-            hasEntry(equalTo("name"), equalTo("Beer 4")), hasEntry(equalTo("name"), equalTo("Beer 5")),
-            hasEntry(equalTo("name"), equalTo("Beer 6"))))));
-  }
-
-  @Test
-  void getRequest_returnBeers_forValidActualAvailableFirst() {
-    var query = "{\n" + "  beers(context:{validOn: \"2018-06-01\", availableOn: \"2019-03-01T00:00:00Z\"}) {\n"
-        + "    \tnodes {\n" + "        name\n" + "        soldPerYear\n" + "        brewery {\n"
-        + "          identifier_brewery\n" + "          name\n" + "        }\n" + "      }\n" + "  }\n" + "}";
-
-    JsonNode json = executeQuery(query);
-
-    assertThat(json.has(ERRORS), is(false));
-
-    Map<String, Object> data = getDataFromJsonNode(json);
-
-    assertThat(data, hasEntry(equalTo("beers"), hasEntry(equalTo("nodes"),
-        hasItems(
-            hasEntry(equalTo("name"), equalTo("Beer 1 validStart: 2018-01-01, availableStart: 2018-02-01T12:00:00Z")),
-            hasEntry(equalTo("name"), equalTo("Beer 2")), hasEntry(equalTo("name"), equalTo("Beer 3")),
-            hasEntry(equalTo("name"), equalTo("Beer 4")), hasEntry(equalTo("name"), equalTo("Beer 5")),
-            hasEntry(equalTo("name"), equalTo("Beer 6"))))));
-  }
-
-  @Test
   void getRequest_returnsEmptyResult_forQueryBeforeOrigin() {
     var query = "{\n" + "  beers(context:{validOn: \"2015-06-01\", availableOn: \"2015-02-01T00:00:00Z\"}) {\n"
         + "    \tnodes {\n" + "        name\n" + "        soldPerYear\n" + "        brewery {\n"
@@ -183,6 +124,31 @@ class GraphQlPostgresWithContextIntegrationTest {
     Map<String, Object> data = getDataFromJsonNode(json);
 
     assertThat(data, hasEntry(equalTo("beers"), hasEntry(equalTo("nodes"), iterableWithSize(0))));
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      value = {"2020-09-01;2020-02-01T00:00:00Z;Beer 1 validStart: 2019-01-01, availableStart: 2019-04-01T12:00:00Z",
+          "2018-06-01;2020-02-01T00:00:00Z;Beer 1 validStart: 2018-01-01, availableStart: 2019-04-01T12:00:00Z",
+          "2018-06-01;2019-03-01T00:00:00Z;Beer 1 validStart: 2018-01-01, availableStart: 2018-02-01T12:00:00Z"},
+      delimiterString = ";")
+  void getRequest_returnBeers_forPunctureDateScenarios(String validOn, String availableOn, String expected) {
+    var query = String.format("{\n" + "  beers(context:{validOn: \"%s\", availableOn: \"%s\"}) {\n" + "    \tnodes {\n"
+        + "        name\n" + "        soldPerYear\n" + "        brewery {\n" + "          identifier_brewery\n"
+        + "          name\n" + "        }\n" + "      }\n" + "  }\n" + "}", validOn, availableOn);
+
+    JsonNode json = executeQuery(query);
+
+    assertThat(json.has(ERRORS), is(false));
+
+    Map<String, Object> data = getDataFromJsonNode(json);
+
+    assertThat(data,
+        hasEntry(equalTo("beers"),
+            hasEntry(equalTo("nodes"),
+                hasItems(hasEntry(equalTo("name"), equalTo(expected)), hasEntry(equalTo("name"), equalTo("Beer 2")),
+                    hasEntry(equalTo("name"), equalTo("Beer 3")), hasEntry(equalTo("name"), equalTo("Beer 4")),
+                    hasEntry(equalTo("name"), equalTo("Beer 5")), hasEntry(equalTo("name"), equalTo("Beer 6"))))));
   }
 
   private JsonNode executeQuery(String query) {
