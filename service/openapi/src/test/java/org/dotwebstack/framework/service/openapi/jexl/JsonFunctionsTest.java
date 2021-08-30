b66f4c7b303c8f862d1689789b762ebafbe22b0b
klaijb <bastiaan.klaij@kadaster.nl>
2021-08-30 10:52:56 +0200

    [IHR2-6658] - fix test

--- service/openapi/src/test/java/org/dotwebstack/framework/service/openapi/jexl/JsonFunctionsTest.java
+++ service/openapi/src/test/java/org/dotwebstack/framework/service/openapi/jexl/JsonFunctionsTest.java
@@ -2,10 +2,11 @@ package org.dotwebstack.framework.service.openapi.jexl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import org.junit.jupiter.api.Test;
 
 class JsonFunctionsTest {
@@ -17,9 +18,12 @@ class JsonFunctionsTest {
 
   @Test
   public void toJson_returnsExpectedValue() {
-    String json = new JsonFunctions().asString(Map.of("key1", List.of("v1, v2"), "key2", 2));
+    Map<String, Object> input = new TreeMap<>();
+    input.put("key1", List.of("v1", "v2"));
+    input.put("key2", 2);
+    String json = new JsonFunctions().asString(input);
 
-    assertThat(json, is("{\"key1\":[\"v1, v2\"],\"key2\":2}"));
+    assertThat(json, is("{\"key1\":[\"v1\",\"v2\"],\"key2\":2}"));
   }
 
 }
