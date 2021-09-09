95487ea5bb6d1a6e2b3d17b7860f72e0045ae20c
Pano Maria <pano.maria@gmail.com>
2021-09-09 08:42:02 +0200

    [GDP-23] make namespaces configurable

--- ext/ext-rml/src/main/java/org/dotwebstack/framework/ext/rml/mapping/RmlConfiguration.java
+++ ext/ext-rml/src/main/java/org/dotwebstack/framework/ext/rml/mapping/RmlConfiguration.java
@@ -4,29 +4,57 @@ import com.taxonic.carml.engine.rdf.RdfRmlMapper;
 import com.taxonic.carml.logicalsourceresolver.JaywayJacksonJsonPathResolver;
 import com.taxonic.carml.model.TriplesMap;
 import com.taxonic.carml.vocab.Rdf;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.NonNull;
+import lombok.Setter;
+import org.dotwebstack.framework.core.config.YamlPropertySourceFactory;
 import org.dotwebstack.framework.service.openapi.HttpMethodOperation;
 import org.eclipse.rdf4j.model.Namespace;
+import org.eclipse.rdf4j.model.impl.SimpleNamespace;
 import org.eclipse.rdf4j.model.vocabulary.DCTERMS;
-import org.eclipse.rdf4j.model.vocabulary.FOAF;
 import org.eclipse.rdf4j.model.vocabulary.OWL;
-import org.eclipse.rdf4j.model.vocabulary.PROV;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.model.vocabulary.RDFS;
-import org.eclipse.rdf4j.model.vocabulary.SKOS;
 import org.eclipse.rdf4j.model.vocabulary.XSD;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-public class RmlMapperConfiguration {
+@ConfigurationProperties(prefix = "rml")
+@PropertySource(value = "classpath:config/${dotwebstack.config:dotwebstack.yaml}",
+    factory = YamlPropertySourceFactory.class)
+@Setter
+public class RmlConfiguration {
+
+  private static final Set<Namespace> DEFAULT_NAMESPACES = Set.of(RDF.NS, RDFS.NS, OWL.NS, XSD.NS, DCTERMS.NS);
+
+  private Map<String, String> namespaces = new HashMap<>();
 
   @Bean
-  RdfRmlMapper rmlMapper(Map<HttpMethodOperation, Set<TriplesMap>> mappingsPerOperation,
-      List<RmlMapperConfigurer> rmlMapperConfigurers) {
+  Set<Namespace> namespaces() {
+    if (namespaces.isEmpty()) {
+      return DEFAULT_NAMESPACES;
+    }
+
+    DEFAULT_NAMESPACES
+        .forEach(ns -> namespaces.merge(ns.getPrefix(), ns.getName(), (configuredNs, defaultNs) -> configuredNs));
+
+    return namespaces.entrySet()
+        .stream()
+        .map(entry -> new SimpleNamespace(entry.getKey(), entry.getValue()))
+        .collect(Collectors.toUnmodifiableSet());
+  }
+
+  @Bean
+  RdfRmlMapper rmlMapper(@NonNull Map<HttpMethodOperation, Set<TriplesMap>> mappingsPerOperation,
+      @NonNull List<RmlMapperConfigurer> rmlMapperConfigurers) {
+
     RdfRmlMapper.Builder mapperBuilder = RdfRmlMapper.builder()
         .setLogicalSourceResolver(Rdf.Ql.JsonPath, JaywayJacksonJsonPathResolver::getInstance)
         .triplesMaps(mappingsPerOperation.values()
@@ -37,10 +65,5 @@ public class RmlMapperConfiguration {
     rmlMapperConfigurers.forEach(rmlMapperConfigurer -> rmlMapperConfigurer.configureMapper(mapperBuilder));
 
     return mapperBuilder.build();
-  }
-
-  @Bean
-  Set<Namespace> namespaces() {
-    return Set.of(RDF.NS, RDFS.NS, OWL.NS, SKOS.NS, PROV.NS, FOAF.NS, XSD.NS, DCTERMS.NS);
   }
 }
