892c93ca50d51c5b3e44f0686f324a26cb9bc14a
Joost Farla <joostfarla@users.noreply.github.com>
2021-09-13 10:34:45 +0200

    [DHUB-757] Refactor blocking calls (#455)

--- service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/requestbody/RequestBodyHandler.java
+++ service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/requestbody/RequestBodyHandler.java
@@ -4,14 +4,14 @@ import io.swagger.v3.oas.models.parameters.RequestBody;
 import java.util.Map;
 import lombok.NonNull;
 import org.dotwebstack.framework.core.query.GraphQlField;
-import org.dotwebstack.framework.service.openapi.exception.BadRequestException;
 import org.dotwebstack.framework.service.openapi.response.RequestBodyContext;
 import org.springframework.web.reactive.function.server.ServerRequest;
+import reactor.core.publisher.Mono;
 
 public interface RequestBodyHandler {
 
-  Map<String, Object> getValues(@NonNull ServerRequest request, @NonNull RequestBodyContext requestBodyContext,
-      @NonNull RequestBody requestBody, Map<String, Object> parameterMap) throws BadRequestException;
+  Mono<Map<String, Object>> getValues(@NonNull ServerRequest request, @NonNull RequestBodyContext requestBodyContext,
+      @NonNull RequestBody requestBody, Map<String, Object> parameterMap);
 
   void validate(@NonNull GraphQlField graphQlField, @NonNull RequestBody requestBody, @NonNull String pathName);
 
