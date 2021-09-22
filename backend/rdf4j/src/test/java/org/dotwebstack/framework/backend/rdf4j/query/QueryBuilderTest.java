aeff59c934759fd8cfaaa35f02fe37b518b01572
Joost Farla <joost@apiwise.nl>
2021-09-22 19:49:56 +0200

    Add test

--- backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/query/QueryBuilderTest.java
+++ backend/rdf4j/src/test/java/org/dotwebstack/framework/backend/rdf4j/query/QueryBuilderTest.java
@@ -15,6 +15,7 @@ import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.SelectedField;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -29,6 +30,7 @@ import org.dotwebstack.framework.core.config.DotWebStackConfiguration;
 import org.dotwebstack.framework.core.config.KeyConfiguration;
 import org.dotwebstack.framework.core.datafetchers.FieldKeyCondition;
 import org.dotwebstack.framework.core.datafetchers.KeyCondition;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -61,7 +63,7 @@ class QueryBuilderTest {
   }
 
   @Test
-  void buildwithKeyCondition() {
+  void build_withKeyCondition() {
     Rdf4jTypeConfiguration typeConfiguration = createBeerTypeConfiguration();
 
     when(nodeShapeRegistry.get("Beer")).thenReturn(NodeShape.builder()
@@ -106,7 +108,7 @@ class QueryBuilderTest {
   }
 
   @Test
-  void buildwithoutKeyCondition() {
+  void build_withoutKeyCondition() {
     Rdf4jTypeConfiguration typeConfiguration = createBeerTypeConfiguration();
 
     when(nodeShapeRegistry.get("Beer")).thenReturn(NodeShape.builder()
@@ -143,6 +145,49 @@ class QueryBuilderTest {
         + "WHERE { ?x1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>/<http://www.w3.org/2000/01/rdf-schema#subClassOf>* <https://github.com/dotwebstack/beer/def#Beer> .\n"
         + "OPTIONAL { ?x1 <https://github.com/dotwebstack/beer/def#identifier> ?x2 . }\n"
         + "OPTIONAL { ?x1 <https://github.com/dotwebstack/beer/def#name> ?x3 . } }\n" + "LIMIT 10\n"));
+  }
+
+  @Test
+  void build_returnsValuesBlock_forOrClassConstraints() {
+    Rdf4jTypeConfiguration typeConfiguration = createBeerTypeConfiguration();
+
+    when(nodeShapeRegistry.get("Beer")).thenReturn(NodeShape.builder()
+        .name("Beer")
+        .classes(Set.of(
+            new LinkedHashSet<IRI>(List.of(simpleValueFactory.createIRI("https://github.com/dotwebstack/beer/def#Beer"),
+                simpleValueFactory.createIRI("https://github.com/dotwebstack/beer/def#Beverage")))))
+        .propertyShapes(Map.of("identifier", PropertyShape.builder()
+            .path(PredicatePath.builder()
+                .iri(simpleValueFactory.createIRI("https://github.com/dotwebstack/beer/def#identifier"))
+                .build())
+            .build(), "name",
+            PropertyShape.builder()
+                .path(PredicatePath.builder()
+                    .iri(simpleValueFactory.createIRI("https://github.com/dotwebstack/beer/def#name"))
+                    .build())
+                .build()))
+        .build());
+
+    List<SelectedField> selectedFields = List.of(mockSelectedField(FIELD_IDENTIFIER,
+        GraphQLFieldDefinition.newFieldDefinition()
+            .name(FIELD_IDENTIFIER)
+            .type(Scalars.GraphQLString)
+            .build()),
+        mockSelectedField(FIELD_NAME, GraphQLFieldDefinition.newFieldDefinition()
+            .name(FIELD_NAME)
+            .type(Scalars.GraphQLString)
+            .build()));
+
+    DataFetchingFieldSelectionSet selectionSet = mockDataFetchingFieldSelectionSet(selectedFields);
+
+    QueryHolder queryHolder = queryBuilder.build(typeConfiguration, selectionSet, (KeyCondition) null);
+
+    assertThat(queryHolder, notNullValue());
+    assertThat(queryHolder.getQuery(), equalTo("SELECT ?x3 ?x4\n"
+        + "WHERE { VALUES ?x2 {<https://github.com/dotwebstack/beer/def#Beer> <https://github.com/dotwebstack/beer/def#Beverage>}\n"
+        + "?x1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>/<http://www.w3.org/2000/01/rdf-schema#subClassOf>* ?x2 .\n"
+        + "OPTIONAL { ?x1 <https://github.com/dotwebstack/beer/def#identifier> ?x3 . }\n"
+        + "OPTIONAL { ?x1 <https://github.com/dotwebstack/beer/def#name> ?x4 . } }\n" + "LIMIT 10\n"));
   }
 
   private Rdf4jTypeConfiguration createBeerTypeConfiguration() {
