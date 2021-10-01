bee0cc01f5594cff0985eca6cbbd024ac491be3c
Arjen Hup <arjen.hup@kadaster.nl>
2021-10-01 16:05:55 +0200

    DHUB-957: ut gefixed

--- core/src/test/java/org/dotwebstack/framework/core/config/validators/FilterValidatorTest.java
+++ core/src/test/java/org/dotwebstack/framework/core/config/validators/FilterValidatorTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.dotwebstack.framework.core.InvalidConfigurationException;
+import org.dotwebstack.framework.core.config.DotWebStackConfiguration;
 import org.dotwebstack.framework.core.config.DotWebStackConfigurationReader;
 import org.dotwebstack.framework.core.config.FieldConfigurationImpl;
 import org.dotwebstack.framework.core.config.TypeConfigurationImpl;
@@ -18,19 +19,27 @@ class FilterValidatorTest {
   @Test
   void validate_throwsNoException_withValidFilterFields() {
     var dotWebStackConfiguration = dwsReader.read("validators/dotwebstack-with-valid-filter.yaml");
-
+    initObjectTypes(dotWebStackConfiguration);
     new FilterValidator().validate(dotWebStackConfiguration);
   }
 
   @Test
   void validate_throwsNoException_withInvalidFilterFields() {
     var dotWebStackConfiguration = dwsReader.read("validators/dotwebstack-with-invalid-filter.yaml");
-
+    initObjectTypes(dotWebStackConfiguration);
     FilterValidator validator = new FilterValidator();
     var exception =
         assertThrows(InvalidConfigurationException.class, () -> validator.validate(dotWebStackConfiguration));
 
-    assertThat(exception.getMessage(), is("Filter field 'brewery.visitAddress.invalid' in object type 'Brewery' "
+    assertThat(exception.getMessage(), is("Filter field 'visitAddress.invalid' in object type 'Brewery' "
         + "can't be resolved to a single scalar type."));
+  }
+
+  private void initObjectTypes(DotWebStackConfiguration dotWebStackConfiguration) {
+    if (dotWebStackConfiguration.getObjectTypes() != null) {
+      dotWebStackConfiguration.getObjectTypes()
+          .values()
+          .forEach(objectType -> objectType.init(dotWebStackConfiguration));
+    }
   }
 }
