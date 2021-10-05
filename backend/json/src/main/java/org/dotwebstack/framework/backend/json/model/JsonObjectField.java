d6a01061d22b7202ef3f55275b5b3b9fc91be8dd
Marco Simons <marco.simons@kadaster.nl>
2021-10-05 10:07:12 +0200

    Remove json backend

--- backend/json/src/main/java/org/dotwebstack/framework/backend/json/model/JsonObjectField.java
+++
@@ -1,7 +0,0 @@
-package org.dotwebstack.framework.backend.json.model;
-
-import org.dotwebstack.framework.core.model.AbstractObjectField;
-
-public class JsonObjectField extends AbstractObjectField {
-
-}
--- backend/json/src/main/java/org/dotwebstack/framework/backend/json/model/JsonObjectType.java
+++
@@ -1,7 +0,0 @@
-package org.dotwebstack.framework.backend.json.model;
-
-import org.dotwebstack.framework.core.model.AbstractObjectType;
-
-public class JsonObjectType extends AbstractObjectType<JsonObjectField> {
-
-}
