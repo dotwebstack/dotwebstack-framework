006ec0b5ff2e51ea5b0c740b22ac2db4aeab43ad
Marco Simons <marco.simons@kadaster.nl>
2021-10-08 17:50:21 +0200

    Fixing unittest

--- backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/query/TableHelperTest.java
+++ backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/query/TableHelperTest.java
@@ -1,33 +1,38 @@
-// package org.dotwebstack.framework.backend.postgres.query;
-//
-// import static org.hamcrest.CoreMatchers.equalTo;
-// import static org.hamcrest.CoreMatchers.notNullValue;
-// import static org.hamcrest.MatcherAssert.assertThat;
-//
-// import java.util.List;
-// import org.dotwebstack.framework.core.query.model.ContextCriteria;
-// import org.jooq.Record;
-// import org.jooq.Table;
-// import org.junit.jupiter.api.Test;
-//
-// class TableHelperTest {
-//
-// @Test
-// void findTable_returnsValue_forContextCriteria() {
-// Table<Record> result = TableHelper.findTable("table", List.of(ContextCriteria.builder()
-// .field("arg")
-// .value("val")
-// .build()));
-//
-// assertThat(result, notNullValue());
-// assertThat(result.toString(), equalTo("table_ctx('val')"));
-// }
-//
-// @Test
-// void findTable_returnsValue_forDefault() {
-// Table<Record> result = TableHelper.findTable("table", List.of());
-//
-// assertThat(result, notNullValue());
-// assertThat(result.getName(), equalTo("table"));
-// }
-// }
+package org.dotwebstack.framework.backend.postgres.query;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.Map;
+import org.dotwebstack.framework.core.model.Context;
+import org.dotwebstack.framework.core.query.model.ContextCriteria;
+import org.jooq.Record;
+import org.jooq.Table;
+import org.junit.jupiter.api.Test;
+
+class TableHelperTest {
+
+  @Test
+  void findTable_returnsValue_forContextCriteria() {
+    var contextCriteria = ContextCriteria.builder()
+        .name("test")
+        .context(mock(Context.class))
+        .values(Map.of("arg", "val"))
+        .build();
+
+    Table<Record> result = TableHelper.findTable("table", contextCriteria);
+
+    assertThat(result, notNullValue());
+    assertThat(result.toString(), equalTo("table_test_ctx('val')"));
+  }
+
+  @Test
+  void findTable_returnsValue_forDefault() {
+    Table<Record> result = TableHelper.findTable("table", null);
+
+    assertThat(result, notNullValue());
+    assertThat(result.getName(), equalTo("table"));
+  }
+}
