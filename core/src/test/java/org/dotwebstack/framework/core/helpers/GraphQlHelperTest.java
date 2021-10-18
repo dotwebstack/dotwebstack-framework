bcf4ad0a70af419af245db73556e9c8c184cb841
Alexander Antipin <alexander.antipin@kadaster.nl>
2021-10-18 09:56:03 +0200

    DHUB-1050: Updated GraphQlHelperTest coverage

--- core/src/test/java/org/dotwebstack/framework/core/helpers/GraphQlHelperTest.java
+++ core/src/test/java/org/dotwebstack/framework/core/helpers/GraphQlHelperTest.java
@@ -1,63 +1,81 @@
 package org.dotwebstack.framework.core.helpers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import graphql.language.BooleanValue;
 import graphql.language.FloatValue;
 import graphql.language.IntValue;
 import graphql.language.ObjectField;
+import graphql.language.ObjectTypeDefinition;
 import graphql.language.ObjectValue;
 import graphql.language.StringValue;
 import graphql.language.TypeName;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLOutputType;
+import graphql.schema.GraphQLScalarType;
+import graphql.schema.GraphQLUnmodifiedType;
+import graphql.schema.SelectedField;
+import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.LocalDate;
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.dotwebstack.framework.core.helpers.GraphQlHelper.isIntrospectionField;
+import static org.dotwebstack.framework.core.helpers.GraphQlHelper.isNestedObjectField;
+import static org.dotwebstack.framework.core.helpers.GraphQlHelper.isObjectField;
+import static org.dotwebstack.framework.core.helpers.GraphQlHelper.isObjectListField;
+import static org.dotwebstack.framework.core.helpers.GraphQlHelper.isScalarField;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class GraphQlHelperTest {
-
+  
   @Test
   void getStringValue_returnsString_forStringValue() {
     StringValue value = StringValue.newStringValue("string value")
         .build();
-
+    
     String actual = GraphQlHelper.getStringValue(value);
-
+    
     assertEquals("string value", actual);
   }
-
+  
   @Test
   void getStringValue_returnsString_forIntValue() {
     IntValue value = IntValue.newIntValue()
         .value(BigInteger.TEN)
         .build();
-
+    
     String actual = GraphQlHelper.getStringValue(value);
-
+    
     assertEquals("10", actual);
   }
-
+  
   @Test
   void getStringValue_returnsString_forFloatValue() {
     FloatValue value = FloatValue.newFloatValue()
         .value(BigDecimal.valueOf(10.12))
         .build();
-
+    
     String actual = GraphQlHelper.getStringValue(value);
-
+    
     assertEquals("10.12", actual);
   }
-
+  
   @Test
   void getStringValue_returnsString_forBooleanValue() {
     BooleanValue value = BooleanValue.newBooleanValue(false)
         .build();
-
+    
     String actual = GraphQlHelper.getStringValue(value);
-
+    
     assertEquals("false", actual);
   }
-
+  
   @Test
   void getStringValue_returnsString_forOtherValue() {
     ObjectValue value = ObjectValue.newObjectValue()
@@ -67,35 +85,193 @@ class GraphQlHelperTest {
                 .build())
             .build())
         .build();
-
+    
     String actual = GraphQlHelper.getStringValue(value);
-
+    
     assertEquals("ObjectValue{objectFields=[ObjectField{name='field', value=StringValue{value='test'}}]}", actual);
   }
-
+  
   @Test
   void getValue_returnsString_forStringValue() {
     StringValue value = StringValue.newStringValue("string value")
         .build();
-
+    
     String actual = (String) GraphQlHelper.getValue(TypeName.newTypeName("String")
         .build(), value);
-
+    
     assertEquals("string value", actual);
   }
-
+  
   @Test
   void getValue_returnsDate_forNowStringValue() {
     LocalDate localDateBeforeTest = LocalDate.now();
     StringValue value = StringValue.newStringValue("NOW")
         .build();
-
+    
     Object actual = GraphQlHelper.getValue(TypeName.newTypeName("Date")
         .build(), value);
-
+    
     assertEquals("LocalDate", actual.getClass()
         .getSimpleName());
     assertEquals(localDateBeforeTest, actual);
     assertEquals(LocalDate.now(), actual);
   }
+  
+  @Test
+  void isScalarField_returnsFalse_forGraphQLObjectType() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    when(definitionMock.getAdditionalData()).thenReturn(Map.of("a", "b"));
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isScalarField.test(selectedFieldMock);
+    assertThat(r, is(false));
+  }
+  
+  @Test
+  void isScalarField_returnsTrue_forGraphQLScalarType() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLScalarType objectMock = mock(GraphQLScalarType.class);
+    when(selectedFieldMock.getType()).thenReturn(objectMock);
+    
+    var r = isScalarField.test(selectedFieldMock);
+    assertThat(r, is(true));
+  }
+  
+  @Test
+  void isScalarField_returnsTrue_forContainsKeyIsScalar() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    when(definitionMock.getAdditionalData()).thenReturn(Map.of("isScalar", "b"));
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isScalarField.test(selectedFieldMock);
+    assertThat(r, is(true));
+  }
+  
+  @Test
+  void isObjectField_returnsTrue_forGraphQLObjectType() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    when(definitionMock.getAdditionalData()).thenReturn(Map.of("a", "b"));
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isObjectField.test(selectedFieldMock);
+    assertThat(r, is(true));
+  }
+  
+  @Test
+  void isObjectField_returnsFalse_forIsConnectionType() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    Map<String, String> additionalData = new HashMap<>();
+    additionalData.put("isConnectionType", "b");
+    when(definitionMock.getAdditionalData()).thenReturn(additionalData);
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isObjectField.test(selectedFieldMock);
+    assertThat(r, is(false));
+  }
+  
+  @Test
+  void isObjectField_returnsFalse_forIsNested() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    Map<String, String> additionalData = new HashMap<>();
+    additionalData.put("isNested", "b");
+    when(definitionMock.getAdditionalData()).thenReturn(additionalData);
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isObjectField.test(selectedFieldMock);
+    assertThat(r, is(false));
+  }
+  
+  @Test
+  void isNestedObjectField_returnsTrue_forIsNested() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    Map<String, String> additionalData = new HashMap<>();
+    additionalData.put("isNested", "b");
+    when(definitionMock.getAdditionalData()).thenReturn(additionalData);
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isNestedObjectField.test(selectedFieldMock);
+    assertThat(r, is(true));
+  }
+  
+  @Test
+  void isNestedObjectField_returnsFalse_forNotIsNested() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    Map<String, String> additionalData = new HashMap<>();
+    additionalData.put("a", "b");
+    when(definitionMock.getAdditionalData()).thenReturn(additionalData);
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isNestedObjectField.test(selectedFieldMock);
+    assertThat(r, is(false));
+  }
+  
+  @Test
+  void isObjectListField_returnsTrue_forIsConnectionType() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    Map<String, String> additionalData = new HashMap<>();
+    additionalData.put("isConnectionType", "b");
+    when(definitionMock.getAdditionalData()).thenReturn(additionalData);
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isObjectListField.test(selectedFieldMock);
+    assertThat(r, is(true));
+  }
+  
+  @Test
+  void isObjectListField_returnsFalse_forNotList_andNotIsConnectionType() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    GraphQLUnmodifiedType objectMock = mock(GraphQLObjectType.class);
+    ObjectTypeDefinition definitionMock = mock(ObjectTypeDefinition.class);
+    Map<String, String> additionalData = new HashMap<>();
+    additionalData.put("a", "b");
+    when(definitionMock.getAdditionalData()).thenReturn(additionalData);
+    when(objectMock.getDefinition()).thenReturn(definitionMock);
+    when(selectedFieldMock.getType()).thenReturn((GraphQLOutputType) objectMock);
+    
+    var r = isObjectListField.test(selectedFieldMock);
+    assertThat(r, is(false));
+  }
+  
+  @Test
+  void isIntrospectionField_returnsTrue_forRightName() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    when(selectedFieldMock.getName()).thenReturn("__AA");
+    
+    var r = isIntrospectionField.test(selectedFieldMock);
+    assertThat(r, is(true));
+  }
+  
+  @Test
+  void isIntrospectionField_returnsFalse_forOtherName() {
+    SelectedField selectedFieldMock = mock(SelectedField.class);
+    when(selectedFieldMock.getName()).thenReturn("AA");
+    
+    var r = isIntrospectionField.test(selectedFieldMock);
+    assertThat(r, is(false));
+  }
+  
 }
