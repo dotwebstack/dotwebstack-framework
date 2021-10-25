5085145cd49823e0a6a51824b9a92b88526ae9d3
Robert-Jan Regout <robert-jan.regout@kadaster.nl>
2021-10-25 15:12:19 +0200

    Removed useless equals.

--- backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/model/PostgresObjectTypeTest.java
+++ backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/model/PostgresObjectTypeTest.java
@@ -1,14 +1,13 @@
 package org.dotwebstack.framework.backend.postgres.model;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 
-public class PostgresObjectTypeTest {
+class PostgresObjectTypeTest {
 
   private PostgresObjectType postgresObjectType = new PostgresObjectType();
 
@@ -23,6 +22,5 @@ public class PostgresObjectTypeTest {
 
     assertFalse(postgresObjectType.isNested());
     assertThat(postgresObjectType.getTable(), CoreMatchers.is("@@@"));
-    assertEquals(postgresObjectType, postgresObjectType);
   }
 }
