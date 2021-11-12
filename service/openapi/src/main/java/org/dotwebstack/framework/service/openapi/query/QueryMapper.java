e86c98798e54518a5bd47b1646aba61db7caa2e2
klaijb <bastiaan.klaij@kadaster.nl>
2021-11-12 10:31:42 +0100

    Map keys from oas params to graphql query

--- service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/query/QueryMapper.java
+++ service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/query/QueryMapper.java
@@ -3,10 +3,13 @@ package org.dotwebstack.framework.service.openapi.query;
 import static org.dotwebstack.framework.core.helpers.ExceptionHelper.invalidConfigurationException;
 
 import graphql.ExecutionInput;
+import graphql.language.Argument;
 import graphql.language.AstPrinter;
 import graphql.language.Field;
 import graphql.language.OperationDefinition;
 import graphql.language.SelectionSet;
+import graphql.language.StringValue;
+import graphql.language.Value;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
@@ -16,6 +19,7 @@ import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.dataloader.DataLoaderRegistry;
@@ -41,8 +45,8 @@ public class QueryMapper {
     var queryProperties = operationRequest.getContext()
         .getQueryProperties();
 
-    var queryField =
-        mapField(queryProperties.getField(), operationRequest.getResponseSchema(), graphQlSchema.getQueryType());
+    var queryField = mapField(queryProperties.getField(), operationRequest.getResponseSchema(),
+        graphQlSchema.getQueryType(), createKeyArguments(operationRequest));
 
     var query = OperationDefinition.newOperationDefinition()
         .name("Query")
@@ -85,17 +89,49 @@ public class QueryMapper {
     return schema.getProperties()
         .entrySet()
         .stream()
-        .map(entry -> mapField(entry.getKey(), entry.getValue(), (GraphQLObjectType) fieldType));
+        .map(entry -> mapField(entry.getKey(), entry.getValue(), (GraphQLObjectType) fieldType, List.of()));
   }
 
-  private Field mapField(String name, Schema<?> schema, GraphQLObjectType objectType) {
+  private Field mapField(String name, Schema<?> schema, GraphQLObjectType objectType, List<Argument> arguments) {
     var fieldDefinition = MapperUtils.getObjectField(objectType, name);
     var rawFieldType = GraphQLTypeUtil.unwrapAll(fieldDefinition.getType());
 
     if (rawFieldType instanceof GraphQLObjectType) {
-      return new Field(name, new SelectionSet(mapFields(schema, fieldDefinition).collect(Collectors.toList())));
+      return new Field(name, arguments,
+          new SelectionSet(mapFields(schema, fieldDefinition).collect(Collectors.toList())));
     }
 
     return new Field(name);
   }
+
+  private List<Argument> createKeyArguments(OperationRequest operationRequest) {
+    Map<String, Object> paramKeyValues = operationRequest.getContext()
+        .getQueryProperties()
+        .getKeys()
+        .entrySet()
+        .stream()
+        .filter(e -> operationRequest.getParameters()
+            .containsKey(paramKeyFromPath(e.getValue())))
+        .collect(Collectors.toMap(Map.Entry::getKey, e -> operationRequest.getParameters()
+            .get(paramKeyFromPath(e.getValue()))));
+
+    return paramKeyValues.entrySet()
+        .stream()
+        .map(e -> new Argument(e.getKey(), toArgumentValue(e.getValue())))
+        .collect(Collectors.toList());
+  }
+
+  private Value toArgumentValue(Object e) {
+    if (e instanceof String) {
+      return new StringValue((String) e);
+    } else {
+      // TODO: support other types
+      return new StringValue(e.toString());
+    }
+  }
+
+  private String paramKeyFromPath(String path) {
+    return path.substring(path.lastIndexOf(".") + 1);
+  }
+
 }
