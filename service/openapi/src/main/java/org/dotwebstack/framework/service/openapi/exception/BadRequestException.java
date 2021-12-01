08e92fd89e83b1cedf92704363bd4345842a5761
bastiaank@kadaster <49758961+bmklaijkadaster@users.noreply.github.com>
2021-12-01 15:56:22 +0100

    [IHR2-6953] support Geometry srid mapping (#513)
    * [IHR2-6953] - add support for srid param mapping
    
    * [IHR2-6953] - add tests
    
    * [IHR2-6953] - refactor, add documentation
    
    * [IHR2-6953] - tweak documentation
    
    * [IHR2-6953] - spotless
    
    * [IHR2-6953] - fix test for @NonNull
    
    * [IHR2-6953] - vars in tests

--- service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/exception/BadRequestException.java
+++ service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/exception/BadRequestException.java
@@ -1,6 +1,6 @@
 package org.dotwebstack.framework.service.openapi.exception;
 
-public class BadRequestException extends Exception {
+public class BadRequestException extends RuntimeException {
 
   static final long serialVersionUID = 1564735180022L;
 
