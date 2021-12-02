12c863043997938c5e837d7ef8e4c2a54dc20a4b
Joost Farla <joostfarla@users.noreply.github.com>
2021-12-02 08:48:17 +0100

    Code smells (#517)

--- service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/param/TransientParamHandler.java
+++ service/openapi/src/main/java/org/dotwebstack/framework/service/openapi/param/TransientParamHandler.java
@@ -5,8 +5,6 @@ import static org.dotwebstack.framework.service.openapi.helper.OasConstants.X_DW
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.parameters.Parameter;
-import lombok.NonNull;
-import org.dotwebstack.framework.core.query.GraphQlField;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +18,4 @@ public class TransientParamHandler extends DefaultParamHandler {
   public boolean supports(Parameter parameter) {
     return hasDwsExtensionWithValue(parameter, X_DWS_TRANSIENT, Boolean.TRUE);
   }
-
-  @Override
-  public void validate(@NonNull GraphQlField graphQlField, @NonNull Parameter parameter, @NonNull String pathName) {}
 }
