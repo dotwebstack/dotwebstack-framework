796cab857782a59710157627fa469f159cc45d86
Robert-Jan <51677068+rregout@users.noreply.github.com>
2021-12-09 09:10:15 +0100

    [DHUB-1169] Support for presence column for nullable nested objects. (#529)

--- backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/query/ObjectMapperTest.java
+++ backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/query/ObjectMapperTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.dotwebstack.framework.core.backend.query.FieldMapper;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,7 @@ class ObjectMapperTest {
     Map<String, Object> row = Map.of("aa", Map.of("b", "c"));
 
     var result = mapper.apply(row);
+
     assertThat(result, is(notNullValue()));
     assertThat(result.size(), is(1));
   }
@@ -47,7 +49,43 @@ class ObjectMapperTest {
     Map<String, Object> row = Map.of("bbb", Map.of("b", "c"));
 
     var result = mapper.apply(row);
+
     assertThat(result, is(notNullValue()));
     assertThat(result.size(), is(1));
+  }
+
+  @Test
+  void apply_returnsMap_forGetPresenceAliasTrue() {
+    mapper = new ObjectMapper(null, "bar");
+    mapper.register("foo", mock(FieldMapper.class));
+    Map<String, Object> row = Map.of("bar", true, "foo", Map.of("foo", "test"));
+
+    var result = mapper.apply(row);
+
+    assertThat(result, is(notNullValue()));
+    assertThat(result.size(), is(1));
+  }
+
+  @Test
+  void apply_returnsNull_forGetPresenceAliasFalse() {
+    mapper = new ObjectMapper(null, "bar");
+    mapper.register("foo", mock(FieldMapper.class));
+    Map<String, Object> row = Map.of("bar", false);
+
+    var result = mapper.apply(row);
+
+    assertThat(result, is(nullValue()));
+  }
+
+  @Test
+  void apply_returnsNull_forGetPresenceAliasNull() {
+    mapper = new ObjectMapper(null, "bar");
+    mapper.register("foo", mock(FieldMapper.class));
+    Map<String, Object> row = new HashMap<>();
+    row.put("bar", null);
+
+    var result = mapper.apply(row);
+
+    assertThat(result, is(nullValue()));
   }
 }
