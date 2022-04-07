cccba8876fe709536a69ddc20804dc88a9a306ca
Marco Simons <marco@imerce.nl>
2022-04-07 13:45:39 +0200

    Improved PostgresClient + unittests

--- backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/PostgresClientTest.java
+++ backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/PostgresClientTest.java
@@ -1,14 +1,19 @@
 package org.dotwebstack.framework.backend.postgres;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.r2dbc.postgresql.api.PostgresqlConnection;
 import io.r2dbc.postgresql.api.PostgresqlResult;
 import io.r2dbc.postgresql.api.PostgresqlStatement;
+import io.r2dbc.spi.Connection;
 import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.Wrapped;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.function.BiFunction;
 import org.dotwebstack.framework.backend.postgres.query.Query;
@@ -20,11 +25,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.reactivestreams.Publisher;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings({"rawtypes", "unchecked"})
 class PostgresClientTest {
 
   @Mock
@@ -33,44 +40,130 @@ class PostgresClientTest {
   private PostgresClient postgresClient;
 
   @BeforeEach
-  public void doBeforeEach() {
+  void doBeforeEach() {
     postgresClient = new PostgresClient(connectionFactory);
   }
 
   @Test
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  public void fetch_returnsMap_forSqlQuery() {
-    var sql = "select 1 from table";
+  void fetch_completes_forSqlQuery() {
+    var query = "select 1 from table";
 
-    var connection = mock(PostgresqlConnection.class);
-    when(connection.close()).thenReturn(Mono.empty());
+    Map<String, Object> rowData = Map.of("k", "v");
+
+    var statement = mockStatement(rowData);
+
+    var connection = mockPostgresConnection(statement, query);
 
     when(connectionFactory.create()).thenReturn((Publisher) Mono.just(connection));
 
-    var statement = mock(PostgresqlStatement.class);
-
-    var result = mock(PostgresqlResult.class);
-
-    Map<String, Object> data = Map.of("a", "value");
-
-    when(result.map(any(BiFunction.class))).thenReturn(Flux.just(data));
-
-    when(statement.execute()).thenReturn(Flux.just(result));
-
-    when(connection.createStatement(sql)).thenReturn(statement);
-
-    var actual = postgresClient.fetch(sql);
-
-    StepVerifier.create(actual)
-        .expectNext(data)
+    StepVerifier.create(postgresClient.fetch(query))
+        .expectNext(rowData)
         .verifyComplete();
+
     verify(connection).close();
   }
 
+  @Test
+  void fetch_throwsError_forException() {
+    var query = "select 1 from table";
+
+    Flux<Map<String, Object>> rowData = Flux.error(new SQLException("Something went wrong executing the query!"));
+
+    var statement = mockStatement(rowData);
+
+    var connection = mockPostgresConnection(statement, query);
+
+    when(connectionFactory.create()).thenReturn((Publisher) Mono.just(connection));
+
+    StepVerifier.create(postgresClient.fetch(query))
+        .verifyError();
+
+    verify(connection).close();
+  }
 
   @Test
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  public void fetch_returnsMap_forQueryWithRowMapper() {
+  void fetch_completes_forCancelWithPostgresConnection() {
+    var query = "select 1 from table";
+
+    Flux<Map<String, Object>> rowData = Flux.just(Map.of("k", "v"));
+
+    var statement = mockStatement(rowData);
+
+    var connection = mockPostgresConnection(statement, query);
+    when(connection.cancelRequest()).thenReturn(Mono.empty());
+
+    when(connectionFactory.create()).thenReturn((Publisher) Mono.just(connection));
+
+    var actual = postgresClient.fetch(query);
+
+    actual.subscribe(new CancelOnNextSubscriber());
+
+    StepVerifier.create(actual)
+        .expectNext(Map.of("k", "v"))
+        .verifyComplete();
+
+    verify(connection).cancelRequest();
+    verify(connection, times(2)).close();
+  }
+
+  @Test
+  void fetch_completes_forCancelWithWrappedConnection() {
+    var query = "select 1 from table";
+
+    Flux<Map<String, Object>> rowData = Flux.just(Map.of("k", "v"));
+
+    var statement = mockStatement(rowData);
+
+    var connection = mock(WrappedConnection.class);
+
+    when(connection.close()).thenReturn(Mono.empty());
+    when(connection.createStatement(query)).thenReturn(statement);
+
+    var postgresConnection = mock(PostgresqlConnection.class);
+    when(postgresConnection.cancelRequest()).thenReturn(Mono.empty());
+    when(connection.unwrap()).thenReturn(postgresConnection);
+
+    when(connectionFactory.create()).thenReturn((Publisher) Mono.just(connection));
+
+    var actual = postgresClient.fetch(query);
+
+    actual.subscribe(new CancelOnNextSubscriber());
+
+    StepVerifier.create(actual)
+        .expectNext(Map.of("k", "v"))
+        .verifyComplete();
+
+    verify(postgresConnection).cancelRequest();
+    verify(connection, times(2)).close();
+  }
+
+  @Test
+  void fetch_throwsError_forCancelWithUnknownConnection() {
+    var query = "select 1 from table";
+
+    Flux<Map<String, Object>> rowData = Flux.just(Map.of("k", "v"));
+
+    var statement = mockStatement(rowData);
+
+    var connection = mock(Connection.class);
+
+    when(connection.close()).thenReturn(Mono.empty());
+    when(connection.createStatement(query)).thenReturn(statement);
+
+    when(connectionFactory.create()).thenReturn((Publisher) Mono.just(connection));
+
+    var actual = postgresClient.fetch(query);
+
+    actual.subscribe(new CancelOnNextSubscriber());
+
+    StepVerifier.create(actual)
+        .expectErrorMessage("R2DBC connection could not be unwrapped.");
+
+    verify(connection, times(1)).close();
+  }
+
+  @Test
+  void fetch_completes_forQueryWithRowMapper() {
     var selectQuery = DSL.select(DSL.asterisk())
         .from(DSL.table("table"))
         .where(DSL.field("identifier", String.class)
@@ -89,28 +182,56 @@ class PostgresClientTest {
 
     when(query.getRowMapper()).thenReturn(rowMapper);
 
-    var connection = mock(PostgresqlConnection.class);
-    when(connection.close()).thenReturn(Mono.empty());
+    var statement = mockStatement(rowData);
+
+    var connection = mockPostgresConnection(statement, "select * from table where identifier = $1");
 
     when(connectionFactory.create()).thenReturn((Publisher) Mono.just(connection));
 
-    var statement = mock(PostgresqlStatement.class);
-
-    var result = mock(PostgresqlResult.class);
-
-    when(result.map(any(BiFunction.class))).thenReturn(Flux.just(rowData));
-
-    when(statement.execute()).thenReturn(Flux.just(result));
-    when(statement.bind(any(String.class), any(Object.class))).thenReturn(statement);
-
-    when(connection.createStatement("select * from table where identifier = $1")).thenReturn(statement);
-
-    var actual = postgresClient.fetch(query);
-
-    StepVerifier.create(actual)
+    StepVerifier.create(postgresClient.fetch(query))
         .expectNext(mappedData)
         .verifyComplete();
+
     verify(connection).close();
   }
 
+  private PostgresqlConnection mockPostgresConnection(PostgresqlStatement statement, String expectedQueryStatement) {
+    var connection = mock(PostgresqlConnection.class);
+
+    when(connection.close()).thenReturn(Mono.empty());
+
+    when(connection.createStatement(expectedQueryStatement)).thenReturn(statement);
+    return connection;
+  }
+
+  private PostgresqlStatement mockStatement(Map<String, Object> data) {
+    return mockStatement(Flux.just(data));
+  }
+
+  private PostgresqlStatement mockStatement(Flux<Map<String, Object>> data) {
+    var statement = mock(PostgresqlStatement.class);
+
+    var result = mock(PostgresqlResult.class);
+    when(result.map(any(BiFunction.class))).thenReturn(data);
+
+    var fluxResult = Flux.just(result);
+
+    lenient().when(statement.execute())
+        .thenReturn(fluxResult);
+
+    lenient().when(statement.bind(any(String.class), any(Object.class)))
+        .thenReturn(statement);
+
+    return statement;
+  }
+
+  private static class CancelOnNextSubscriber extends BaseSubscriber<Map<String, Object>> {
+    @Override
+    protected void hookOnNext(Map<String, Object> value) {
+      cancel();
+    }
+  }
+
+  private interface WrappedConnection extends Connection, Wrapped<PostgresqlConnection> {
+  }
 }
--- backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/codec/GeometryCodecTest.java
+++ backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/codec/GeometryCodecTest.java
@@ -19,27 +19,27 @@ class GeometryCodecTest {
   private GeometryCodec geometryCodec;
 
   @BeforeEach
-  public void doBeforeEach() {
+  void doBeforeEach() {
     geometryFactory = new GeometryFactory();
     geometryCodec = new GeometryCodec(Map.of("geometry", 1));
   }
 
   @Test
-  public void canDecode_returnsTrue_forSupportedDataType() {
+  void canDecode_returnsTrue_forSupportedDataType() {
     var actual = geometryCodec.canDecode(1, FORMAT_BINARY, Geometry.class);
 
     assertThat(actual, equalTo(true));
   }
 
   @Test
-  public void canDecode_returnsFalse_forUnsupportedDataType() {
+  void canDecode_returnsFalse_forUnsupportedDataType() {
     var actual = geometryCodec.canDecode(2, FORMAT_BINARY, Geometry.class);
 
     assertThat(actual, equalTo(false));
   }
 
   @Test
-  public void canEncode_returnsTrue_forSupportedObject() {
+  void canEncode_returnsTrue_forSupportedObject() {
     var object = geometryFactory.createPoint();
 
     var actual = geometryCodec.canEncode(object);
@@ -48,7 +48,7 @@ class GeometryCodecTest {
   }
 
   @Test
-  public void canEncode_returnsFalse_forUnsupportedObject() {
+  void canEncode_returnsFalse_forUnsupportedObject() {
     var object = "foo";
 
     var actual = geometryCodec.canEncode(object);
@@ -57,21 +57,21 @@ class GeometryCodecTest {
   }
 
   @Test
-  public void canEncodeNull_returnsTrue_forSupportedClass() {
+  void canEncodeNull_returnsTrue_forSupportedClass() {
     var actual = geometryCodec.canEncodeNull(Geometry.class);
 
     assertThat(actual, equalTo(true));
   }
 
   @Test
-  public void canEncodeNull_returnsFalse_forSupportedClass() {
+  void canEncodeNull_returnsFalse_forSupportedClass() {
     var actual = geometryCodec.canEncodeNull(String.class);
 
     assertThat(actual, equalTo(false));
   }
 
   @Test
-  public void encodeWithDatatype_returnsEncodedParameter_forGeometry() {
+  void encodeWithDatatype_returnsEncodedParameter_forGeometry() {
     var geometry = geometryFactory.createPoint(new Coordinate(1, 2));
 
     var actual = geometryCodec.encode(geometry, 1);
@@ -80,7 +80,7 @@ class GeometryCodecTest {
   }
 
   @Test
-  public void encode_returnsEncodedParameter_forGeometry() {
+  void encode_returnsEncodedParameter_forGeometry() {
     var geometry = geometryFactory.createPoint(new Coordinate(1, 2));
 
     var actual = geometryCodec.encode(geometry);
@@ -89,7 +89,7 @@ class GeometryCodecTest {
   }
 
   @Test
-  public void type_returnsGeometryClass() {
+  void type_returnsGeometryClass() {
     assertThat(geometryCodec.type(), equalTo(Geometry.class));
   }
 
