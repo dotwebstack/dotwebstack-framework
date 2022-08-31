0514594448d257cfc09017a45d47bb7807fc83da
Marco Simons <marco@imerce.nl>
2022-08-31 11:30:05 +0200

    Add Dummy class for code coverage test

---
+++ core/src/main/java/org/dotwebstack/framework/core/Dummy.java
@@ -0,0 +1,12 @@
+package org.dotwebstack.framework.core;
+
+public class Dummy {
+
+  public static boolean foo() {
+    return true;
+  }
+
+  public static boolean bar() {
+    return false;
+  }
+}
