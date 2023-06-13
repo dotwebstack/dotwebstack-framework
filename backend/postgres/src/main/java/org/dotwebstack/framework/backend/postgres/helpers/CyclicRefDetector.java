d982a0f6e7f3e04d9d85492856b5a198b702b7e5
Albert van de Kamp <albert@kamp-it.nl>
2023-06-13 15:51:23 +0200

    Cyclische detection added for when an object is referencing itself, this would cause an stackoverflow otherwise.

---
+++ backend/postgres/src/main/java/org/dotwebstack/framework/backend/postgres/helpers/CyclicRefDetector.java
@@ -0,0 +1,33 @@
+package org.dotwebstack.framework.backend.postgres.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.dotwebstack.framework.backend.postgres.model.PostgresObjectField;
+import org.dotwebstack.framework.backend.postgres.model.PostgresObjectType;
+
+@Log4j2
+public class CyclicRefDetector {
+
+  private final List<String> processedFields;
+
+  public CyclicRefDetector() {
+    processedFields = new ArrayList<>();
+  }
+
+  public boolean isProcessed(PostgresObjectType objectType, PostgresObjectField objectField) {
+    var fieldName = generateFieldName(objectType, objectField);
+    if (processedFields.contains(fieldName)) {
+      LOG.warn("Cyclic reference detected! Field {} is already processed.", fieldName);
+      return true;
+    }
+    LOG.trace("Field {} has been processed and is not part of a Cyclic reference.", fieldName);
+    processedFields.add(fieldName);
+    return false;
+  }
+
+  private String generateFieldName(PostgresObjectType objectType, PostgresObjectField objectField) {
+    return String.format("%s__%s__%s", objectType.getName(), objectField.getName(), objectField.getTargetType()
+        .getName());
+  }
+}
