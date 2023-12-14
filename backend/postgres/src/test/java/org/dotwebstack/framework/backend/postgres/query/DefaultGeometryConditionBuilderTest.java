6d0d6b069775c9b03060f80836af0f05655aa136
Michel ten Voorde <michel.tenvoorde@kadaster.nl>
2023-12-14 10:27:52 +0100

    DHUB-3187 Spotless

--- backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/query/DefaultGeometryConditionBuilderTest.java
+++ backend/postgres/src/test/java/org/dotwebstack/framework/backend/postgres/query/DefaultGeometryConditionBuilderTest.java
@@ -23,8 +23,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 class DefaultGeometryConditionBuilderTest {
   @ParameterizedTest
   @MethodSource("getGeometryConditionBuilderArguments")
-  void build_returnsCondition_forArguments(Object geometryValue, boolean useWorkaroundForIntersects, FilterOperator filterOperator,
-      String expectedCondition) {
+  void build_returnsCondition_forArguments(Object geometryValue, boolean useWorkaroundForIntersects,
+      FilterOperator filterOperator, String expectedCondition) {
     var objectField = mockObjectField(useWorkaroundForIntersects);
     var srid = 1;
     var sourceTable = DSL.table(DSL.name("db", "brewery"))
